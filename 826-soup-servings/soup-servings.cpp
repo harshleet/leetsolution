f85aabf4891290fa53805844e83351183760e174
harshleet <122361301+harshleet@users.noreply.github.com>
2023-07-29 12:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (82.51%) - LeetSync

--- 826-soup-servings/soup-servings.cpp
+++ 826-soup-servings/soup-servings.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    double helper(vector<vector<double>>&dp,int sa,int sb,vector<vector<int>>&types){
-        if( sa<=0 && sb<=0){
+    double helper(vector<vector<double>>& dp, int sa, int sb, vector<vector<double>>& types) {
+        if (sa <= 0 && sb <= 0) {
             return 0.5;
         }
-        if(sa<=0){
+        if (sa <= 0) {
             return 1.0;
         }
-        if(sa>=0 && sb<=0){
+        if (sb <= 0) {
             return 0.0;
         }
-        
-        if(dp[sa][sb]!=-1.0){
+
+        if (dp[sa][sb] != -1.0) {
             return dp[sa][sb];
         }
-        double ans=0.0;
 
-        for(int i=0;i<4;i++){
-           
-               ans+=helper(dp,sa-types[i][0],sb-types[i][1],types);
+        double ans = 0.0;
+        for (int i = 0; i < 4; i++) {
+            ans += helper(dp, sa - types[i][0], sb - types[i][1], types);
         }
-        return dp[sa][sb]=ans*0.25;
+
+        return dp[sa][sb] = ans * 0.25;
     }
+
     double soupServings(int n) {
-        //in first though looks of dp can take hint from  khight probability problem
-        int max=n/25;
-       if(n>10000) return 1;
-        vector<vector<double>>dp(n+1,vector<double>(n+1,-1.0));
-        vector<vector<int>>types={{100,0},{75,25},{50,50},{25,75}};
-       double val= helper(dp,n,n,types);
-        return val;
+        if (n >= 5000) return 1.0;
+
+        int maxServe = (n + 24) / 25;
+        vector<vector<double>> dp(maxServe + 1, vector<double>(maxServe + 1, -1.0));
+        vector<vector<double>> types = {{4, 0}, {3, 1}, {2, 2}, {1, 3}};
+
+        double probability = helper(dp, maxServe, maxServe, types);
+        return probability;
     }
 };
