1ca64d40a92393b2f4d8233c4e70b18a460976be
harshleet <122361301+harshleet@users.noreply.github.com>
2023-08-01 08:46:21 +0530

    Time: 111 ms (19.53%) | Memory: 8.9 MB (96.01%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-void rec(vector<int>&ds,int n,int& k,map<int,bool>&m,set<vector<int>>&s){
+void rec(vector<int>&ds,int n,int& k,vector<vector<int>>&s){
     if(ds.size()==k){
-        s.insert(ds);
+        s.push_back(ds);
         return;
     }
     for(int i=1;i<=n;i++){
         if(ds.empty()|| i>ds.back()){
-        if(m[i]==false ){
-        m[i]=true;
+    
+      
         ds.push_back(i);
-        rec(ds,n,k,m,s);
+        rec(ds,n,k,s);
         ds.pop_back();
-        m[i]=false;}}
+        }
     }
 }
     vector<vector<int>> combine(int n, int k) {
          vector<int>ds;
-        map<int,bool>m;
-        for(int i=1;i<=n;i++){
-            m[i]=false;
-        }
+       
      
-       set<vector<int>>s;
-        rec(ds,n,k,m,s);
-          vector<vector<int>>ans(s.begin(),s.end());
-        return ans;
+       vector<vector<int>>s;
+        rec(ds,n,k,s);
+         
+        return s;
     }
 };
