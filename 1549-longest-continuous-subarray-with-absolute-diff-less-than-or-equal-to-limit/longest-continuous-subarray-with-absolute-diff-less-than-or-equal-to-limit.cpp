533f9e66bb26da51fb943b4dd9239598d776dff0
harshleet <122361301+harshleet@users.noreply.github.com>
2023-09-06 10:23:41 +0530

    Time: 153 ms (56.66%) | Memory: 78.6 MB (21.29%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        int j=0;
-        map<int,int>m;
-       int maxi=0;
-        for(int i=0;i<nums.size();i++){
-               m[nums[i]]++;
-              if(abs(m.begin()->first-m.rbegin()->first)<=limit ){
-                maxi=max(maxi,i-j+1);
-              }
-             else{ 
-                 while(abs(m.begin()->first-m.rbegin()->first)>limit){
-                 m[nums[j]]--;
-                 if(m[nums[j]]==0){
-                     m.erase(nums[j]);
-                 }
-                 j++;
-              }
-              }
+        int n = nums.size();
+        int maxi = 0;
+        multiset<int> m;
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            m.insert(nums[right]);
+
+            while (*m.rbegin() - *m.begin() > limit) {
+                m.erase(m.find(nums[left]));
+                left++;
+            }
+
+            maxi = max(maxi, right - left + 1);
         }
+
         return maxi;
     }
 };
