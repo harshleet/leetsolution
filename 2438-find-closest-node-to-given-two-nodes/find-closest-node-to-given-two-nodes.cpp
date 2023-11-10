1eae493fab6944e62de61511ba03f0c32b56207b
harshleet <122361301+harshleet@users.noreply.github.com>
2023-11-10 12:54:50 +0530

    Time: 254 ms (40.40%) | Memory: 139.5 MB (31.20%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    void bfs(int node, vector<int>& dis, vector<int> adj[]) {
+    vector<int> bfs(int node,int n,  vector<int> adj[]) {
+        vector<int>dis(n, 1e8);
         queue<int> q;
-        vector<int>vis(dis.size(),0);
+        vector<int>vis(n,0);
         q.push(node);
         dis[node] = 0;
         vis[node]=1;
@@ -17,6 +18,7 @@ public:
                 }
             }
         }
+        return dis;
     }
 
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
@@ -26,10 +28,9 @@ public:
             if(edges[i]!=-1)
               adj[i].push_back(edges[i]);
         }
-        vector<int> dis(n, 1e8);
-        vector<int> dis2(n, 1e8);
-        bfs(node1, dis, adj);
-        bfs(node2, dis2, adj);
+    
+        vector<int> dis= bfs(node1,n,adj);
+        vector<int> dis2=bfs(node2,n, adj);
         int mini = INT_MAX;
         int ans = -1;
         for (int i = 0; i < n; i++) {
