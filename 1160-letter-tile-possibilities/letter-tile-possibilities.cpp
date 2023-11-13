706ee747b91d516d3e6a07f1fce56da3947e50b1
harshleet <122361301+harshleet@users.noreply.github.com>
2023-11-13 22:16:13 +0530

    Time: 6 ms (75.74%) | Memory: 6.3 MB (81.52%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-
-    int helper(string s,map<char,int>&m,string& tiles,int len,set<string>&st){
-         if(len>=tiles.size()){
-             if(s.size()!=0)
-                st.insert(s);
-                return 1;
-         }
-         int np=helper(s,m,tiles,len+1,st);
-         int p=0;
-         for(int i=0;i<tiles.size();i++){
-             if(m[tiles[i]]!=0){
-                 m[tiles[i]]--;
-                 p+=helper(s+tiles[i],m,tiles,len+1,st);
-                 m[tiles[i]]++;
-             }
-         }
-         return np+p;
-    }
     int numTilePossibilities(string tiles) {
-        //proper backtracking question
-        map<char,int>m;
-        for(int i=0;i<tiles.size();i++){
-            m[tiles[i]]++;
+        map<char, int> m;
+        for (char ch : tiles) {
+            m[ch]++;
         }
-        set<string>st;
-        helper("",m,tiles,0,st);
-        return st.size();
 
+        int result = 0;
+        backtrack(m, result);
+        return result;
+    }
+
+    void backtrack(map<char, int>& m, int& result) {
+        for (auto& kvp : m) {
+            if (kvp.second > 0) {
+                result++; // Increment result for each unique combination
+
+                kvp.second--; // Choose the current character
+                backtrack(m, result);
+                kvp.second++; // Backtrack to explore other possibilities
+            }
+        }
     }
 };
