a049b0a455b481d38471d8102d62846107226fa9
harshleet <122361301+harshleet@users.noreply.github.com>
2023-11-15 09:23:39 +0530

    Time: 74 ms (77.61%) | Memory: 51.6 MB (33.96%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
-    bool check(int mid,vector<int>& arr){
-
+   
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        
+        sort(arr.begin(),arr.end());
         arr[0]=1;
         for(int i=1;i<arr.size();i++){
-            if(abs(arr[i]-arr[i-1])>1){
-                 if(arr[i]>arr[i-1]+1){
-                     arr[i]=arr[i-1]+1;
-                 }
-            }
-            if(arr[i]==mid){
-                return true;
+            if(arr[i]-arr[i-1]>1){
+                arr[i]=arr[i-1]+1;
             }
         }
-        return false;
-    }
-    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        if(arr.size()==1){
-            return 1;
-        }
-        if(arr.size()==2){
-            if(arr.back()>1){
-                return 2;
-            }
-            return 1;
-        }
-        sort(arr.begin(),arr.end());
-        int low=1,high=arr.back();
-
-        while(low<=high){
-            int mid=(low+high)/2;
-
-            if(check(mid,arr)){
-                  low=mid+1;
-            }else{
-                 high=mid-1;
-            }
-        }
-        return low-1;
+        return arr.back();
+       
     }
 };
