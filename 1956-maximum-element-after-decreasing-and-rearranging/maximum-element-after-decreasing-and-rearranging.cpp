dbd31981968b69b4e5469156fd56621e410d7fba
harshleet <122361301+harshleet@users.noreply.github.com>
2023-11-15 09:22:51 +0530

    Time: 146 ms (5.22%) | Memory: 51.5 MB (56.72%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
-    // bool check(int mid,vector<int>& arr){
-    //     arr[0]=1;
-    //     for(int i=1;i<arr.size();i++){
+    bool check(int mid,vector<int>& arr){
 
-    //         if(abs(arr[i]-arr[i-1])>1){
-    //                  arr[i]=arr[i-1]+1;
-    //         }
-    //         if(arr[i]==mid){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
+        arr[0]=1;
+        for(int i=1;i<arr.size();i++){
+            if(abs(arr[i]-arr[i-1])>1){
+                 if(arr[i]>arr[i-1]+1){
+                     arr[i]=arr[i-1]+1;
+                 }
+            }
+            if(arr[i]==mid){
+                return true;
+            }
+        }
+        return false;
+    }
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         if(arr.size()==1){
             return 1;
         }
+        if(arr.size()==2){
+            if(arr.back()>1){
+                return 2;
+            }
+            return 1;
+        }
         sort(arr.begin(),arr.end());
-        arr[0]=1;
-        for(int i=1;i<arr.size();i++){
-            if(arr[i]-arr[i-1]>1){
-                arr[i]=arr[i-1]+1;
+        int low=1,high=arr.back();
+
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(check(mid,arr)){
+                  low=mid+1;
+            }else{
+                 high=mid-1;
             }
         }
-        return arr.back();
-       
+        return low-1;
     }
 };
