398d9c3e1a726bd327965400ad49969bac27196b
harshleet <122361301+harshleet@users.noreply.github.com>
2023-12-19 12:11:35 +0530

    Time: 4 ms (23.50%) | Memory: 7.5 MB (24.01%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-int helper(string&s,vector<int>&dp,int ind,map<string,char>&m){
-    if(ind==s.size()){
-        return 1;
-    }
-    if(dp[ind]!=-1){
-        return dp[ind];
-    }
-    int take=0;
-
-    if(m.find(s.substr(ind,1))!=m.end()){
-        take+=helper(s,dp,ind+1,m);
-    }
-    
-     if(ind + 1 < s.size() && m.find(s.substr(ind,2))!=m.end()){
-        take+=helper(s,dp,ind+2,m);
-    }
-  
-    return dp[ind]=take;
-}
-    int numDecodings(string s) {
-        vector<int>dp(s.size(),-1);
-        map<string,char>m;
-        for(int i=1;i<=26;i++){
-            string a=to_string(i);
-           m[a]='A'+i-1;
+    int helper(int ind,string &s,vector<int>&dp,map<string,char>&m){
+        if(ind==s.length()){
+            return 1;
+        }
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        int t=0;
+        if(m.find(s.substr(ind,1))!=m.end()){
+            t+=helper(ind+1,s,dp,m);
         }
 
-       return  helper(s,dp,0,m);
+        if(ind<s.length()-1 && m.find(s.substr(ind,2))!=m.end() ){
+            t+=helper(ind+2,s,dp,m);
+        }
+        return dp[ind]=t;
+    }
+    int numDecodings(string s) {
+       map<string,char>m;
+        vector<int>dp(s.length(),-1);
+       for(int i=1;i<=26;i++){
+           m[to_string(i)]='A'+i-1;
+       }
+      
+       return helper(0,s,dp,m);
     }
 };
