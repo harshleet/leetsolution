61c328a5ea98ec6958e874c8c65d54683c843f21
harshleet <122361301+harshleet@users.noreply.github.com>
2023-12-26 15:04:08 +0530

    Time: 670 ms (20.53%) | Memory: 117.5 MB (16.98%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -5,14 +5,14 @@ public:
         priority_queue<tuple<double,int,int>>pq;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                pq.push({-double(arr[i]/(double(arr[j]))),-arr[i],-arr[j]});
+                pq.push({-double(arr[i]/(double(arr[j]))),arr[i],arr[j]});
             }
         }
         vector<int> ans;
         int i=0;
         while(!pq.empty()&& i<k){
            
-              ans={-get<1>(pq.top()), -get<2>(pq.top())};
+              ans={get<1>(pq.top()), get<2>(pq.top())};
               pq.pop();
               i++;
         }
