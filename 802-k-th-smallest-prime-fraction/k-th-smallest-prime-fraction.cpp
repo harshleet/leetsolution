8daf487ed221deaa123c5a686c2a4d2f9ae43b77
harshleet <122361301+harshleet@users.noreply.github.com>
2023-12-26 15:03:41 +0530

    Time: 666 ms (20.53%) | Memory: 117.5 MB (19.11%) - LeetSync

---
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        int n=arr.size();
+        priority_queue<tuple<double,int,int>>pq;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                pq.push({-double(arr[i]/(double(arr[j]))),-arr[i],-arr[j]});
+            }
+        }
+        vector<int> ans;
+        int i=0;
+        while(!pq.empty()&& i<k){
+           
+              ans={-get<1>(pq.top()), -get<2>(pq.top())};
+              pq.pop();
+              i++;
+        }
+        return ans;
+    }
+};
