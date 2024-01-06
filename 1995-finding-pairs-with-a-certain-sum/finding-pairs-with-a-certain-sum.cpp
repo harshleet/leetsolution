d918822b69a81b8e8f0f41d433e928e4059f4731
harshleet <122361301+harshleet@users.noreply.github.com>
2024-01-06 15:56:59 +0530

    Time: 151 ms (96.86%) | Memory: 74.1 MB (76.23%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -1,24 +1,37 @@
 class FindSumPairs {
 public:
-   vector<int> b;
-    map<int,int> first;
-    unordered_map<int,int>second;
+    unordered_map<int,int>m1;
+    unordered_map<int,int>m2;
+
+    vector<int>temp2;
     FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
-        b = nums2;
-        for(int x : nums2)second[x]++;
-        for(int x : nums1)first[x]++;
+         for(int i=0;i<nums1.size();i++){
+             m1[nums1[i]]++;
+         } 
+          for(int i=0;i<nums2.size();i++){
+             m2[nums2[i]]++;
+         } 
+       
+         temp2=nums2;
     }
     
     void add(int index, int val) {
-        second[b[index]]--;
-        b[index]+=val;
-        second[b[index]]++;
+        m2[temp2[index]]--;
+        if(m2[temp2[index]]==0){
+            m2.erase(temp2[index]);
+        }
+        temp2[index]+=val;
+        m2[temp2[index]]++;
     }
     
     int count(int tot) {
-        int ans = 0;
-        for(auto it = first.begin(); it != first.end() && it->first<tot ; it++)
-        if(second.count(tot-it->first)) ans += it->second * second[tot-it->first];
+        int ans=0;
+        for(auto it:m1){
+            int a=it.second;
+            if(m2.find(tot-it.first)!=m2.end()){
+                ans+=m2[tot-it.first]*a;
+            }
+        }
         return ans;
     }
 };
