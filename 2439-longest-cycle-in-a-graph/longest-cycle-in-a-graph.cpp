033240e66b010eaa32f0d6c22d2955afcb7212db
harshleet <122361301+harshleet@users.noreply.github.com>
2024-01-11 22:25:47 +0530

    Time: 323 ms (36.75%) | Memory: 169.6 MB (32.77%) - LeetSync

---
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+int maxi=-1;
+    void dfs(int node,vector<int>adj[],vector<int>&vis,vector<int>&len,int lvl,vector<int>&pvis){
+         vis[node]=1;
+               len[node]=lvl;
+               pvis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+               
+               dfs(it,adj,vis,len,lvl+1,pvis);
+                
+            }else if(pvis[it]){
+                    maxi=max(maxi,lvl+1-len[it]);
+             }
+        }
+       pvis[node]=0;
+      
+    }
+    int longestCycle(vector<int>& edges) {
+        
+        int n=edges.size();
+        vector<int>adj[n];
+        for(int i=0;i<n;i++){
+            if(edges[i]!=-1)
+               adj[i].push_back(edges[i]);
+        }
+        vector<int>vis(n,0);
+        vector<int>len(n,0);
+        vector<int>pvis(n,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,adj,vis,len,0,pvis);
+            }
+        }
+        return maxi;
+    }
+};
