c1396755694479a7b5b176a0775e1b8abe91e03c
harshleet <122361301+harshleet@users.noreply.github.com>
2024-01-18 23:06:32 +0530

    Time: 479 ms (5.02%) | Memory: 6.4 MB (48.40%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int helper(int ind,map<int,bool>&m,vector<int>&ds,int& n){
-        if(ds.size()==n){
+        if(ind==n+1){
             return 1;
         }
         int a=0;
         for(int i=1;i<=n;i++){
            if(m[i]==false && (i%ind==0 || ind%i==0)){
                m[i]=true;
-               ds.push_back(i);
                a+=helper(ind+1,m,ds,n);
                m[i]=false;
-               ds.pop_back();
                
            }
         }
