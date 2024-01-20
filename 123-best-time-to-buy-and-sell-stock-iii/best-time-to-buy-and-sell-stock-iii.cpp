5bb7dd590466ca4049b30e2e68fe59dc96accfe9
harshleet <122361301+harshleet@users.noreply.github.com>
2024-01-20 19:15:12 +0530

    Time: 472 ms (48.30%) | Memory: 222 MB (6.99%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+   int helper(int ind,vector<int>&prices, vector<vector<vector<int>>>&dp,bool pur,int lim){
+        if(ind>=prices.size() || lim==0){
+            if(pur==0){
+                return 0;
+            }
+            return -1e8;
+        }
+        if(dp[ind][pur][lim]!=-1){
+            return dp[ind][pur][lim];
+        }
+       
+        int ntake=helper(ind+1,prices,dp,pur,lim);
+        
+        if(pur==1){
+              return dp[ind][pur][lim]=max(ntake,prices[ind]+helper(ind+1,prices,dp,0,lim-1));
+        }
+        return dp[ind][pur][lim]=max(ntake,-prices[ind]+helper(ind+1,prices,dp,1,lim));
+    }
+    int maxProfit(vector<int>& prices) {
+          int n=prices.size();
+          int lim=2;
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(lim+1,-1)));
+        return helper(0,prices,dp,0,lim);
+    }
+};
