f5eed06cd7a266151eb27292fc298dcb1e9516ad
harshleet <122361301+harshleet@users.noreply.github.com>
2024-01-30 23:31:06 +0530

    Time: 36 ms (62.71%) | Memory: 38.2 MB (34.63%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    bool helper(int ind,vector<int>&dp,vector<int>& arr){
-        if(ind>=arr.size() || ind<0){
-            return false;
-        }
+    bool helper(int ind,vector<int>&arr,vector<int>&dp){
         if(arr[ind]==0){
             return true;
         }
-        
         if(dp[ind]!=-1){
             return dp[ind];
         }
-        dp[ind]=false;
-        return dp[ind]=helper(ind-arr[ind],dp,arr)||helper(ind+arr[ind],dp,arr);
+        bool ans=false;
+        dp[ind]=ans;
+        if(ind-arr[ind]>=0){
+            ans=ans||helper(ind-arr[ind],arr,dp);
+        }
+
+        if(ind+arr[ind]<arr.size()){
+            ans=ans||helper(ind+arr[ind],arr,dp);
+        }
+        return dp[ind]=ans;
     }
     bool canReach(vector<int>& arr, int start) {
         vector<int>dp(arr.size(),-1);
-        return helper(start,dp,arr);
+        return helper(start,arr,dp);
     }
 };
