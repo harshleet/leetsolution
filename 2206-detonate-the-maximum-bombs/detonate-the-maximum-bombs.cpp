e3099947494fdcf2dc405d5faff4447b20a80b7b
harshleet <122361301+harshleet@users.noreply.github.com>
2024-02-02 00:26:07 +0530

    Time: 376 ms (21.76%) | Memory: 18.2 MB (70.30%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.cpp
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    void dfs(int ind,vector<vector<int>>& bombs,vector<int>&vis,int &cnt){
-       
+    int dfs(int ind,vector<vector<int>>& bombs,vector<int>&vis){
         vis[ind]=1;
         int r1=bombs[ind][2];
-       cnt++;
+        int cnt=1;
         for(int i=0;i<bombs.size();i++){
-          long long dis=pow(bombs[i][0]-bombs[ind][0],2)+pow(bombs[i][1]-bombs[ind][1],2);
-           
-            if(!vis[i] && i!=ind && dis<=pow(r1,2)){
-               dfs(i,bombs,vis,cnt);
+            long long dis=pow(bombs[i][0]-bombs[ind][0],2)+pow(bombs[i][1]-bombs[ind][1],2);
+            long long dis2= pow(r1,2);
+            if(!vis[i] &&dis<=dis2){
+               cnt+=dfs(i,bombs,vis);
             }
         }
+  
+        return cnt;
     }
     int maximumDetonation(vector<vector<int>>& bombs) {
         int n=bombs.size();
         
         int maxi=0;
         for(int i=0;i<n;i++){
-                vector<int>vis(n,0);
-                int cnt=0;
-                dfs(i,bombs,vis,cnt);
-                maxi=max(maxi,cnt);
+            vector<int>vis(n,0);
+                maxi=max(maxi,dfs(i,bombs,vis));
         }
         return maxi;
     }
