27981c64918ccaa56142e98d82c0d8d3ef5f2150
harshleet <122361301+harshleet@users.noreply.github.com>
2024-02-01 23:20:32 +0530

    Time: 21 ms (70.55%) | Memory: 18 MB (22.18%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,32 +1,58 @@
 class Solution {
 public:
-     void dfs(int node,vector<int>adj[],vector<int>&vis){
-         vis[node]=1;
-         for(auto it:adj[node]){
-             if(!vis[it]){
-                dfs(it,adj,vis);
-            }
-         }
-     }
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
     int findCircleNum(vector<vector<int>>& isConnected) {
+    
         int n=isConnected.size();
-        vector<int>adj[n];
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(isConnected[i][j]){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+            DisjointSet ds(n);
+        for(int i=0;i<isConnected.size();i++){
+            for(int j=0;j<isConnected[0].size();j++){
+                if(isConnected[i][j]==1){
+                    ds.unionBySize(i,j);
                 }
             }
         }
-        int c=0;
-         vector<int>vis(n,0);
+        int cnt=0;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                c++;
-                dfs(i,adj,vis);
+            if(ds.findUPar(i)==i){
+                cnt++;
             }
         }
-        return c;
+        return cnt;
+
     }
 };
