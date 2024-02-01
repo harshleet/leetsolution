e57702b54e11d1526fac9e3b0efe2803c0ced1ce
harshleet <122361301+harshleet@users.noreply.github.com>
2024-02-02 01:02:23 +0530

    Time: 64 ms (99.55%) | Memory: 41.6 MB (77.12%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,75 @@
+  class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int cnt=0;
+        for(int i=0;i<connections.size();i++){
+            if(ds.findUPar(connections[i][0])==ds.findUPar(connections[i][1])){
+                cnt++;
+            }else{
+                ds.unionBySize(connections[i][0],connections[i][1]);
+            }
+            
+        }
+        int ans=cnt;
+        for(int i=0;i<n;i++){
+            if(ds.findUPar(i)==i){
+                cnt--;
+            }
+        }
+        if(cnt>=-1){
+            return ans-cnt-1;
+        }
+        return -1;
+
+    }
+};
