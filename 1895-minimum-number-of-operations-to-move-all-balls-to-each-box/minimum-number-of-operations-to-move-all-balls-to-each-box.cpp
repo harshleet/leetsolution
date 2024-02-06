83a0f80f45952c2f9e123dabffe91738738be8f6
harshleet <122361301+harshleet@users.noreply.github.com>
2024-02-06 10:45:45 +0530

    Time: 9 ms (72.29%) | Memory: 11.1 MB (5.84%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-         vector<int>ans;
-        for(int j=0;j<boxes.length();j++){
+         vector<int>ans(boxes.length(),0);
+         pair<int,int>p={0,0};
+         pair<int,int>s={0,0};
+         vector<pair<int,int>>pre(boxes.length());
+         vector<pair<int,int>>suf(boxes.length());
+         int n=boxes.size();
+        for(int i=0;i<boxes.length();i++){
+            if(boxes[i]=='1'){
+               p.first+=i;
+               p.second++;
+            }
+            if(boxes[n-1-i]=='1'){
+                s.first+=(n-1-i);
+               s.second++;
+            }
+           
+            pre[i]=p;
+            suf[n-1-i]=s;
+
+        }
+       
+         for(int i=0;i<boxes.length();i++){
+           
+               ans[i]+=abs(pre[i].second*i-pre[i].first);
+               ans[i]+=abs(suf[i].second*i-suf[i].first);
             
-                int m=0;
-           for(int i=0;i<boxes.length();i++)
-           {
-              if(boxes[i]=='1')
-              {
-                    m+=abs(j-i);
-              }
-           }
-           ans.push_back(m);
         }
         return ans;
     }
