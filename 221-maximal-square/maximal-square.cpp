709443a0419a8e665ed563c43a1a304ce83e3c81
harshleet <122361301+harshleet@users.noreply.github.com>
2024-02-17 11:35:54 +0530

    Time: 62 ms (76.77%) | Memory: 24.4 MB (39.80%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
- int maxi=0;
-    int helper(int row,int col,vector<vector<int>>&dp,vector<vector<char>>& matrix){
-        if(row>=matrix.size()|| col>=matrix[0].size() ){
-            return 0;
-        }
-        
+    vector<int>drow={1,1,0};
+    vector<int>dcol={1,0,1};
+    int maxi=0;
+    int dfs(int row,int col,vector<vector<int>>&dp,int n,int m,vector<vector<char>>& matrix){
         if(dp[row][col]!=-1){
             return dp[row][col];
         }
-        int drow[]={0,1,1};
-        int dcol[]={1,0,1};
+       
         int mini=1e8;
         for(int i=0;i<3;i++){
             int nrow=row+drow[i];
             int ncol=col+dcol[i];
-            mini=min(mini,helper(nrow,ncol,dp,matrix));
+            if(nrow>=0 && ncol>=0 && nrow<n && ncol<m){
+                mini=min(mini,dfs(nrow,ncol,dp,n,m,matrix));
+            }else{
+                 mini=min(mini,0);
+            }
         }
-          if(matrix[row][col]=='0'){
-                return dp[row][col]=0;
+         if(matrix[row][col]=='0'){
+            return dp[row][col]=0;
         }
-        
         maxi=max(maxi,mini+1);
         return dp[row][col]=mini+1;
     }
     int maximalSquare(vector<vector<char>>& matrix) {
-        vector<vector<int>>dp(matrix.size(),vector<int>(matrix[0].size(),-1));       
-        helper(0,0,dp,matrix);
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        dfs(0,0,dp,n,m,matrix);
+        if(maxi>=1e8){
+            return 1;
+        }
         return maxi*maxi;
     }
 };
