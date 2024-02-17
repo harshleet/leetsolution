2d7f6c1595200d87648cd1bd3f08ad23e1043474
harshleet <122361301+harshleet@users.noreply.github.com>
2024-02-17 11:58:02 +0530

    Time: 3 ms (62.76%) | Memory: 19.8 MB (6.00%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+vector<int>d={1,7,30};
+    int helper(int ind,int last,vector<int>&days,vector<int>&costs, vector<vector<int>>&dp){
+        if(ind>=days.size()){
+            return 0;
+        }
+        if(dp[ind][last+1]!=-1){
+            return dp[ind][last+1];
+        }
+        if(last>=days[ind]){
+            return dp[ind][last+1]=helper(ind+1,last,days,costs,dp);    
+        }
+        int mini=1e8;
+        for(int i=0;i<3;i++){
+           mini=min(mini,costs[i]+helper(ind+1,days[ind]+d[i]-1,days,costs,dp));
+        }
+        return dp[ind][last+1]=mini;
+
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        sort(days.begin(),days.end());
+        vector<vector<int>>dp(days.size(),vector<int>(days.back()+31,-1));
+        return helper(0,-1,days,costs,dp);
+    }
+};
