4a4b3758e35de7b3b706335ef14ab4d76e04a759
harshleet <122361301+harshleet@users.noreply.github.com>
2024-02-20 23:27:38 +0530

    Time: 9 ms (31.15%) | Memory: 14.8 MB (22.16%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.cpp
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-void helper(vector<string>&ans,string&ds,string&s,int ind,map<int,bool>&m){
-    if(ind==s.size()){
-      ans.push_back(ds);
-      return;
-    }
-   
-        if(isalpha(s[ind])){
-       
-        ds.push_back(tolower(s[ind]));
-    
-        helper(ans,ds,s,ind+1,m);
-        ds.pop_back();
-      
-        ds.push_back(toupper(s[ind]));
-       
-        helper(ans,ds,s,ind+1,m);
-         ds.pop_back();
-     
-        }else {
-            ds.push_back(s[ind]);
-            helper(ans, ds, s, ind+1, m);
-            ds.pop_back();
+void helper(string ds,int ind,vector<string>&ans,string s){
+    if(ind>=s.size()){
+        if(!ds.empty()){
+            ans.push_back(ds);
         }
-   
+        return;
+    }
+    if(isalpha(s[ind])){
+        if(isupper(s[ind])){
+              ds.push_back(tolower(s[ind]));
+              helper(ds,ind+1,ans,s);
+              ds.pop_back();
+        }else if(islower(s[ind])){
+              ds.push_back(toupper(s[ind]));
+              helper(ds,ind+1,ans,s);
+              ds.pop_back();
+        }
+    }
+        ds.push_back(s[ind]);
+        helper(ds,ind+1,ans,s);
+    
 }
     vector<string> letterCasePermutation(string s) {
-        vector<string>ans;
-        string ds="";
-        map<int,bool>m;
-        helper(ans,ds,s,0,m);
-        return ans;
-     //easy question make not make backtracking   
+       vector<string>ans;
+       helper("",0,ans,s);
+       return ans;
     }
 };
