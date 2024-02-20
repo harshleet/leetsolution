df522353b2bba21fe4523221cf9352a956554bfd
harshleet <122361301+harshleet@users.noreply.github.com>
2024-02-20 15:44:53 +0530

    Time: 85 ms (91.26%) | Memory: 61.6 MB (76.10%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-void rec(vector<int>&ds,int n,int& k,vector<vector<int>>&s){
-    if(ds.size()==k){
-        s.push_back(ds);
-        return;
-    }
-    for(int i=1;i<=n;i++){
-        if(ds.empty()|| i>ds.back()){
-    
-      
-        ds.push_back(i);
-        rec(ds,n,k,s);
-        ds.pop_back();
-        }
-    }
-}
+   void helper(int n,int k,vector<int>&ds,vector<vector<int>>&ans,int st){
+       if(ds.size()==k){
+           ans.push_back(ds);
+           return;
+       }
+       for(int i=st;i<=n;i++){
+           if(ds.size()<k){
+               ds.push_back(i);
+               helper(n,k,ds,ans,i+1);
+               ds.pop_back();
+           }
+       }
+   }
     vector<vector<int>> combine(int n, int k) {
-         vector<int>ds;
-       
-     
-       vector<vector<int>>s;
-        rec(ds,n,k,s);
-         
-        return s;
+        vector<vector<int>>ans;
+        vector<int>ds;
+        helper(n,k,ds,ans,1);
+        return ans;
+
     }
 };
