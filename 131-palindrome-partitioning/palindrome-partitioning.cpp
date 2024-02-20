1a6ac0ee36e740e11a6c0e26cd201875d5cb6589
harshleet <122361301+harshleet@users.noreply.github.com>
2024-02-20 18:29:18 +0530

    Time: 65 ms (96.85%) | Memory: 52.7 MB (82.20%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-     bool isPalindrome(string &temp){
-         for(int i=0;i<temp.size()/2;i++){
-             if(temp[i]!=temp[temp.size()-1-i]){
-                 return false;
+bool isPalindrome(string &s){
+    for(int i=0;i<s.size()/2;i++){
+        if(s[i]!=s[s.size()-1-i]){
+            return false;
+        }
+    }
+    return true;
+}
+    void helper(int ind,string &s,vector<string>&ds,vector<vector<string>>&ans){
+        if(ind>=s.size()){
+            if(!ds.empty()){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        for(int i=1;i<=s.size()-ind;i++){
+             string st=s.substr(ind,i);
+             if(isPalindrome(st)){
+                 ds.push_back(st);
+                 helper(ind+i,s,ds,ans);
+                 ds.pop_back();
              }
-         }
-         return true;
-     }
-    void helper(int ind,string s, vector<string>&ds,vector<vector<string>>&ans){
-         if(ind>=s.size()){
-             if(!ds.empty()){
-                 ans.push_back(ds);
-             }
-             return;
-         }
-
-         for(int i=1;i<=s.size()-ind;i++){
-             string temp=s.substr(ind,i);
-             if(isPalindrome(temp)){
-                    ds.push_back(temp);
-                    helper(ind+i,s,ds,ans);
-                    ds.pop_back();
-             }
-         }
+        }
     }
     vector<vector<string>> partition(string s) {
-       vector<vector<string>>ans;
-       vector<string>ds;
-     helper(0,s,ds,ans);
-     return ans;
+        vector<vector<string>>ans;
+        vector<string>ds;
+        helper(0,s,ds,ans);
+        return ans;
     }
 };
