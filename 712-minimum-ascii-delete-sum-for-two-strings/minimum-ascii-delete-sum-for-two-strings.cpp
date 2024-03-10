7c68ca7910d4ea2afbf0231086668b568fdaf508
harshleet <122361301+harshleet@users.noreply.github.com>
2024-03-10 22:57:44 +0530

    Time: 33 ms (41.00%) | Memory: 16.6 MB (40.24%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    int minimumDeleteSum(string s1, string s2) {
-          int n = s1.size();
-  int m = s2.size();
+    int getSumOfAscii(const std::string& inputString) {
+        int asciiSum = 0;
 
-  vector < vector < int >> dp(n + 1, vector < int > (m + 1, 0));
-  for (int i = 0; i <= n; i++) {
-    dp[i][0] = 0;
-  }
-  for (int i = 0; i <= m; i++) {
-    dp[0][i] = 0;
-  }
+        // Iterate over each character in the string
+        for (char c : inputString) {
+            // Add the ASCII value of the character to the sum
+            asciiSum += static_cast<int>(c);
+        }
 
-  for (int ind1 = 1; ind1 <= n; ind1++) {
-    for (int ind2 = 1; ind2 <= m; ind2++) {
-      if (s1[ind1 - 1] == s2[ind2 - 1])
-        dp[ind1][ind2] = s1[ind1-1] + dp[ind1 - 1][ind2 - 1];
-      else
-        dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+        return asciiSum;
     }
-  }
+    int f(string& s1,string& s2,int i,int j,vector<vector<int>>&dp){
+           if(i<0 || j<0){
+               return 0;
+           }
+           if(dp[i][j]!=-1){
+               return dp[i][j];
+           }
+           int ntake=max(f(s1,s2,i-1,j,dp),f(s1,s2,i,j-1,dp));
+           int take=0;
+           if(s1[i]==s2[j]){
+               take=s1[i]+f(s1,s2,i-1,j-1,dp);
+           }
+           return dp[i][j]=max(ntake,take);
 
-
-
-      int p=0;
-      for(int i=0;i<n;i++){
-        p+=int(s1[i]);
-      }
-      int q=0;
-      for(int i=0;i<m;i++){
-        q+=int(s2[i]);
-      }
-  
-      return p+q-2*dp[n][m];
+    }
+    int minimumDeleteSum(string s1, string s2) {
+        vector<vector<int>>dp(s1.length(),vector<int>(s2.length(),-1));
+        int lcs=f(s1,s2,s1.size()-1,s2.size()-1,dp);
+        cout<<lcs<<endl;
+        int val=getSumOfAscii(s1)+getSumOfAscii(s2)-2*lcs;
+        return val;
     }
 };
