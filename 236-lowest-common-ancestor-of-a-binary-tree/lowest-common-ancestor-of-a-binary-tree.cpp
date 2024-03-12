0b681f888a5dfe7985665de353e32b3d2f9797a6
harshleet <122361301+harshleet@users.noreply.github.com>
2024-03-12 13:53:03 +0530

    Time: 11 ms (73.40%) | Memory: 16.2 MB (21.96%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,45 +9,31 @@
  */
 class Solution {
 public:
-    bool search(TreeNode* root,TreeNode* tar){
+    TreeNode* helper(TreeNode* root, TreeNode* p, TreeNode* q){
         if(root==NULL){
-            return false;
-        }
-        bool a=false;
-        if(root->val==tar->val){
-          a=true;
-        }
-        return a|search(root->left,tar)|search(root->right,tar);
-    }
-    TreeNode* ans=NULL;
-   TreeNode* helper(TreeNode* root,TreeNode* p,TreeNode* q){
-        if(root==NULL){
-            return NULL; 
+            return NULL;
         }
         if(root->val==p->val){
             return p;
         }
-          if(root->val==q->val){
+        if(root->val==q->val){
             return q;
         }
-        TreeNode* ls=helper(root->left,p,q);
-        TreeNode* rs=helper(root->right,p,q);
-        if(ls==NULL &&  rs==NULL){
+        TreeNode* l=helper(root->left,p,q);
+        TreeNode* r=helper(root->right,p,q);
+        if(l==NULL && r==NULL){
             return NULL;
         }
-        else if(ls!=NULL && rs!=NULL){
+        if(l!=NULL && r!=NULL){
             return root;
         }
-        else if(ls!=NULL && rs==NULL){
-            return ls;
+        if(l==NULL){
+            return r;
         }
-        else{
-            return rs;
-        }
+        return l;
         
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-       return helper(root,p,q);
-       
+        return helper(root,p,q);
     }
 };
