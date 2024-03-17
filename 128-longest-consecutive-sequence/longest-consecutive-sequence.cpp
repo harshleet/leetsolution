200cffab1fdf90d22308e85f36a845efa212185e
harshleet <122361301+harshleet@users.noreply.github.com>
2024-03-17 21:53:27 +0530

    Time: 195 ms (35.44%) | Memory: 84.2 MB (5.01%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,60 +1,56 @@
-class DisjointSet {
-    vector<int> rank, parent, size;
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
+class DisjointSet{
+    vector<int>parent,size;
+    public:
+        DisjointSet(int n){
+            parent.resize(n+1);
+            size.resize(n+1,0);
+            for(int i=0;i<=n;i++){
+                parent[i]=i;
+                size[i]=1;
+            }
         }
-    }
 
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-   
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
+        int findUPar(int node){
+            if(node==parent[node]){
+                return node;
+            }
+            return parent[node]=findUPar(parent[node]);
         }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+        void unionBySize(int u,int v){
+            int uPar=findUPar(u);
+            int vPar=findUPar(v);
+            if(size[uPar]<=size[vPar]){
+                parent[uPar]=vPar;
+                size[vPar]+=size[uPar];
+            }else{
+                 parent[vPar]=uPar;
+                size[uPar]+=size[vPar];
+            }
         }
-    }
 };
-
-
 class Solution {
 public:
-    int longestConsecutive(vector<int>& num) {
-       
-        set<int>s(num.begin(),num.end());
-       vector<int>nums(s.begin(),s.end());
-       DisjointSet ds(nums.size());
-       int n=nums.size();
-       for(int i=1;i<n;i++){
-          if(nums[i]-nums[i-1]==1){
-              ds.unionBySize(i,i-1);
-          }
-       } 
-       unordered_map<int,int>m;
-       int maxi=0;
-       for(int i=0;i<n;i++){
-         int par=ds.findUPar(i);
-         m[par]++;
-         maxi=max(maxi,m[par]);
-       }
-       return maxi;
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==0){
+            return 0;
+        }
+        int maxi=*max_element(nums.begin(),nums.end());
+          set<int>st(nums.begin(),nums.end());
+        DisjointSet ds(nums.size());
+        vector<int>num(st.begin(),st.end());
+        for(int i=1;i<num.size();i++){
+            if(num[i]-num[i-1]==1){
+                ds.unionBySize(i,i-1);
+            }
+           
+            
+        }
+        int maxs=0;
+        map<int,int>m;
+        for(int i=0;i<num.size();i++){
+          m[ds.findUPar(i)]++;
+          maxs=max(maxs,m[ds.findUPar(i)]);
+        }
+        return maxs;
     }
 };
