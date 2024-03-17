9acfc1351386ed07c8a69e0c0e644a205eeaa34e
harshleet <122361301+harshleet@users.noreply.github.com>
2024-03-18 00:03:29 +0530

    Time: 5 ms (10.13%) | Memory: 9.2 MB (15.58%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    void helper(int k,int n,int ind,set<int>&ds,set<vector<int>>&ans){
-       
-       
-             if( ds.size()==k){
-                 if(n==0){
-                vector<int> d(ds.begin(), ds.end());
-                ans.insert(d);
-               }
-                return ;
-             }
-             
-        for(int i=ind;i<=9;i++){
-            if(i<=n){
-                ds.insert(i);
-                helper(k,n-i,i+1,ds,ans);
-                ds.erase(i);
-            }
+   void helper(int cnt,int sum,vector<vector<int>>&ans,vector<int>ds,map<int,int>&m,int k,int n,int last){
+      if(cnt>=k){
+        if(sum==n){
+            ans.push_back(ds);
         }
-    }
+        return;
+      }
+      for(int i=last+1;i<=9;i++){
+         if(m[i]==0){
+            m[i]=1;
+            sum+=i;
+            cnt++;
+            ds.push_back(i);
+            helper(cnt,sum,ans,ds,m,k,n,i);
+            ds.pop_back();
+            cnt--;
+            sum-=i;
+            m[i]=0;
+         }
+      }
+   }
     vector<vector<int>> combinationSum3(int k, int n) {
-        set<int>ds;
-        set<vector<int>>ans;
-        helper(k,n,1,ds,ans);
-        vector<vector<int>>an(ans.begin(),ans.end());
-        return an;
+        vector<vector<int>>ans;
+        vector<int>ds;
+        map<int,int>m;
+        helper(0,0,ans,ds,m,k,n,0);
+        return ans;
     }
 };
