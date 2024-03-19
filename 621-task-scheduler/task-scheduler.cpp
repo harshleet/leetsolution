7b32afda5a9f0d0dfca641ab58b5048501a79978
harshleet <122361301+harshleet@users.noreply.github.com>
2024-03-19 11:52:56 +0530

    Time: 119 ms (9.06%) | Memory: 52.6 MB (9.96%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -10,33 +10,30 @@ public:
             pq.push({it.second,it.first});
         }
         int timer=0;
-        int maxi=0;
-        vector<int>time(26,0);
         while(!pq.empty()){
-            if(pq.top().first==1){
-              timer+=pq.size();
-               break;
-           }
-            char task=pq.top().second;
-            int freq=pq.top().first;
-            vector<pair<int,char>>todo;
-            todo.push_back(pq.top());
-             pq.pop();
+          if(pq.top().first==1){
+            timer+=pq.size();
+            break;
+          }
+            vector<pair<int,char>>left;
+             cout<<pq.top().second<<" "<<timer<<endl;
+            left.push_back(pq.top());
+            pq.pop();
             timer++;
+           
             for(int i=0;i<n;i++){
-               if(!pq.empty()){
-                   todo.push_back(pq.top());
-                   pq.pop();
-               }
-               timer++;
-            }
-            for(int i=0;i<todo.size();i++){
-                cout<<todo[i].first<<" "<<todo[i].second<<endl;
-                if(todo[i].first>1){
-                    pq.push({todo[i].first-1,todo[i].second});
+                 if(!pq.empty()){
+                   left.push_back(pq.top());
+                    pq.pop();  
+                   
                 }
+                 timer++;
             }
-            
+            for(int i=0;i<left.size();i++){
+                if(left[i].first>1){
+                    pq.push({left[i].first-1,left[i].second});
+                } 
+            }
         }
         return timer;
     }
