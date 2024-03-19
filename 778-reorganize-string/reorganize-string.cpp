c914c0f1d542a96012eb8fbb0a518b8f8cf3fcb6
harshleet <122361301+harshleet@users.noreply.github.com>
2024-03-19 15:03:32 +0530

    Time: 5 ms (9.18%) | Memory: 9.2 MB (8.63%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,49 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
- 
     string reorganizeString(string s) {
-       string ans="";
         map<char,int>m;
         for(int i=0;i<s.size();i++){
             m[s[i]]++;
-            if(m[s[i]]>(s.length() / 2) + (s.length() % 2)){
-                return "";
-            }
         }
         priority_queue<pair<int,char>>pq;
         for(auto it:m){
             pq.push({it.second,it.first});
         }
-        
-        vector<int>time(26,0);
-       
+         string ans="";
         while(!pq.empty()){
-            char task=pq.top().second;
-            int freq=pq.top().first;
-            ans+=task;
-            pq.pop();
-            if(!pq.empty()){
-                char task1=pq.top().second;
-                int freq1=pq.top().first;
-                ans+=task1;
-                pq.pop();
-                if(freq1>1){
-                  pq.push({freq1-1,task1});
-                }
-            }
-            if(freq>1){
-             pq.push({freq-1,task});
-            }
             
+            vector<pair<int,char>>left;
+             ans+=pq.top().second;
+             left.push_back(pq.top());
+             pq.pop();
+             if(pq.size()==0){
+                 break;
+             }else{
+                ans+=pq.top().second;
+                left.push_back(pq.top());
+                pq.pop();
+             }
+              for(int i=0;i<left.size();i++){
+                if(left[i].first>1){
+                    pq.push({left[i].first-1,left[i].second});
+                }
+               
+             }
+        }
+        if(ans.size()!=s.size()){
+            return "";
         }
         return ans;
     }
 };
-
-
