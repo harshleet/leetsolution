e8c9b6b409789a4025b03176ccc077c56e3a9fff
harshleet <122361301+harshleet@users.noreply.github.com>
2024-03-21 01:11:19 +0530

    Time: 534 ms (6.32%) | Memory: 104.7 MB (38.58%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-    int calfreq(string& s) {
-        int c = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '1') {
-                c++;
+  int helper(int ind,int o,int z,vector<string>& strs, int m, int n,map<int,int>&m1,map<int,int>&m2,vector<vector<vector<int>>>&dp){
+    if(ind>=strs.size()){
+      return 0;
+    }
+    if(dp[ind][o][z]!=-1){
+        return dp[ind][o][z];
+    }
+    int ntake=helper(ind+1,o,z,strs,m,n,m1,m2,dp);
+    int take=-1e9;
+    if(o+m1[ind]<=n && z+m2[ind]<=m){
+        take=1+helper(ind+1,o+m1[ind],z+m2[ind],strs,m,n,m1,m2,dp);
+    }  
+    return dp[ind][o][z]=max(take,ntake);
+  }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        map<int,int>m1;
+        map<int,int>m2;
+        for(int i=0;i<strs.size();i++){
+            for(int j=0;j<strs[i].size();j++){
+                if(strs[i][j]=='1'){
+                    m1[i]++;
+                }else{
+                      m2[i]++;
+                }
             }
         }
-        return c;
-    }
-
-    int helper(vector<string>& strs, vector<vector<vector<int>>>& dp, int m, int n, int ind, vector<int>& ones, vector<int>& zero) {
-        if (ind >= strs.size() || (m <= 0 && n <= 0)) {
-            return 0;
-        }
-
-        if (dp[ind][m][n] != -1) {
-            return dp[ind][m][n];
-        }
-
-        int ntake = helper(strs, dp, m, n, ind + 1, ones, zero);
-        int take = 0;
-        if (zero[ind] <= m && ones[ind] <= n) {
-            take = 1 + helper(strs, dp, m - zero[ind], n - ones[ind], ind + 1, ones, zero);
-        }
-
-        return dp[ind][m][n] = max(ntake, take);
-    }
-
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<vector<int>>> dp(strs.size(), vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
-        vector<int> ones(strs.size());
-        vector<int> zero(strs.size());
-        for (int i = 0; i < strs.size(); i++) {
-            int a = calfreq(strs[i]);
-            ones[i] = a;
-            zero[i] = strs[i].size() - a;
-        }
-        return helper(strs, dp, m, n, 0, ones, zero);
+        vector<vector<vector<int>>>dp(strs.size(),vector<vector<int>>(n+1,vector<int>(m+1,-1)));
+        return helper(0,0,0,strs,m,n,m1,m2,dp);
     }
 };
