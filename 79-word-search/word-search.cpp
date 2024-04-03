94e89425b52b64946603e728f5c947e392428b48
harshleet <122361301+harshleet@users.noreply.github.com>
2024-04-03 11:17:09 +0530

    Time: 682 ms (51.01%) | Memory: 12 MB (9.82%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,48 +1,42 @@
 class Solution {
 public:
-     vector<int>dr={0,0,-1,1};
-     vector<int>dc={-1,1,0,0};
-    bool helper(int row,int col,string ds,vector<vector<char>>& board, string word,vector<vector<int>>&vis,int ind){
-        vis[row][col]=1;
-        if(ds.size()==word.size()){
-          
-            if(ds==word){
-                return true;
-            }
-            return false;
+    vector<int> dr = {1, -1, 0, 0};
+    vector<int> dc = {0, 0, 1, -1};
+    bool helper(int r, int c, string ds, vector<vector<char>>& board,
+                string word, vector<vector<int>>& vis,int ind) {
+        vis[r][c] = 1;
+        if (ds.size() == word.size()) {
+            cout<<ds<<" ";
+            return ds == word;
         }
-        bool ans=false;
-        for(int i=0;i<4;i++){
-            int nrow=row+dr[i];
-            int ncol=col+dc[i];
-            if(nrow<board.size() && ncol<board[0].size() && nrow>=0 && ncol>=0 && !vis[nrow][ncol] && word[ind]==board[nrow][ncol]){
-              
-                ds.push_back(board[nrow][ncol]);
-                ans|=helper(nrow,ncol,ds,board,word,vis,ind+1);
-                ds.pop_back();
-              
-            }
-        }
-          vis[row][col]=0;
-        return ans;
+        bool ans = false;
 
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if (nr >= 0 && nc >= 0 && nr < board.size() &&
+                nc < board[0].size() && !vis[nr][nc] && word[ind]==board[nr][nc]) {
+                ds.push_back(board[nr][nc]);
+                ans|=helper(nr, nc, ds, board, word, vis,ind+1);
+                ds.pop_back();
+            }
+        }
+        vis[r][c] = 0;
+        return ans;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        string ds="";
-        bool ans=false;
-        vector<vector<int>>vis(board.size(),vector<int>(board[0].size(),0));
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-               if(word[0]==board[i][j]){ 
-               
-                 ds.push_back(board[i][j]);
-
-                  ans|=helper(i,j,ds,board,word,vis,1);
-                  ds.pop_back();
-                }
+        bool ans = false;
+        int n = board.size(), m = board[0].size();
+         string ds="";
+         vector<vector<int>> vis(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+              if(word[0]==board[i][j]){
+                ds.push_back(board[i][j]);
+                ans |= helper(i, j,ds, board, word, vis,1);
+                 ds.pop_back();}
             }
         }
-       return ans;
-
+        return ans;
     }
 };
