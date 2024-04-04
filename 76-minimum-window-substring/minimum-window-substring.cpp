cf56abf61ede63a8c525c76284e35a9f49405264
harshleet <122361301+harshleet@users.noreply.github.com>
2024-04-04 22:54:12 +0530

    Time: 20 ms (52.25%) | Memory: 10.1 MB (68.00%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -3,32 +3,39 @@ public:
     string minWindow(string s, string t) {
        
         vector<int>m2(256,0);
-       
+        set<char>st;
        
         int j=0,mini=INT_MAX;
         int mind=-1;
         for(int i=0;i<t.size();i++){
             m2[t[i]]++;
-          
+           st.insert(t[i]);
+           
         }
         int c=t.size();
        
         for(int i=0;i<s.size();i++){
+               if(st.find(s[i])!=st.end()){
                 m2[s[i]]--;
                 if(m2[s[i]]>=0){
                     c--;
                    
                 }
+                
+            }
 
             while(c==0 && j<=i ){
                 if(mini>i-j+1){
                     mini=i-j+1;
                     mind=j;
                 }
+                       if(st.find(s[j])!=st.end()){
                         m2[s[j]]++;
                         if(m2[s[j]]>0){
                             c++;
                         }
+                    
+                }
 
                 j++;
             }
