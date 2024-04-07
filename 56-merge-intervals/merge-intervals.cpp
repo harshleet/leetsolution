90f7d568c0c3bbb68efb2f458c010d47bfb86fb9
harshleet <122361301+harshleet@users.noreply.github.com>
2024-04-07 23:09:25 +0530

    Time: 27 ms (33.63%) | Memory: 24.7 MB (7.83%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>>ans;
-        int n=intervals.size();
-        int i=0;
-        for(i=1;i<n;i++){
-            vector<int>v;
-            int start=intervals[i-1][0];
-            int end=intervals[i-1][1];
-             while(i< n && end>=intervals[i][0]){
-               start=min(start,intervals[i][0]);
-               end=max(end,intervals[i][1]);
-               i++;
-             }
-             v.push_back(start);
-              v.push_back(end);
-              ans.push_back(v);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(auto it: intervals){
+            pq.push({it[0],it[1]});
         }
-        cout<<i<<" ";
-        if(i==n){
-            ans.push_back(intervals[n-1]);
+       vector<vector<int>>ans;
+        while(!pq.empty()){
+            int st=pq.top().first;
+            int en=pq.top().second;
+            pq.pop();
+
+            while(!pq.empty() && pq.top().first<=en){
+                en=max(en,pq.top().second);
+                pq.pop();
+            }
+            vector<int>temp={st,en};
+            ans.push_back(temp);
         }
         return ans;
     }
