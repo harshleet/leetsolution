dfdd5180785392d9f56cb468022e1e9e6b4dae63
harshleet <122361301+harshleet@users.noreply.github.com>
2024-04-12 10:19:23 +0530

    Time: 7 ms (88.11%) | Memory: 23.1 MB (25.29%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int maxi=height[0],n=height.size();
-        vector<int>mf(n,-1);
-        vector<int>mb(n,-1);
-        for(int i=1;i<n;i++){
-            if(height[i]<maxi){
-                mf[i]=maxi;
+        int maxi = height[0], n = height.size(), maxi2 = height[n - 1];
+        ;
+        vector<int> mf(n, -1);
+        vector<int> mb(n, -1);
+        for (int i = 1; i < n; i++) {
+            if (height[i] < maxi) {
+                mf[i] = maxi;
             }
-              maxi=max(maxi,height[i]);
-            cout<<mf[i]<<" ";
-        }
-        cout<<endl;
-        maxi=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            if(height[i]<maxi){
-                mb[i]=maxi;
+            maxi = max(maxi, height[i]);
+            
+            if (height[n - 1 - i] < maxi2) {
+                mb[n - 1 - i] = maxi2;
             }
-              maxi=max(maxi,height[i]);
-            cout<<mb[i]<<" ";
+            maxi2 = max(maxi2, height[n - 1 - i]);
+           
         }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(mb[i]==-1 || mf[i]==-1){
-                continue;
-            }else{
-                ans+=min(mb[i],mf[i])-height[i];
 
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (mb[i] == -1 || mf[i] == -1) {
+                continue;
+            } else {
+                ans += min(mb[i], mf[i]) - height[i];
             }
         }
         return ans;
