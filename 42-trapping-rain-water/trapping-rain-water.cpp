d5d5bf976d71c3cad8f7b401f97d2097a383e045
harshleet <122361301+harshleet@users.noreply.github.com>
2024-04-12 10:17:50 +0530

    Time: 43 ms (5.05%) | Memory: 23.3 MB (13.23%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<int>mb(height.size());
-        vector<int>mf(height.size());
-        mb[0]=0;
-        mf.back()=0;
-        int mba=height[0],mfr=height.back();
-        for(int i=1;i<height.size();i++){
-            mb[i]=mba;
-            mba=max(mba,height[i]);
+        int maxi=height[0],n=height.size();
+        vector<int>mf(n,-1);
+        vector<int>mb(n,-1);
+        for(int i=1;i<n;i++){
+            if(height[i]<maxi){
+                mf[i]=maxi;
+            }
+              maxi=max(maxi,height[i]);
+            cout<<mf[i]<<" ";
         }
-        for(int i=height.size()-2;i>=0;i--){
-            mf[i]=mfr;
-            mfr=max(mfr,height[i]);
+        cout<<endl;
+        maxi=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(height[i]<maxi){
+                mb[i]=maxi;
+            }
+              maxi=max(maxi,height[i]);
+            cout<<mb[i]<<" ";
         }
-        int c=0;
-        for(int i=0;i<height.size();i++){
-            if(mb[i]==0 || mf[i]==0){
-                continue;
-            }else if(min(mb[i],mf[i])<=height[i]){
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(mb[i]==-1 || mf[i]==-1){
                 continue;
             }else{
-                c+=min(mb[i],mf[i])-height[i];
+                ans+=min(mb[i],mf[i])-height[i];
+
             }
         }
-        return c;
+        return ans;
     }
 };
