d8911df051817c552be8a4064f9f0ff7d8da290b
harshleet <122361301+harshleet@users.noreply.github.com>
2024-04-15 23:16:17 +0530

    Time: 154 ms (30.03%) | Memory: 60 MB (41.74%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        //o(n^2 solun)
-       sort(nums.begin(),nums.end());
-        long long ans=0;
+        sort(nums.begin(),nums.end());
+        long long cnt=0;
         for(int i=0;i<nums.size();i++){
-         
-            auto lowerIt = lower_bound(nums.begin()+i+1,nums.end(),lower-nums[i]);
-            auto upperIt = upper_bound(nums.begin()+i+1,nums.end(),upper-nums[i]);
-            long long count = std::distance(lowerIt, upperIt);
-            ans+=count;
-            
-        }return ans;
+             int l=lower_bound(nums.begin()+i+1,nums.end(),lower-nums[i])-nums.begin();
+             int h=upper_bound(nums.begin()+i+1,nums.end(),upper-nums[i])-nums.begin();
+             cnt+=h-l;
+        }
+        return cnt;
     }
 };
