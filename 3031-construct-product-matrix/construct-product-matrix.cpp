94410393a3ff267a32565f1bbd041e4b0c365727
harshleet <122361301+harshleet@users.noreply.github.com>
2024-04-16 00:21:27 +0530

    Time: 194 ms (61.30%) | Memory: 139.7 MB (64.71%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -2,24 +2,30 @@ class Solution {
 public:
 int mod=12345;
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
-        long long int p=1,s=1;
-        vector<vector<long long int>>pre(grid.size(),vector<long long int>(grid[0].size()));
-        vector<vector<long long int>>suf(grid.size(),vector<long long int>(grid[0].size()));
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-               pre[i][j]=p;
-               suf[grid.size()-1-i][grid[0].size()-1-j]=s;
-               s=(s*grid[grid.size()-1-i][grid[0].size()-1-j])%mod;
-               p=(p*grid[i][j])%mod;
-            }
-        }
-          vector<vector<int>>ans(grid.size(),vector<int>(grid[0].size()));
-         for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-               ans[i][j]=(pre[i][j]*suf[i][j])%mod;
-            }
-        }
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<long long int>left((n*m)+1);
+        vector<long long int>right((n*m)+1);
+       
+          left[0]=1;
+          right[n*m]=1;
+          for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    left[i*m+j+1]=(left[i*m+j]*grid[i][j])%mod;
+                    
+                }  
+          }
+          for(int i=n-1;i>=0;i--){
+                for(int j=m-1;j>=0;j--){
+                    right[i*m+j]=(right[i*m+j+1]*grid[i][j])%mod;
+                }  
+          }
+          vector<vector<int>>ans(n,vector<int>(m));
+          for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                   ans[i][j]=(left[i*m+j]*right[i*m+j+1])%mod;
+                }  
+          }
         return ans;
-
     }
 };
