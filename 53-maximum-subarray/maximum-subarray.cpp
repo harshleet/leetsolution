78f46df2ae69498e323af68ab9cc5091b4f10ef8
harshleet <122361301+harshleet@users.noreply.github.com>
2024-04-15 19:39:14 +0530

    Time: 92 ms (6.32%) | Memory: 69.9 MB (99.89%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,11 +4,12 @@ public:
         int j=0,sum=0,maxi=INT_MIN;
         for(int i=0;i<nums.size();i++){
            sum+=nums[i];
-           maxi=max(maxi,sum);
-           while(sum<0){
-               sum=0;
+            maxi=max(maxi,sum);
+           while(sum<0 ){
+               sum-=nums[j];
+               j++;
            }
-           
+          
         }
         return maxi;
     }
