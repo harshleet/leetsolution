29a48c0c0fd1f852e218fafaebb06e0252317613
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-19 23:58:47 +0530

    Time: 742 ms (5.03%) | Memory: 404.8 MB (5.15%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-     int helper(int ind1,int ind2,string word1, string word2,vector<vector<int>>&dp){
-         if(ind1>=word1.size()){
-             return word2.size()-ind2; 
-         }
-         if(ind2>=word2.size()){
-             return word1.size()-ind1;
-         }
+    int helper(int ind1,int ind2,string word1,string word2,int n,int m,vector<vector<int>>&dp){
+        if(ind1>=n ){
+            return word2.size()-ind2 ;
+        }
+        if(ind2>=m ){
+            return word1.size()-ind1 ;
+        }
         if(dp[ind1][ind2]!=-1){
             return dp[ind1][ind2];
         }
-         if(word1[ind1]==word2[ind2]){
-             return dp[ind1][ind2]=helper(ind1+1,ind2+1,word1,word2,dp);
-         }
-         int in=1+helper(ind1,ind2+1,word1,word2,dp);
-         int de=1+helper(ind1+1,ind2,word1,word2,dp);
-         int re=1+helper(ind1+1,ind2+1,word1,word2,dp);
-
-         return dp[ind1][ind2]=min({in,de,re});
-     }
+        int nop=INT_MAX;
+        if(word1[ind1]==word2[ind2]){
+            nop=helper(ind1+1,ind2+1,word1,word2,n,m,dp);
+        }
+        int re=1+helper(ind1+1,ind2+1,word1,word2,n,m,dp);
+        int de=1+helper(ind1+1,ind2,word1,word2,n,m,dp);
+        int in=1+helper(ind1,ind2+1,word1,word2,n,m,dp);
+        return dp[ind1][ind2]= min({re,de,in,nop});
+    }
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
-        return helper(0,0,word1,word2,dp);
+        int n=word1.size(),m=word2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return helper(0,0,word1,word2,n,m,dp);
     }
 };
