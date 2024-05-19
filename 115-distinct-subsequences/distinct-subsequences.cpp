d499b6a51461695ba0689e6dabf9a1bed7c1caba
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-19 18:14:45 +0530

    Time: 20 ms (51.73%) | Memory: 19.7 MB (60.86%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int helper(string& s,string& t,int i,int j,vector<vector<int>>&dp){
-        if(j<0){
+    int helper(int ind1,int ind2,string& s,string& t,vector<vector<int>>&dp,int n,int m){
+        if(ind2>=m){
             return 1;
         }
-        if(i<0){
+        if(ind1>=n){
             return 0;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+          if(dp[ind1][ind2]!=-1){
+            return dp[ind1][ind2];
+          }
+        int op1=helper(ind1+1,ind2,s,t,dp,n,m);
+        int op2=0;
+        if(s[ind1]==t[ind2]){
+            op2=helper(ind1+1,ind2+1,s,t,dp,n,m);
         }
-        if(s[i]==t[j]){
-            return dp[i][j]=helper(s,t,i-1,j-1,dp)+helper(s,t,i-1,j,dp);
-        }
-        return dp[i][j]=helper(s,t,i-1,j,dp);
+        return dp[ind1][ind2]=op1+op2;
     }
     int numDistinct(string s, string t) {
-        int n=s.length();
-        int m=t.length();
-        vector<vector<int>>dp(n,(vector<int>(m,-1)));
-        return helper(s,t,n-1,m-1,dp);
-
+        int n=s.size(),m=t.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return helper(0,0,s,t,dp,n,m);
     }
 };
