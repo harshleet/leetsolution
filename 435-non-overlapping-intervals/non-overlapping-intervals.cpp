543b2d503dfde49fb6bb0877cb5a8870935588bb
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-24 01:12:27 +0530

    Time: 516 ms (11.73%) | Memory: 109.9 MB (13.49%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
-    int findNextNonOverlapping(vector<vector<int>>& intervals, int target, int ind) {
-        int left = ind + 1;
-        int right = intervals.size() - 1;
-        int res = intervals.size();
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (intervals[mid][0] >= target) {
-                res = mid;
-                right = mid - 1;
+    int search(int vl, vector<vector<int>>& intervals, int ind) {
+        int low = ind, high = intervals.size() - 1, ans = intervals.size();
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (intervals[mid][0] >= vl) {
+                ans = mid;
+                high = mid - 1;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
-        
-        return res;
+        return ans;
     }
-
-    int helper(vector<vector<int>>& intervals, int ind, vector<int>& dp) {
+    int helper(int ind, int last, vector<vector<int>>& intervals,
+               vector<int>& dp) {
         if (ind >= intervals.size()) {
             return 0;
         }
         if (dp[ind] != -1) {
             return dp[ind];
         }
+        int nt = helper(ind + 1, last, intervals, dp);
+        int f = search(intervals[ind][1], intervals, ind + 1);
+        cout << intervals[ind][1] << " " << f << endl;
+        int t = 1+helper(f, intervals[ind][1], intervals, dp);
 
-        int ntake = helper(intervals, ind + 1, dp);
-
-        int nextNonOverlapping = findNextNonOverlapping(intervals, intervals[ind][1], ind);
-        int take = 0;
-        if (nextNonOverlapping < intervals.size() && intervals[ind][1] <= intervals[nextNonOverlapping][0]) {
-            take = 1 + helper(intervals, nextNonOverlapping, dp);
-        }
-
-        return dp[ind] = max(take, ntake);
+        return dp[ind] = max(t, nt);
     }
-
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        int n = intervals.size();
-        vector<int> dp(n, -1);
-        int val = helper(intervals, 0, dp);
-        return n - val-1;
+        vector<pair<int, int>> temp(intervals.size());
+        // for (int i = 0; i < temp.size(); i++) {
+        //     temp[i] = {intervals[i][0], intervals[i][1]};
+        // }
+        vector<int> dp(intervals.size(), -1);
+
+        return intervals.size()-helper(0, INT_MIN, intervals, dp);
     }
 };
