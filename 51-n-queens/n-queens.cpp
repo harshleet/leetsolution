b793f144da43afefb0d030f191c42f730e907cf9
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-24 15:19:32 +0530

    Time: 13 ms (20.89%) | Memory: 16.7 MB (16.27%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    void helper(std::vector<std::vector<std::string>>& ans, std::vector<std::string>& board, int n, int ind, std::vector<int>& r, std::vector<int>& ld, std::vector<int>& rd) {
-        if (ind >= n) {
-            ans.push_back(board);
+    void helper(int ind,int n,vector<string>ds,vector<vector<string>>&ans,map<int,int>&c,map<int,int>&ld,map<int,int>&rd){
+        if(ind>=n){
+            ans.push_back(ds);
             return;
         }
 
-        for (int row = 0; row < n; row++) {
-            if (r[row] == 0 && ld[ind + row] == 0 && rd[n - 1 - row + ind] == 0) {
-                r[row] = 1;
-                ld[ind + row] = 1;
-                rd[n - 1 - row + ind] = 1;
-                board[ind][row] = 'Q';
-                helper(ans, board, n, ind + 1, r, ld, rd);
-                board[ind][row] = '.';
-                r[row] = 0;
-                ld[ind + row] = 0;
-                rd[n - 1 - row + ind] = 0;
-            }
+        for(int i=0;i<n;i++){
+           if(c[i]==0 && ld[n-ind+i]==0 && rd[i+ind]==0){
+            c[i]=1;
+            ld[n-ind+i]=1;
+            rd[i+ind]=1;
+            ds[ind][i]='Q';
+            helper(ind+1,n,ds,ans,c,ld,rd);
+            c[i]=0;
+            ld[n+i-ind]=0;
+            rd[i+ind]=0;
+            ds[ind][i]='.';
+           }
         }
+
     }
-
-    std::vector<std::vector<std::string>> solveNQueens(int n) {
-        std::vector<int> r(n, 0);
-        std::vector<int> ld(2 * n, 0);
-        std::vector<int> rd(2 * n, 0);
-        std::vector<std::string> board(n, std::string(n, '.'));
-
-        std::vector<std::vector<std::string>> ans;
-        helper(ans, board, n, 0, r, ld, rd);
-
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>ds(n);
+        for(int i=0;i<n;i++){
+            string s(n,'.');
+            ds[i]=s;
+        }
+        map<int,int>c;
+        map<int,int>ld;
+        map<int,int>rd;
+        vector<vector<string>>ans;
+        helper(0,n,ds,ans,c,ld,rd);
+        
         return ans;
     }
 };
