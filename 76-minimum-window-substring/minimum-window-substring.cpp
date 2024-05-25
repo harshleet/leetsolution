06d42427e838c0280999b91718ea86a60d104bb4
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-25 19:06:53 +0530

    Time: 21 ms (50.81%) | Memory: 9.9 MB (72.93%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       
-        vector<int>m2(256,0);
-        set<char>st;
-       
-        int j=0,mini=INT_MAX;
-        int mind=-1;
-        for(int i=0;i<t.size();i++){
-            m2[t[i]]++;
-           st.insert(t[i]);
-           
+        map<char, int> m;
+        vector<int> temp(66, 0);
+        int cnt=0;
+        set<char> st;
+        for (int i = 0; i < t.size(); i++) {
+            temp[t[i] - 'A']++;
+            st.insert(t[i]);
         }
-        int c=t.size();
-       
-        for(int i=0;i<s.size();i++){
-               if(st.find(s[i])!=st.end()){
-                m2[s[i]]--;
-                if(m2[s[i]]>=0){
-                    c--;
-                   
-                }
-                
+        int mini=INT_MAX;
+        int minii=-1,  minij=-1;
+        int j = 0;
+        for (int i = 0; i < s.size(); i++) {
+            temp[s[i] - 'A']--;
+            if (st.find(s[i]) != st.end() && temp[s[i]-'A']==0) {   
+                cnt++;
             }
 
-            while(c==0 && j<=i ){
+           
+            while (cnt==st.size() && j <= i) {
+            
                 if(mini>i-j+1){
                     mini=i-j+1;
-                    mind=j;
+                    minii=i;
+                    minij=j;
                 }
-                       if(st.find(s[j])!=st.end()){
-                        m2[s[j]]++;
-                        if(m2[s[j]]>0){
-                            c++;
-                        }
-                    
+                temp[s[j]-'A']++;
+                if(st.find(s[j])!=st.end() && temp[s[j]-'A']>0){
+                    cnt--;
                 }
-
                 j++;
             }
-
         }
-    if(mind==-1){
-        return "";
-    }
-        return s.substr(mind,mini);
+        if(minii==-1 && minij==-1){
+            return "";
+        }
+        return s.substr(minij,minii-minij+1);
     }
 };
