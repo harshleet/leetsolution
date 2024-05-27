aab821441312894301c9ca6d10aab76e96dad3f3
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-27 15:33:22 +0530

    Time: 374 ms (22.49%) | Memory: 122.3 MB (37.79%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.cpp
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.cpp
@@ -4,18 +4,11 @@ public:
         int j=0,ans=0,rep=0,mini=INT_MAX;
         map<int,int>m;
         for(int i=0;i<cards.size();i++){
-                m[cards[i]]++;
-                if(m[cards[i]]>1){
-                    rep++;
+             
+                if(m.find(cards[i])!=m.end()){
+                    mini=min(mini,i-m[cards[i]]+1);
                 }
-                while(rep>0 && j<=i){
-                    mini=min(mini,i-j+1);
-                    m[cards[j]]--;
-                    if(m[cards[j]]==1){
-                        rep--;
-                    }
-                    j++;
-                }
+               m[cards[i]]=i;
         }
         if(mini==INT_MAX){
             return -1;
