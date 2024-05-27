2bc39439c4bf82d9ef92c27c854354624339557e
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-27 23:30:57 +0530

    Time: 7 ms (92.51%) | Memory: 13.9 MB (37.81%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,53 +1,56 @@
 class Solution {
 public:
-vector<int>drow={-1,1,0,0};
-vector<int>dcol={0,0,-1,1};
-
-    void dfs(int row,int col,vector<vector<char>>& board,vector<vector<int>>&vis){
-        
-        if(row<0 || col<0 || row>=board.size()|| col>=board[0].size()){
-            return;
-        }
-        if(vis[row][col]==1 || board[row][col]=='X'){
-              return;
-        }
-        vis[row][col]=1;
-       
-        for(int i=0;i<4;i++){
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            dfs(nrow,ncol,board,vis);
-        }
-    }
     void solve(vector<vector<char>>& board) {
         int n=board.size(),m=board[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-      
+        queue<pair<int,int>>q;
+       
         for(int i=0;i<n;i++){
-            if(!vis[i][0] && board[i][0]=='O'){
-                dfs(i,0,board,vis);
+            if(board[i][0]=='O'){
+                vis[i][0]=1;
+               q.push({i,0});
             }
-            if(!vis[i][m-1] && board[i][m-1]=='O'){
-                dfs(i,m-1,board,vis);
+            if(board[i][m-1]=='O'){
+                vis[i][m-1]=1;
+               q.push({i,m-1});
             }
         }
-        for(int j=0;j<m;j++){
-             if(!vis[0][j] && board[0][j]=='O'){
-                dfs(0,j,board,vis);
+        for(int i=0;i<m;i++){
+            if(board[0][i]=='O'){
+                vis[0][i]=1;
+               q.push({0,i});
             }
-            if(!vis[n-1][j] && board[n-1][j]=='O'){
-                dfs(n-1,j,board,vis);
+            if(board[n-1][i]=='O'){
+                
+                vis[n-1][i]=1;
+               q.push({n-1,i});
             }
         }
-        for(int i=0;i<n;i++){
-             for(int j=0;j<m;j++){
-                 if(vis[i][j]==0 && board[i][j]=='O'){
-                    board[i][j]='X';
+        vector<int>dr={1,-1,0,0};
+        vector<int>dc={0,0,-1,1};
+
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            // cout<<r<<" "<<c<<endl;
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+
+                if(nr>=0 && nc>=0 && nr<n && nc<m && !vis[nr][nc] && board[nr][nc]=='O'){
+                    vis[nr][nc]=1;
+                    q.push({nr,nc});
                 }
-             }
-            
-          
+            }
         }
-                 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O' && vis[i][j]==0){
+                     board[i][j]='X';
+                }
+            }
+        }
+        // return board;
     }
 };
