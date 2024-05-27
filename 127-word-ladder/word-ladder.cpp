5df471af471b3eecfaa08ef28ee37f07bb0b5006
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-28 00:30:55 +0530

    Time: 698 ms (24.25%) | Memory: 21.7 MB (31.00%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    int diff(const std::string& str1, const std::string& str2) {
-        int count = 0;
-        for (size_t i = 0; i < str1.length(); ++i) {
-            if (str1[i] != str2[i]) {
-                ++count;
-            }
-        }
-        return count;
-    }
-
-    int ladderLength(std::string beginWord, std::string endWord, std::vector<std::string>& wordList) {
-        std::queue<std::pair<std::string, int>> q;
-        std::unordered_set<std::string> st(wordList.begin(), wordList.end());
-        q.push({beginWord, 0});
-
-        while (!q.empty()) {
-            std::string node = q.front().first;
-            int step = q.front().second;
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        set<string>word(wordList.begin(),wordList.end());
+        map<string,int>vis;
+        queue<pair<int,string>>q;
+        q.push({0,beginWord});
+        vis[beginWord]=1;
+        while(!q.empty()){
+            int dis=q.front().first;
+            string node=q.front().second;
             q.pop();
 
-            if (node == endWord) {
-                return step + 1;
+            if(node==endWord){
+                return dis+1;
             }
-
-            for (auto it = st.begin(); it != st.end(); ) {
-                if (diff(*it, node) == 1) {
-                    q.push({*it, step + 1});
-                    it = st.erase(it);
-                } else {
-                    ++it;
+            string ori=node;
+            for(int i=0;i<node.size();i++){
+                for(char j='a';j<='z';j++){
+                    ori[i]=j;
+                   if(vis.find(ori)==vis.end() && word.find(ori)!=word.end()){
+                      q.push({dis+1,ori});
+                      vis[ori]=1;
+                   }
+                   ori=node;
                 }
             }
         }
-
         return 0;
     }
 };
