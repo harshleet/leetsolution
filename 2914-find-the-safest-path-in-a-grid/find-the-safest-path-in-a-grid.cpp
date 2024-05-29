7efcec4afc0cd1075ecd205e00327198c148abe6
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-29 16:05:48 +0530

    Time: 338 ms (57.78%) | Memory: 156.1 MB (36.54%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,84 +1,71 @@
+
 class Solution {
 public:
-    vector<int>dr={1,0,-1,0};
-    vector<int>dc={0,1,0,-1};
-
-   
-    bool dfs(int r,int c,int mini,int n,int m,vector<vector<int>>&man, vector<vector<int>>&vi){
-        if(man[r][c]<mini){
-            return false;
-        }
-        if(r==n-1 && c==m-1){
-            return true;
-        }
-        vi[r][c]=1;
-        bool a=false;
-          for(int i=0;i<4;i++){
-              int nrow=r+dr[i];
-             int ncol=c+dc[i];
-              if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vi[nrow][ncol] ){
-                        a=a||dfs(nrow,ncol,mini,n,m,man,vi);
-              }
-          }
-          
-          return a;
-           
-    }
     int maximumSafenessFactor(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size();
+        int n=grid.size();
+        int m=grid[0].size();
         vector<vector<int>>man(n,vector<int>(m,0));
-         vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<tuple<int,int,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        vector<vector<int>>vis(n,vector<int>(m,0));
+           queue<tuple<int, int, int>> q;
+                for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    vis[i][j]=1;
-                     q.push({i,j,0});
+                      q.push({0,i,j});
+                      vis[i][j]=1;
                 }
-                
             }
         }
-
-        
-       
+        vector<int>dr={1,-1,0,0};
+        vector<int>dc={0,0,1,-1};
         while(!q.empty()){
-                int r=get<0>(q.front());
-                int c=get<1>(q.front());
-                int step=get<2>(q.front());
-                
-                q.pop();
-                man[r][c]=step;
-                for(int i=0;i<4;i++){
-                    int nrow=r+dr[i];
-                    int ncol=c+dc[i];
-                    if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0){
-                        vis[nrow][ncol]=1;
-                        q.push({nrow,ncol,step+1});
-                    }
+            int dis=get<0>(q.front());
+            int r=get<1>(q.front());
+            int c=get<2>(q.front());
+            q.pop();
+           for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m && !vis[nr][nc]){
+                    man[nr][nc]=dis+1;
+                    vis[nr][nc]=1;
+                    q.push({dis+1,nr,nc});
                 }
-   
+            }
+
         }
-        //now check for paths
         // for(int i=0;i<n;i++){
         //     for(int j=0;j<m;j++){
         //         cout<<man[i][j]<<" ";
         //     }
         //     cout<<endl;
         // }
-         
-         int low=0,high=min(man[0][0],man[n-1][m-1]);
-         int ans=0;
-         while(low<=high){
-            int mid=(low+high)/2;
-            vector<vector<int>>vi(n,vector<int>(m,0));
-             if(dfs(0,0,mid,n,m,man,vi)){
-                ans=mid;
-                low=mid+1;
-             }else{
-                high=mid-1;
-             }
-         }
-          
-        return ans;
+        vector<vector<int>>dist(n,vector<int>(m,0));
+        priority_queue<tuple<int,int,int>>pq;
+        pq.push({man[0][0],0,0});
+        while(!pq.empty()){
+             int dis=get<0>(pq.top());
+            int r=get<1>(pq.top());
+            int c=get<2>(pq.top());
+            pq.pop();
+            // cout<<r<<" "<<c<<" "<<dis<<endl;
+            if(r==n-1 && c==m-1){
+                return dis;
+            }
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m ){
+                    int mini=min(dis,man[nr][nc]);
+                    if(dist[nr][nc]<mini){
+                        dist[nr][nc]=mini;
+                        pq.push({mini,nr,nc});
+                    }
+                }
+            }
+
+        }
+        
+        return 0;
+
     }
 };
