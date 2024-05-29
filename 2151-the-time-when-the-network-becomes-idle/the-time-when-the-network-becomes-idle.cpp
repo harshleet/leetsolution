66c9380fde2e56e5f8c4ee4c38b091483ed285c2
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-30 00:10:26 +0530

    Time: 385 ms (90.65%) | Memory: 194.4 MB (60.75%) - LeetSync

---
+++ 2151-the-time-when-the-network-becomes-idle/the-time-when-the-network-becomes-idle.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
+       //try to find distance of each node from the 0
+       int n= patience.size();
+       vector<vector<int>>adj(n);
+       for(int i=0;i<edges.size();i++){
+         adj[edges[i][0]].push_back(edges[i][1]);
+         adj[edges[i][1]].push_back(edges[i][0]);
+       }
+       queue<pair<int,int>>q;
+       vector<int>dist(n,INT_MAX);
+       q.push({0,0});
+       dist[0]=0;
+
+       while(!q.empty()){
+        int node=q.front().second;
+        int dis=q.front().first;
+        q.pop();
+
+        for(auto it:adj[node]){
+            if(dist[it]>1+dis){
+                dist[it]=1+dis;
+                q.push({1+dis,it});
+            }
+        }
+       }
+       int maxi=0;
+       for(int i=1;i<n;i++){
+        dist[i]*=2;
+         int no=(dist[i]-1)/patience[i];
+         if(no==0){
+            maxi=max(maxi,dist[i]+1);
+         }else{
+            maxi=max(maxi,1+dist[i]+((dist[i]-1)/patience[i])*patience[i]);
+         }
+       }
+       return maxi;
+
+
+    }
+};
