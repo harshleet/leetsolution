10ebab4f8a86d022c3935476017203c64bb364e2
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-29 13:33:06 +0530

    Time: 93 ms (55.01%) | Memory: 23.5 MB (69.60%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -2,34 +2,36 @@ class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size(),m=heights[0].size();
-        vector<vector<int>>dis(n,vector<int>(m,INT_MAX));
         priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
 
+        dist[0][0]=0;
         pq.push({0,0,0});
-        dis[0][0]=1;
         vector<int>dr={1,-1,0,0};
-        vector<int>dc={0,0,1,-1};
+        vector<int>dc={0,0,-1,1};
         while(!pq.empty()){
-            int dist=get<0>(pq.top());
-            int row=get<1>(pq.top());
-            int col=get<2>(pq.top());
+            int maxE=get<0>(pq.top());
+            int r=get<1>(pq.top());
+            int c=get<2>(pq.top());
             pq.pop();
-            if(row==n-1 && col==m-1){
-                return dist;
-            }
+       
+           if(r==n-1 && c==m-1){
+            return maxE;
+           }
             for(int i=0;i<4;i++){
-                int nrow=row+dr[i];
-                int ncol=col+dc[i];
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m){
-                int maxi=max(dist,abs(heights[row][col]-heights[nrow][ncol]));
-                if(dis[nrow][ncol]>maxi){
-                    dis[nrow][ncol]=maxi;
-                    pq.push({maxi,nrow,ncol});
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m){
+                    int maxi=max(maxE,abs(heights[nr][nc]-heights[r][c]));
+                   
+                    if(dist[nr][nc]>maxi){
+                        dist[nr][nc]=maxi;
+                        pq.push({maxi,nr,nc});
+                    }
                 }
-                }
-
             }
         }
-        return -1;
+        return 0;
+
     }
 };
