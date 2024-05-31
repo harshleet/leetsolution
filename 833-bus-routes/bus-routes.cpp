30d750894915574a63f107e1dfbb7fa109419c3c
harshleet <122361301+harshleet@users.noreply.github.com>
2024-05-31 13:53:14 +0530

    Time: 273 ms (56.14%) | Memory: 75.1 MB (36.49%) - LeetSync

--- 833-bus-routes/bus-routes.cpp
+++ 833-bus-routes/bus-routes.cpp
@@ -1,39 +1,58 @@
 class Solution {
 public:
+    
+    // int dfs(int src,int tar,int bus,map<int,int>&vis,map<int,vector<pair<int,int>>>&adj){
+    //     if(src==tar){
+    //         return 0;
+    //     }
+    //     vis[src]=1;
+    //     int mini=1e8;
+    //     for(auto it:adj[src]){
+            
+    //         if(bus!=it.second && !vis[it.first]){
+    //             mini=min(mini,1+dfs(it.first,tar,it.second,vis,adj));
+    //         }else if(bus==it.second && !vis[it.first]){
+    //             mini=min(mini,dfs(it.first,tar,bus,vis,adj));
+    //         }
+    //     }
+    //      vis[src]=0;
+    //     return mini;
+
+    // }
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
         map<int,vector<int>>adj;
-        for (int i = 0; i < routes.size(); i++) {
-            for (int j = 0; j < routes[i].size(); j++) {
-                adj[routes[i][j]].push_back(i);
+        for(int i=0;i<routes.size();i++){
+            for(int j=0;j<routes[i].size();j++){
+                  adj[routes[i][j]].push_back(i);
             }
+        //    adj[routes[i][routes[i].size()-1]].push_back({routes[i][0],i});
         }
-
-        queue<pair<int, int>> q;
-        map<int, bool> m;
-        map<int, bool> vis;
-        q.push({source, 0});
-        vis[source] = true;
-
-        while (!q.empty()) {
-            int stop = q.front().first;
-            int lev = q.front().second;
+         map<int,int>vis;
+       
+        queue<pair<int,int>>q;
+        q.push({0,source});
+        vis[source]=1;
+        vector<bool>bus(routes.size(),false);
+        while(!q.empty()){
+            int node=q.front().second;
+            int dis=q.front().first;
             q.pop();
-
-            if (stop == target) {
-                               return lev;
-                            }
-
-            for (auto itr : adj[stop]) {
-                if (m[itr] == false) {
-                    m[itr] = true;
-                    for (auto it : routes[itr]) {
-                        if (vis[it] == false) {
-                            
-                            vis[it] = true;
-                            q.push({it, lev + 1});
-                        }
+           
+           if(node==target){
+            return dis;
+           }
+            for(auto it:adj[node]){
+                if(bus[it]==false){
+                    bus[it]=true;
+               
+                for(int i=0;i<routes[it].size();i++){
+                    // cout<<routes[it][i]<<" ";
+                    if(vis.find(routes[it][i])==vis.end()){
+                        vis[routes[it][i]]=1; 
+                        q.push({dis+1,routes[it][i]});
                     }
                 }
+                 }
             }
         }
         return -1;
