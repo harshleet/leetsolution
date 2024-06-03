c7973d2e8dbbcad33802f624fff1b78480d19368
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-03 17:06:25 +0530

    Time: 11 ms (71.89%) | Memory: 16 MB (80.72%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -15,25 +15,30 @@ public:
         }
         TreeNode* lp=helper(root->left,p,q);
         TreeNode* rp=helper(root->right,p,q);
+        //SO from both root we have some non-NULL vAL SO WE RETURN ROOT
         if(lp!=NULL && rp!=NULL){
             return root;
         }
-
+        //if root is itself p and e are getting some non NULL valur feom one child hence we return root
         if(root==p && (lp!=NULL && rp!=NULL)){
             return root;
         }
+         //if root is itself q and e are getting some non NULL valur feom one child hence we return root
         if(root==q && (lp!=NULL && rp!=NULL)){
             return root;
         }
+        //no last chaeck no lp and rp has something bothe are  NULL but root is either p or q than we return it
         if(root==p ||root==q){
             return root;
         }
+        //no root do not havenyhing we return their childrens
         if(lp!=NULL){
             return lp;
         }
         if(rp!=NULL){
             return rp;
         }
+        //if nohing in all of them we return the NULL
         return NULL;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
