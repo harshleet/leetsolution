94dc0deac14e55c0b3ba8e468ede96a94be207c9
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-06 02:27:32 +0530

    Time: 16 ms (35.05%) | Memory: 25 MB (26.87%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -15,26 +15,21 @@ public:
         if(root==NULL){
             return 0;
         }
-       
         if(dp.find(root)!=dp.end()){
             return dp[root];
         }
-        int ntake=INT_MIN,take=root->val;
-          ntake=max(ntake,helper(root->left,dp)+helper(root->right,dp));
+        int nt=helper(root->left,dp)+helper(root->right,dp);
+        int t=root->val;
         if(root->left!=NULL){
-                  take+=helper(root->left->left,dp)+helper(root->left->right,dp);
+            t+=helper(root->left->left,dp)+helper(root->left->right,dp);
         }
         if(root->right!=NULL){
-                  take+=helper(root->right->left,dp)+helper(root->right->right,dp);
+            t+=helper(root->right->left,dp)+helper(root->right->right,dp);
         }
-        return dp[root]=max(take,ntake);
+        return dp[root]=max(nt,t);
     }
     int rob(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-       map<TreeNode*,int>dp;
-       return helper(root,dp);
-
+        map<TreeNode*,int>dp;
+        return helper(root,dp);
     }
 };
