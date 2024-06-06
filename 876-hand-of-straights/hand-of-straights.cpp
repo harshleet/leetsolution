ea0541830d6a92e69767a86d7299a9354d1eea0b
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-06 13:54:06 +0530

    Time: 32 ms (97.79%) | Memory: 25.4 MB (92.18%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        map<int,int>m;
-        for(int i=0;i<hand.size();i++){
-            m[hand[i]]++;
-        }
-        for(auto it:m){
-            pq.push({it.first,it.second});
-        }
-        if(hand.size()%groupSize!=0){
+        if (hand.size()%groupSize!=0) {
             return false;
         }
-        while(!pq.empty()){
-            vector<pair<int,int>>todo;
-            todo.push_back(pq.top());
-            int last=pq.top().first;
-            cout<<last<<endl;
-            pq.pop();
-            for(int i=1;i<groupSize;i++){
-                cout<<pq.top().first<<endl;
-                 if(!pq.empty() && pq.top().first-last==1){
-                    todo.push_back(pq.top());
-                    last=pq.top().first;
-                    cout<<last<<endl;
-                    pq.pop();
-                 }else{
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < hand.size(); i++) {
+            pq.push(hand[i]);
+        }
+        while (!pq.empty()) {
+            vector<int> temp;
+            int cnt = 0, pre = -1;
+            
+            while (cnt < groupSize && !pq.empty()) {
+                int vl = pq.top();
+                pq.pop();
+                if (pre == -1 || vl - pre == 1) {
+                    cnt++;
+                    pre = vl;
+                } else if(pre==vl){
+                    temp.push_back(vl);
+                }else {
                     return false;
-                 }
+                }
             }
-            for(int i=0;i<todo.size();i++){
-               if(todo[i].second>1){
-                   pq.push({todo[i].first,todo[i].second-1});
-               }
+            if(cnt!=groupSize){
+                return false;
+            }
+            for(int i=0;i<temp.size();i++){
+                cout<<temp[i]<<endl;
+                pq.push(temp[i]);
             }
         }
         return true;
-
     }
 };
