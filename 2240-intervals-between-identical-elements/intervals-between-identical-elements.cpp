a4ce5eb6d8fc24061f34641bc003c2c6cac37fb7
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-08 13:23:49 +0530

    Time: 525 ms (5.46%) | Memory: 144.5 MB (32.28%) - LeetSync

---
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.cpp
@@ -0,0 +1,28 @@
+typedef long long ll;
+class Solution {
+public:
+    vector<long long> getDistances(vector<int>& arr) {
+        int n=arr.size();
+        vector<long long >ans(n,0);
+        map<ll,ll>m;
+        map<ll,ll>cnt;
+        for(int i=0;i<n;i++){
+            if(cnt.find(arr[i])!=cnt.end()){
+                ans[i]+=abs(cnt[arr[i]]*i-m[arr[i]]);
+            }
+            cnt[arr[i]]++;
+            m[arr[i]]+=i;
+        }
+         map<ll,ll>m1;
+        map<ll,ll>cnt1;
+        for(int i=n-1;i>=0;i--){
+            if(cnt1.find(arr[i])!=cnt1.end()){
+                ans[i]+=abs(cnt1[arr[i]]*i-m1[arr[i]]);
+            }
+            cnt1[arr[i]]++;
+            m1[arr[i]]+=i;
+        }
+
+        return ans;
+    }
+};
