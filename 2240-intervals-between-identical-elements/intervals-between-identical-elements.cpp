ac4fd8589476affd32ad1d1bbdf31302f63343d1
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-08 13:56:24 +0530

    Time: 195 ms (86.82%) | Memory: 128 MB (65.91%) - LeetSync

--- 2240-intervals-between-identical-elements/intervals-between-identical-elements.cpp
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.cpp
@@ -1,28 +1,24 @@
-typedef long long ll;
 class Solution {
 public:
     vector<long long> getDistances(vector<int>& arr) {
-        int n=arr.size();
-        vector<long long >ans(n,0);
-        map<ll,ll>m;
-        map<ll,ll>cnt;
-        for(int i=0;i<n;i++){
-            if(cnt[arr[i]]!=0){
-                ans[i]+=abs(cnt[arr[i]]*i-m[arr[i]]);
-            }
-            cnt[arr[i]]++;
-            m[arr[i]]+=i;
+        std::vector<long long> res(arr.size(), 0);
+        std::unordered_map<int, std::vector<long long>> num_to_indices;
+        
+        for(long long i=0; i < arr.size(); i++) {
+            num_to_indices[arr[i]].push_back(i);
         }
-         map<ll,ll>m1;
-        map<ll,ll>cnt1;
-        for(int i=n-1;i>=0;i--){
-            if(cnt1[arr[i]]!=0){
-                ans[i]+=abs(cnt1[arr[i]]*i-m1[arr[i]]);
+        
+        for(auto const& ele: num_to_indices) {
+            auto const& indices = ele.second;
+            for(int i = 1; i < indices.size(); i++) {
+                res[indices[0]] += std::abs(indices[i]-indices[0]);
             }
-            cnt1[arr[i]]++;
-            m1[arr[i]]+=i;
+            for(int i = 1; i < indices.size(); i++) {
+			    // Not simplifying this line intentionally for better understanding.
+                res[indices[i]] = res[indices[i-1]] - (indices[i]-indices[i-1])*(indices.size()-i-1) + (indices[i]-indices[i-1])*(i-1);
+            }
         }
-
-        return ans;
+        
+        return res;
     }
 };
