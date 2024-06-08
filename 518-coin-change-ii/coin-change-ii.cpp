ba29e33b88a29edf4c468a731d2937bbc80c20a1
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-08 16:33:31 +0530

    Time: 20 ms (20.32%) | Memory: 20 MB (31.15%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-   int rec(vector<int>&coins, vector<vector<int>>&dp,int amount,int ind){
-        
-         if(ind==0){
-             if(amount%coins[0]==0){
-                 return 1;
-             }
-             return 0;
-             
-         }
-         if(dp[ind][amount]!=-1){
-             return dp[ind][amount];
-         }
-         int ntake=rec(coins,dp,amount,ind-1);
-         int take=0;
-         if(coins[ind]<=amount){
-             take=rec(coins,dp,amount-coins[ind],ind);
-         }
-         return dp[ind][amount]=ntake+take;
-     }
+
+int helper(int amo,int ind,vector<int>&coins,vector<vector<int>>&dp){
+	if(amo==0){
+		return 1;
+	}
+	if(ind>=coins.size()){
+		return 0;
+	}
+    if(dp[ind][amo]!=-1){
+        return dp[ind][amo];
+    }
+	int ntake=helper(amo,ind+1,coins,dp);
+	int t=0;
+	if(amo>=coins[ind]){
+       t=helper(amo-coins[ind],ind,coins,dp);
+	}
+         
+	return dp[ind][amo]=ntake+t;
+}
     int change(int amount, vector<int>& coins) {
-         vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-          if(rec(coins,dp,amount,coins.size()-1)==1e9)
-              return -1;
-        else 
-            return rec(coins,dp,amount,coins.size()-1);
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+        return helper(amount,0,coins,dp);
     }
 };
