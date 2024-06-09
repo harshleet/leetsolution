0478fabac46c260ad20964b6f1f3092b6458e35c
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-09 23:25:48 +0530

    Time: 521 ms (12.25%) | Memory: 293.5 MB (29.57%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-   
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prev=ind-1;prev>=-1;prev--){
-               int ntake=dp[ind+1][prev+1];
-                int take=0;
-                if(prev==-1|| nums[prev]<nums[ind]){
-                    take=1+dp[ind+1][ind+1];
-                }
-                dp[ind][prev+1]=max(ntake,take);
-            }
+    int helper(int ind,int pre,vector<vector<int>>&dp,vector<int>&nums){
+        if(ind>=nums.size()){
+            return 0;
         }
-        return dp[0][0];
+        if(dp[ind][pre+1]!=-1){
+            return dp[ind][pre+1];
+        }
+        int nt=helper(ind+1,pre,dp,nums);
+        int t=0;
+        if(pre==-1 || nums[ind]>nums[pre]){
+            t=1+helper(ind+1,ind,dp,nums);
+        }
+        return dp[ind][pre+1]=max(t,nt);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>>dp(nums.size(),vector<int>(nums.size(),-1));
+        return helper(0,-1,dp,nums);
     }
 };
