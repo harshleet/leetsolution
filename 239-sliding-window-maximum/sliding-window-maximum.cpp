0cf08aae2f6799f4ab49573b6dd0bf97521a6bd9
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-15 11:02:16 +0530

    Time: 493 ms (12.13%) | Memory: 178 MB (13.46%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         map<int,int>m;
-        vector<int>ans;
         for(int i=0;i<k;i++){
             m[nums[i]]++;
         }
-        ans.push_back((--m.end())->first);
+         vector<int>ans;
+        if(!m.empty())
+              ans.push_back((--m.end())->first);
+       
         for(int i=k;i<nums.size();i++){
             m[nums[i-k]]--;
             if(m[nums[i-k]]==0){
                 m.erase(nums[i-k]);
             }
             m[nums[i]]++;
+            if(!m.empty())
               ans.push_back((--m.end())->first);
         }
         return ans;
