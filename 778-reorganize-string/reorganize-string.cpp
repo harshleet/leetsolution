29264f6e8ab6df62705a366600df6455a13b5b33
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-15 17:23:07 +0530

    Time: 5 ms (7.72%) | Memory: 9.1 MB (10.17%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
     string reorganizeString(string s) {
+        //same as task scheduler
+        priority_queue<pair<int,char>>pq;
         map<char,int>m;
+        int maxi=0;
         for(int i=0;i<s.size();i++){
             m[s[i]]++;
+            maxi=max(maxi,m[s[i]]);
         }
-        priority_queue<pair<int,char>>pq;
+        if(maxi>ceil(s.size()/(double)2)){
+            return "";
+        }
         for(auto it:m){
             pq.push({it.second,it.first});
         }
-         string ans="";
+        string ans="";
+
         while(!pq.empty()){
-            
-            vector<pair<int,char>>left;
-             ans+=pq.top().second;
-             left.push_back(pq.top());
-             pq.pop();
-             if(pq.size()==0){
-                 break;
-             }else{
-                ans+=pq.top().second;
-                left.push_back(pq.top());
-                pq.pop();
-             }
-              for(int i=0;i<left.size();i++){
-                if(left[i].first>1){
-                    pq.push({left[i].first-1,left[i].second});
+            if(pq.top().first==1){
+                while(!pq.empty()){
+                    ans+=pq.top().second;
+                    pq.pop();
                 }
-               
-             }
-        }
-        if(ans.size()!=s.size()){
-            return "";
+                return ans;
+            }
+            vector<pair<int,char>>temp;
+            temp.push_back(pq.top());
+ 
+            ans+=pq.top().second;
+            pq.pop();
+            if(!pq.empty()){
+               temp.push_back(pq.top());
+               ans+=pq.top().second;
+               pq.pop();
+            }
+            for(int i=0;i<temp.size();i++){
+                // cout<<temp[i].first<<" "<<ans<<endl;
+                if(temp[i].first>1){
+                   pq.push({temp[i].first-1,temp[i].second});
+                }
+            }
         }
         return ans;
     }
