cc13ee2ef0b73e772ad893d00f2f1a37dc7b0a07
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-15 15:20:59 +0530

    Time: 310 ms (5.01%) | Memory: 52.7 MB (6.85%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
+        priority_queue<pair<int,char>>pq;
         map<char,int>m;
         for(int i=0;i<tasks.size();i++){
             m[tasks[i]]++;
         }
-        priority_queue<pair<int,char>>pq;
         for(auto it:m){
             pq.push({it.second,it.first});
         }
-        int timer=0;
+        int ans=0;
+
         while(!pq.empty()){
-          if(pq.top().first==1){
-            timer+=pq.size();
-            break;
-          }
-            vector<pair<int,char>>left;
-             cout<<pq.top().second<<" "<<timer<<endl;
-            left.push_back(pq.top());
-            pq.pop();
-            timer++;
-           
-            for(int i=0;i<n;i++){
-                 if(!pq.empty()){
-                   left.push_back(pq.top());
-                    pq.pop();  
-                   
-                }
-                 timer++;
+            if(pq.top().first==1){
+                return ans+pq.size();
             }
-            for(int i=0;i<left.size();i++){
-                if(left[i].first>1){
-                    pq.push({left[i].first-1,left[i].second});
-                } 
+            vector<pair<int,char>>temp;
+            temp.push_back(pq.top());
+            pq.pop();
+            ans++;
+            for(int i=0;i<n && !pq.empty();i++){
+               temp.push_back(pq.top());
+               pq.pop();
+            }
+            ans+=n;
+            for(int i=0;i<temp.size();i++){
+                cout<<temp[i].first<<" "<<ans<<endl;
+                if(temp[i].first>1){
+                   pq.push({temp[i].first-1,temp[i].second});
+                }
             }
         }
-        return timer;
+        return ans;
     }
 };
