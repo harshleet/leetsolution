c2b1c39a79fcbc46ab7f43de21744fd4eed7120f
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-19 18:55:40 +0530

    Time: 41 ms (21.01%) | Memory: 22.8 MB (11.67%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.cpp
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void lpsFinder(string &str,vector<int>&lps){
-         lps[0]=0;
-         int i=1,len=0;
 
-         while(i<str.length()){
-             if(str[i]==str[len]){
-                 len++;
-                 lps[i]=len;
-                 i++;
-             }else{
-                 if(len==0){
-                     lps[i]=0;
-                     i++;
-                 }else{
-                     len=lps[len-1];
-                 }
-             }
-         }
-    }
+void lps_fill(string pat,vector<int>&lps){
+            int i=1,len=0;
+            while(i<lps.size()){
+                if(pat[i]==pat[len]){
+                   
+                    len++;
+                    lps[i]=len;
+                    i++;
+                }else{
+                    if(len!=0){
+                        len=lps[len-1];
+                    }else{
+                        lps[i]=0;
+                        i++;
+                    }
+                }
+            }
+     }
     string longestPrefix(string s) {
-        vector<int>lps(s.size());
-        lpsFinder(s,lps);
-       
-        return s.substr(0,lps.back());
-         
-
+        int n=s.size();
+        vector<int>lps(n,0);
+        lps_fill(s,lps);
+        int lind=lps[n-1];
+        return s.substr(0,lind);
     }
 };
