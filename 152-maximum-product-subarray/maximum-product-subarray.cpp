2706549928c30750e903881c08a7d85d63c449e0
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-21 19:05:17 +0530

    Time: 5 ms (60.47%) | Memory: 17.2 MB (5.17%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n=nums.size(),ne=0;
-        int pro=1;
-        // for(int i=0;i<n;i++){
-        //    if(nums[i]<0){
-        //     ne++;
-        //    }
-        //    pro*=nums[i];
-        // }
-        // if(ne%2==0){
-        //     return pro;
-        // }
-        if(nums.size()==1){
-            return nums[0];
+        int n=nums.size();
+       vector< long long int>suf(n);
+       vector< long long int>pre(n);
+       long long int sufp=1,prep=1,maxi=INT_MIN;
+       for(int i=0;i<n;i++){
+        if(sufp<=-1e9){
+            sufp=1;
         }
-        int st=ne,en=ne,ps=1,pe=1,maxi=0;
-        for(int i=0;i<n;i++){
-            if(ps==0){
-                ps=1;
-            }
-            if(pe==0){
-                pe=1;
-            }
-            ps*=nums[i];
-            pe*=nums[n-1-i];
-            maxi=max(maxi,ps);
-            maxi=max(maxi,pe);
+        if(prep<=-1e9){
+            prep=1;
         }
-        return maxi;
+        sufp*=nums[i];
+        prep*=nums[n-1-i];
+        maxi=max(maxi,sufp);
+        maxi=max(maxi,prep);
+        if(sufp==0){
+            sufp=1;
+        }
+        if(prep==0){
+            prep=1;
+        }
+        // suf[i]=sufp;
+        // pre[i]=prep;
+        
+       }
+       return maxi;
     }
 };
