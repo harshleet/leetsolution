b37073bf5591d46d9152211adb2cb56a7f5a6c97
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-22 12:18:17 +0530

    Time: 19 ms (32.58%) | Memory: 33.7 MB (23.49%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    bool helper(int ind1,int ind2,int ind3,string s1, string s2, string s3,vector<vector<vector<int>>>&dp){
-        cout<<ind1<<" "<<ind2<<" "<<ind3<<endl;
+    bool helper(int ind1,int ind2,int ind3,string s1, string s2, string s3,vector<vector<int>>&dp){
+        // cout<<ind1<<" "<<ind2<<" "<<ind3<<endl;
         if(ind3==s3.size()){
             return true;
         }
-       if(dp[ind1][ind2][ind3]!=-1){
-        return dp[ind1][ind2][ind3];
+       if(dp[ind1][ind2]!=-1){
+        return dp[ind1][ind2];
        }
         bool ans=false;
         if(ind1<s1.size()&& s1[ind1]==s3[ind3] ){
@@ -16,14 +16,14 @@ public:
             ans=ans|helper(ind1,ind2+1,ind3+1,s1,s2,s3,dp);
         }
        
-        return dp[ind1][ind2][ind3]=ans;
+        return dp[ind1][ind2]=ans;
 
     }
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size()+s2.size()!=s3.size()){
             return false;
         }
-        vector<vector<vector<int>>>dp(s1.size()+1,vector<vector<int>>(s2.size()+1,vector<int>(s3.size(),-1)));
+        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
         return helper(0,0,0,s1,s2,s3,dp);
     }
 };
