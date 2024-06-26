5f77cbb7211ac4e54dbedfbb25441662e02e6a69
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-26 12:28:39 +0530

    Time: 45 ms (60.26%) | Memory: 17.4 MB (50.25%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -17,10 +17,15 @@ bool search(int row,vector<vector<int>>&matrix,int target){
     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         //log(m*n)
-        
-        for(int i=0;i<matrix.size();i++){
-            if(search(i,matrix,target)){
+        int i=0,j=matrix[0].size()-1;
+        while(i<matrix.size() && j>=0){
+            if(matrix[i][j]==target){
                 return true;
+            }
+            if(matrix[i][j]<target){
+                i++;
+            }else{
+                j--;
             }
         }
         return false;
