c0c56753a411c5d5f554a6ce0666837e8361be60
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-28 10:42:45 +0530

    Time: 294 ms (62.76%) | Memory: 132.6 MB (60.69%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        map<int,int>ind;
-        for(int i=0;i<roads.size();i++){
-            ind[roads[i][0]]++;
-            ind[roads[i][1]]++;
+        vector<int>ind(n,0);
+        for(auto it:roads){
+            ind[it[0]]++;
+            ind[it[1]]++;
         }
-        vector<pair<int,int>>indegree;
-        for(auto it:ind){
-            indegree.emplace_back(it.second,it.first);
+        sort(ind.begin(),ind.end());
+        long long l=1,ans=0;
+        for(int i=0;i<ind.size();i++){
+            ans=ans+(ind[i]*l);
+            l++;
         }
-
-        sort(indegree.begin(),indegree.end());
-        int i=n;
-        vector<int>ans(n);
-
-        for(auto it = indegree.rbegin(); it != indegree.rend(); ++it) {
-            ans[it->second]=i;
-            i--;
-        }
-        long long a=0;
-         for(int i=0;i<roads.size();i++){
-            a+=ans[roads[i][0]];
-           a+= ans[roads[i][1]];
-        }
-        return a;
+        return ans;
     }
 };
