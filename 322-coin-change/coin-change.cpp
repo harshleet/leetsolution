76fd30a3ffd2c43bb50db46d6436f3d2dd044710
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-29 16:29:49 +0530

    Time: 48 ms (29.70%) | Memory: 46.8 MB (37.01%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-     int rec(vector<int>&coins, vector<vector<int>>&dp,int amount,int ind){
-        
-         if(ind==0){
-             if(amount%coins[0]==0){
-                 return amount/coins[0];
-             }
-             return 1e9;
-             
-         }
-         if(dp[ind][amount]!=-1){
-             return dp[ind][amount];
-         }
-         int ntake=rec(coins,dp,amount,ind-1);
-         int take=INT_MAX;
-         if(coins[ind]<=amount){
-             take= 1+rec(coins,dp,amount-coins[ind],ind);
-         }
-         return dp[ind][amount]=min(ntake,take);
-     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-        if(rec(coins,dp,amount,coins.size()-1)==1e9)
-              return -1;
-        else 
-            return rec(coins,dp,amount,coins.size()-1);
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,1e9));
+        for(int i=0;i<coins.size();i++){
+            dp[i][0]=0;
+        }
+        for(int sum=1;sum<=amount;sum++){
+            if(sum%coins[0]==0){
+                dp[0][sum]=sum/coins[0];
+            }
+        }
+        for(int ind=1;ind<coins.size();ind++){
+            for(int sum=1;sum<=amount;sum++){
+                int nt=dp[ind-1][sum];
+                int t=1e9;
+                if(sum>=coins[ind]){
+                    t=1+dp[ind][sum-coins[ind]];
+                }
+                dp[ind][sum]=min(t,nt);
+            }
+        }
+        if(dp[coins.size()-1][amount]==1e9){
+            return -1;
+        }
+        return dp[coins.size()-1][amount];
     }
 };
