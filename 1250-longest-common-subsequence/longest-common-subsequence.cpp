50abeaf91c262f600f8fa988def8b394602bf678
harshleet <122361301+harshleet@users.noreply.github.com>
2024-06-29 17:35:10 +0530

    Time: 21 ms (71.14%) | Memory: 25.7 MB (53.98%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    int f(string& s1, string& s2, int i, int j, vector<vector<int>>& dp) {
-        if (i < 0 || j < 0) {
+    int helper(int ind1,int ind2,string text1,string text2,int n,int m){
+        if(ind1>=n || ind2>=m){
             return 0;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        int nt1=helper(ind1+1,ind2,text1,text2,n,m);
+        int nt2=helper(ind1,ind2+1,text1,text2,n,m);
+        int t=0;
+        if(text1[ind1]==text2[ind2]){
+            t=1+helper(ind1+1,ind2+1,text1,text2,n,m);
         }
-        int ntake = max(f(s1, s2, i - 1, j, dp), f(s1, s2, i, j - 1, dp));
-        int take = 0;
-        if (s1[i] == s2[j]) {
-            take = 1 + f(s1, s2, i - 1, j - 1, dp);
-        }
-        return dp[i][j] = max(ntake, take);
+        return max({nt1,nt2,t});
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size(), m = text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        int n=text1.size(),m=text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int nt1=dp[i-1][j];
+                int nt2=dp[i][j-1];
+                int t=0;
+                if(text1[i-1]==text2[j-1]){
+                    t=1+dp[i-1][j-1];
                 }
+                dp[i][j]= max({nt1,nt2,t});
             }
         }
         return dp[n][m];
