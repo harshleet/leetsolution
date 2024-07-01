cdff1094da836e2494913605c356b8867824d5fb
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-02 02:50:02 +0530

    Time: 32 ms (55.32%) | Memory: 21.5 MB (22.11%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,52 +1,67 @@
 class Solution {
 public:
- int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int>preS(n,-1);
-        vector<int>nextS(n,-1);
+    int solve(vector<int>&temp){
         stack<int>st;
+           int n=temp.size();
+        vector<int>ps(temp.size(),-1);
+        vector<int>ns(temp.size(),n);
+     
         for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
-                nextS[st.top()]=i;
-                st.pop();
+            if(st.empty() || temp[st.top()]<temp[i]){
+                if(!st.empty()){
+                    ps[i]=st.top();
+                }
+                st.push(i);
+            }else{
+                while(!st.empty() && temp[st.top()]>=temp[i]){
+                    st.pop();
+                }
+                if(!st.empty()){
+                    ps[i]=st.top();
+                }
+                st.push(i);
             }
-            if(!st.empty()){
-                preS[i]=st.top();
+        }
+        stack<int>st2;
+        for(int i=n-1;i>=0;i--){
+            if(st2.empty() || temp[st2.top()]<temp[i]){
+                if(!st2.empty()){
+                    ns[i]=st2.top();
+                }
+                st2.push(i);
+            }else{
+                while(!st2.empty() && temp[st2.top()]>=temp[i]){
+                    st2.pop();
+                }
+                if(!st2.empty()){
+                    ns[i]=st2.top();
+                }
+                st2.push(i);
             }
-            st.push(i);
         }
         int maxi=0;
         for(int i=0;i<n;i++){
-            int sel=heights[i];
-            
-            if(preS[i]!=-1){
-                sel+=heights[i]*(i-preS[i]-1);
-            }else if(preS[i]==-1){
-                 sel+=heights[i]*(i);
-            }
-            if(nextS[i]!=-1){
-                sel+=heights[i]*(nextS[i]-i-1);
-            }else if(nextS[i]==-1){
-                 sel+=heights[i]*(n-i-1);
-            }
-            maxi=max(maxi,sel);
+            // cout<<temp[i]<<" "<<ps[i]<<" "<<ns[i]<<" ";
+           maxi=max(maxi,(ns[i]-ps[i]-1)*temp[i]);
         }
+        cout<<endl;
+        
         return maxi;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int>temp(m,0);
+        vector<vector<int>>temp(n,vector<int>(m,0));
         int maxi=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]=='0'){
-                    temp[j]=0;
-                }else{
-                    temp[j]+=1;
+                if(i==0 && matrix[i][j]=='1'){
+                    temp[i][j]=matrix[i][j]-'0';
+                }else if(matrix[i][j]=='1'){
+                    temp[i][j]=temp[i-1][j]+(matrix[i][j]-'0');
                 }
             }
-            maxi=max(maxi,largestRectangleArea(temp));
+            maxi=max(maxi,solve(temp[i]));
         }
         return maxi;
     }
