00bc3710ddd0b0c8f92f367d0d9fe1355f37e68a
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-02 13:06:09 +0530

    Time: 165 ms (76.14%) | Memory: 137.1 MB (44.27%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<pair<int,int>>dq;
+        deque<int>dq;
+        vector<int>ans;
         for(int i=0;i<k;i++){
-            if(dq.empty() || dq.front().first>nums[i]){
-                dq.push_front({nums[i],i});
+            if(dq.empty() || nums[dq.back()]>nums[i]){
+                dq.push_back(i);
             }else{
-                while(!dq.empty()&& dq.front().first<=nums[i]){
-                    dq.pop_front();
+                while(!dq.empty() && nums[dq.back()]<=nums[i]){
+                    dq.pop_back();
                 }
-                dq.push_front({nums[i],i});
+                dq.push_back(i);
             }
         }
-        vector<int>ans;
-        ans.push_back(dq.back().first);
+        ans.push_back(nums[dq.front()]);
         for(int i=k;i<nums.size();i++){
-            if(dq.back().second==i-k){
-                dq.pop_back();
+            if(dq.front()==i-k){
+                dq.pop_front();
             }
-            if(dq.empty() || dq.front().first>nums[i]){
-                dq.push_front({nums[i],i});
+            if(dq.empty() || nums[dq.back()]>nums[i]){
+                dq.push_back(i);
             }else{
-                while(!dq.empty()&& dq.front().first<=nums[i]){
-                    dq.pop_front();
+                while(!dq.empty() && nums[dq.back()]<=nums[i]){
+                    dq.pop_back();
                 }
-                dq.push_front({nums[i],i});
+                dq.push_back(i);
             }
-            ans.push_back(dq.back().first);
+            ans.push_back(nums[dq.front()]);
         }
         return ans;
+
     }
 };
