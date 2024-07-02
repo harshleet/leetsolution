72b04c7aaea30960d03e549dc93db742ec7a93b7
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-02 12:30:53 +0530

    Time: 35 ms (5.59%) | Memory: 15.7 MB (5.43%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        stack<char>s;
+        if(num.size()==k){
+            return "0"; 
+        }
+        set<int>removed;
+        stack<int>st;
         for(int i=0;i<num.size();i++){
-         
-           while(!s.empty() && s.top()>num[i] && k>0){
-             s.pop();
-             k--;
-           }
-           s.push(num[i]);
+            if(st.empty() || (num[st.top()]-'0')<=(num[i]-'0')){
+                st.push(i);
+            }else{
+                while(!st.empty() && k>0 && (num[st.top()]-'0')>(num[i]-'0')){
+                    removed.insert(st.top());
+                    k--;
+                    st.pop();
+                }
+                st.push(i);
+            }
         }
+        // cout<<st.size()<<" "<<k<<endl;
+        while(!st.empty() && k>0){
+            // cout<<st.top()<<endl;
+           removed.insert(st.top());
+                    k--;
+                    st.pop();
+        }
+        // cout<<removed.size()<<" ";
         string ans="";
-        while(!s.empty()){
-            ans+=s.top();
-            s.pop();
+        for(int i=0;i<num.size();i++){
+            
+            if(removed.find(i)==removed.end()){
+                if(ans=="" && num[i]=='0'){
+                    continue;
+                }
+                ans+=num[i];
+            }
         }
-        
-        if(k>0){
-            ans=ans.substr(k);
-        }
-        
-        reverse(ans.begin(),ans.end());
-        int i=0;
-        while(ans.size()>1 && ans[i]=='0'){
-           i++;
-        }
-        ans=ans.substr(i);
         if(ans==""){
             return "0";
         }
         return ans;
-        
     }
 };
