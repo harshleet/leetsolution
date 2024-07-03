3c9eb172d0f23f666d9034e11f8cc3bdc1f0d38b
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-03 19:12:48 +0530

    Time: 50 ms (19.15%) | Memory: 15.9 MB (7.26%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
+   
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(nums.size(), 1);
-        vector<int>cnt(nums.size());
-        cnt[0]=1;
-        int gmax = 1;
-        for (int ind = 1; ind < n; ind++) {
-            int maxi = 1,c=1;
-            for (int pre = ind - 1; pre >= 0; pre--) {
+        int n=nums.size();
+        vector<int> temp;
+    temp.push_back(nums[0]);
 
-                if (nums[ind] > nums[pre]) {
-                   if(maxi<1 + dp[pre]){
-                     maxi=1+dp[pre];
-                     c=cnt[pre];
-                   }else if(maxi==1+dp[pre]){
-                    c+=cnt[pre];
-                   }
-                }
-            }
-            cnt[ind]=c;
-            dp[ind] = maxi;
-            gmax = max(gmax, dp[ind]);
-        }
-        cout<<gmax<<" ";
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if(dp[i]==gmax){
-                ans+=cnt[i];
+    int len = 1;
+    vector<int>dp(n,1);
+    dp[0]=1;
+    vector<int>cnt(n,1);
+    for (int i = 1; i < n; i++) {
+       
+        for(int j=0;j<i;j++){
+            if(nums[j]<nums[i] && dp[j]+1>dp[i]){
+                dp[i]=1+dp[j];
+                cnt[i]=cnt[j];
+            }else if(nums[j]<nums[i] && dp[j]+1==dp[i]){
+                cnt[i]+=cnt[j];
             }
         }
-        return ans;
+        len=max(len,dp[i]);
+        // cout<<cnt[i]<<" ";
+    }
+    int ans=0;
+    for(int i=0;i<n;i++){
+        if(dp[i]==len){
+            ans+=cnt[i];
+        }
+    }
+    return ans;
     }
 };
