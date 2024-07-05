370c93a5baa699dec41e3230bbd4f111897780c5
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-05 18:03:34 +0530

    Time: 279 ms (55.32%) | Memory: 162.2 MB (27.66%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-int maxi=-1;
-    void dfs(int node,vector<int>adj[],vector<int>&vis,vector<int>&len,int lvl,vector<int>&pvis){
-         vis[node]=1;
-               len[node]=lvl;
-               pvis[node]=1;
+    int maxi=-1;
+    void dfs(int node,int par,vector<vector<int>>&adj,vector<int>&pvis,vector<int>&vis,int n){
+        vis[node]=1;
+        if(par!=-1){
+            pvis[node]=1+pvis[par];
+        }else{
+            pvis[node]=1;
+        }
+        
         for(auto it:adj[node]){
             if(!vis[it]){
+                dfs(it,node,adj,pvis,vis,n);
+            }else if(vis[it]==1 && pvis[it]!=0){
                
-               dfs(it,adj,vis,len,lvl+1,pvis);
-                
-            }else if(pvis[it]){
-                    maxi=max(maxi,lvl-len[it]);
-             }
-        }
-       pvis[node]=0;
-      
-    }
-    int longestCycle(vector<int>& edges) {
-        
-        int n=edges.size();
-        vector<int>adj[n];
-        for(int i=0;i<n;i++){
-            if(edges[i]!=-1)
-               adj[i].push_back(edges[i]);
-        }
-        vector<int>vis(n,0);
-        vector<int>len(n,0);
-        vector<int>pvis(n,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis,len,0,pvis);
+                maxi=max(maxi,1+pvis[node]-pvis[it]);
             }
         }
-        if(maxi==-1){
-            return -1;
+        pvis[node]=0;
+    }
+    int longestCycle(vector<int>& edges) {
+        int n=edges.size();
+        vector<vector<int>>adj(n);
+        vector<int>vis(n,0);
+        vector<int>pvis(n,0);
+        for(int i=0;i<n;i++){
+            if(edges[i]!=-1)
+                adj[i].push_back(edges[i]);
         }
-        return maxi+1;
+        for(int i=0;i<n;i++){
+            dfs(i,-1,adj,pvis,vis,n);
+        }
+        return maxi;
     }
 };
