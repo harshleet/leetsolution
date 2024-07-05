6bb776b3e19cdbdb2c1f696c105719e48eb4bc72
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-05 10:19:25 +0530

    Time: 155 ms (87.67%) | Memory: 115.7 MB (95.45%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,29 +11,39 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return {-1,-1};
+        }
         vector<int>st;
         ListNode* temp=head;
-        while(temp!=NULL){
-            st.push_back(temp->val);
-            temp=temp->next;
-        }
-        int mini=-1,maxi=-1,first=-1,pre=-1;
-        for(int i=1;i<st.size()-1;i++){
-            if((st[i]>st[i-1]&& st[i]>st[i+1]) || (st[i+1]>st[i] && st[i-1]>st[i])){
-                if(first==-1){
-                    first=i;
-                }
-                if(pre!=-1){
-                    if(mini==-1){
-                        mini=i-pre;
-                    }else{
-                         mini=min(mini,i-pre);
+        ListNode* nxt=head->next;
+        ListNode* prev=NULL;
+         int mini=-1,maxi=-1,first=-1,pre=-1;
+         int i=0;
+        while(temp!=NULL && nxt!=NULL){
+            if(prev!=NULL){
+                if((temp->val>prev->val && temp->val>nxt->val) || (temp->val<prev->val && temp->val<nxt->val)){
+                    if(first==-1){
+                        first=i;
                     }
-                   
+                    if(pre!=-1){
+                        if(mini==-1){
+                            mini=i-pre;
+                        }else{
+                            mini=min(mini,i-pre);
+                        }
+                    
+                    }
+                    pre=i;
                 }
-                pre=i;
             }
+            i++;
+            prev=temp;
+            temp=temp->next;
+            nxt=nxt->next;
         }
+       
+      
         if(first!=-1 && pre!=-1 && pre!=first){
             maxi=max(maxi,pre-first);
         }
