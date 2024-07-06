8001bc30a15bf5686077975b7005e3e507567fc8
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-06 14:40:19 +0530

    Time: 22 ms (85.30%) | Memory: 15.8 MB (39.95%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>adj(n,vector<int>(n,1e9));
+        vector<vector<int>>dis(n,vector<int>(n,1e9));
         for(int i=0;i<n;i++){
-            adj[i][i]=0;
+            dis[i][i]=0;
         }
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]][edges[i][1]]=edges[i][2];
-            adj[edges[i][1]][edges[i][0]]=edges[i][2];
+        for(auto it:edges){
+            dis[it[0]][it[1]]=it[2];
+            dis[it[1]][it[0]]=it[2];
         }
+
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 for(int k=0;k<n;k++){
-                    adj[j][k]=min(adj[j][k],adj[j][i]+adj[i][k]);
+                    if(dis[j][i]+dis[i][k]<dis[j][k]){
+                        dis[j][k]=dis[j][i]+dis[i][k];
+                    }
                 }
             }
         }
-        int mini=INT_MAX,city=-1;
-        for(int i=0;i<n;i++){
-            int cnt=0;
+        int mini=INT_MAX,minii=-1;
+         for(int i=0;i<n;i++){
+            int c=0;
             for(int j=0;j<n;j++){
-                if(i!=j && adj[i][j]<=distanceThreshold){
-                    cnt++;
+                if(i!=j && dis[i][j]<=distanceThreshold){
+                    c++;
                 }
             }
-            if(cnt<=mini){
-                mini=cnt;
-                city=i;
+            cout<<c<<" ";
+            if(c<=mini){
+                mini=c;
+                minii=i;
             }
-        }
-        return city;
+         }
+         return minii;
     }
 };
