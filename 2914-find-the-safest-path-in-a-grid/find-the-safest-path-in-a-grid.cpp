71b6f122b323d4845d635976afd61326b343c0e8
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-08 14:46:30 +0530

    Time: 550 ms (33.06%) | Memory: 156.6 MB (36.91%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,71 +1,66 @@
-
 class Solution {
 public:
     int maximumSafenessFactor(vector<vector<int>>& grid) {
+        //very good qustion
+
         int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>man(n,vector<int>(m,0));
-        vector<vector<int>>vis(n,vector<int>(m,0));
-           queue<tuple<int, int, int>> q;
-                for(int i=0;i<n;i++){
+        queue<tuple<int,int,int>>q;
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        vector<int>dc={0,0,1,-1};
+        vector<int>dr={1,-1,0,0};
+
+        vector<vector<int>>dis(n,vector<int>(n));
+        for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                      q.push({0,i,j});
-                      vis[i][j]=1;
+                    q.push({i,j,0});
+                    vis[i][j]=1;
                 }
             }
         }
-        vector<int>dr={1,-1,0,0};
-        vector<int>dc={0,0,1,-1};
-        while(!q.empty()){
-            int dis=get<0>(q.front());
-            int r=get<1>(q.front());
-            int c=get<2>(q.front());
-            q.pop();
-           for(int i=0;i<4;i++){
-                int nr=r+dr[i];
-                int nc=c+dc[i];
-                if(nr>=0 && nc>=0 && nr<n && nc<m && !vis[nr][nc]){
-                    man[nr][nc]=dis+1;
-                    vis[nr][nc]=1;
-                    q.push({dis+1,nr,nc});
-                }
-            }
 
-        }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         cout<<man[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        vector<vector<int>>dist(n,vector<int>(m,0));
-        priority_queue<tuple<int,int,int>>pq;
-        pq.push({man[0][0],0,0});
-        while(!pq.empty()){
-             int dis=get<0>(pq.top());
-            int r=get<1>(pq.top());
-            int c=get<2>(pq.top());
-            pq.pop();
-            // cout<<r<<" "<<c<<" "<<dis<<endl;
-            if(r==n-1 && c==m-1){
-                return dis;
-            }
+        while(!q.empty()){
+            auto it=q.front();
+            int r=get<0>(it);
+            int c=get<1>(it);
+            int dist=get<2>(it);
+            q.pop();
+
+            dis[r][c]=dist;
+
             for(int i=0;i<4;i++){
                 int nr=r+dr[i];
                 int nc=c+dc[i];
-                if(nr>=0 && nc>=0 && nr<n && nc<m ){
-                    int mini=min(dis,man[nr][nc]);
-                    if(dist[nr][nc]<mini){
-                        dist[nr][nc]=mini;
-                        pq.push({mini,nr,nc});
+
+                if(nr>=0 && nc>=0 && nr<n && nc<n && !vis[nr][nc]){
+                    vis[nr][nc]=1;
+                    q.push({nr,nc,dist+1});
+                }
+            }
+        }
+
+        vector<vector<int>>dis2(n,vector<int>(n,0));
+        priority_queue<tuple<int,int,int>>pq;
+
+        pq.push({dis[0][0],0,0});
+
+        while(!pq.empty()){
+            auto [dist,r,c]=pq.top();
+            pq.pop();
+
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+
+                if(nr>=0 && nc>=0 && nr<n && nc<n){
+                    int maxi=min(dist,dis[nr][nc]);
+                    if(dis2[nr][nc]<maxi){
+                        dis2[nr][nc]=maxi;
+                        pq.push({maxi,nr,nc});
                     }
                 }
             }
-
         }
-        
-        return 0;
-
+        return dis2[n-1][n-1];
     }
 };
