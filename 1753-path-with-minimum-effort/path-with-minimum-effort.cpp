4ad42d1186aea72abdc42ecb62ae726e0104de52
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-08 11:34:16 +0530

    Time: 101 ms (41.21%) | Memory: 23.6 MB (71.65%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -2,36 +2,35 @@ class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size(),m=heights[0].size();
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
-        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-
-        dist[0][0]=0;
-        pq.push({0,0,0});
         vector<int>dr={1,-1,0,0};
         vector<int>dc={0,0,-1,1};
+
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+
+        int ans=INT_MAX;
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        pq.push({0,0,0});
+        dist[0][0]=0;
         while(!pq.empty()){
-            int maxE=get<0>(pq.top());
+            int dis=get<0>(pq.top());
             int r=get<1>(pq.top());
             int c=get<2>(pq.top());
             pq.pop();
-       
-           if(r==n-1 && c==m-1){
-            return maxE;
-           }
+
             for(int i=0;i<4;i++){
                 int nr=r+dr[i];
                 int nc=c+dc[i];
                 if(nr>=0 && nc>=0 && nr<n && nc<m){
-                    int maxi=max(maxE,abs(heights[nr][nc]-heights[r][c]));
-                   
-                    if(dist[nr][nc]>maxi){
-                        dist[nr][nc]=maxi;
-                        pq.push({maxi,nr,nc});
-                    }
+                    int mini=max(dis,abs(heights[nr][nc]-heights[r][c]));
+                     if(dist[nr][nc]>mini){
+                        dist[nr][nc]=mini;
+                        pq.push({mini,nr,nc});
+                     }
                 }
+               
             }
+            
         }
-        return 0;
-
+        return dist[n-1][m-1];
     }
 };
