6e5bcea13ff9d214d528393ac7c961ccf1e6eb05
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-09 08:04:53 +0530

    Time: 152 ms (52.35%) | Memory: 76.4 MB (95.59%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -11,7 +11,7 @@ public:
             }else if(total<=customers[i][0]){
                 total=customers[i][0]+customers[i][1];
             }
-            cout<<total<<" ";
+           
             twait+=total-customers[i][0];
         }
         return twait/(double)customers.size();
