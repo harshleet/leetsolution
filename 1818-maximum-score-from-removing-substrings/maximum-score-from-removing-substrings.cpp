b10998f1bec7ee43a4c7dc5cb944295f4c907fe3
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-12 13:11:32 +0530

    Time: 167 ms (10.98%) | Memory: 36.5 MB (5.28%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,95 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+       
+        if(x>=y){
+             stack<pair<char,int>>st;
+       
+        vector<int>used(s.size(),0);
+            int ans=0;
+            for(int i=0;i<s.size();i++){
+                if(s[i]!='a' && s[i]!='b'){
+                    while(!st.empty()){
+                        st.pop();
+                    }
+                  continue;
+                }else{
+                    if(s[i]=='a'){
+                        st.push({s[i],i});
+                    }else if(s[i]=='b' && !st.empty() && st.top().first=='a'){
+                        ans+=x;
+                        used[st.top().second]=1;
+                        used[i]=1;
+                        st.pop();
+                    }
+                }
+            }
+             string temp="";
+             for(int i=0;i<s.size();i++){
+                if(used[i]==0){
+                    temp+=s[i];
+                }
+             }
+
+             for(int i=0;i<temp.size();i++){
+                if(temp[i]!='a' && temp[i]!='b'){
+                    while(!st.empty()){
+                        st.pop();
+                    }
+                }else{
+                    if(temp[i]=='b'){
+                        st.push({temp[i],i});
+                    }else if(temp[i]=='a' && !st.empty() && st.top().first=='b'){
+                        ans+=y;
+                        st.pop();
+                    }
+                }
+            }
+            return ans;
+            
+        }
+          stack<pair<char,int>>st;
+       
+        vector<int>used(s.size(),0);
+        int ans=0;
+            for(int i=0;i<s.size();i++){
+                if(s[i]!='a' && s[i]!='b'){
+                    while(!st.empty()){
+                        st.pop();
+                    }
+                }else{
+                    if(s[i]=='b'){
+                        st.push({s[i],i});
+                    }else if(s[i]=='a' && !st.empty() && st.top().first=='b'){
+                        ans+=y;
+                        used[st.top().second]=1;
+                        used[i]=1;
+                        st.pop();
+                    }
+                }
+            }
+             string temp="";
+             for(int i=0;i<s.size();i++){
+                if(used[i]==0){
+                    temp+=s[i];
+                }
+             }
+          
+             for(int i=0;i<temp.size();i++){
+                if(temp[i]!='a' && temp[i]!='b'){
+                    while(!st.empty()){
+                        st.pop();
+                    }
+                }else{
+                    if(temp[i]=='a'){
+                        cout<<i<<" ";
+                        st.push({temp[i],i});
+                    }else if(temp[i]=='b' && !st.empty() && st.top().first=='a'){
+                        ans+=x;
+                        st.pop();
+                    }
+                }
+            }
+            return ans;
+    }
+};
