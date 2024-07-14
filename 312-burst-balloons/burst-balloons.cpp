384a8663331d217eff0b7d7c38787a866179e633
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-14 14:53:44 +0530

    Time: 213 ms (17.87%) | Memory: 12.3 MB (5.21%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int helper(int st,int end,vector<int>&nums,vector<vector<int>>&dp){
+    int helper(int st,int end,vector<int>&arr,vector<vector<int>>&dp){
         if(st>end){
             return 0;
         }
-        
         if(dp[st][end]!=-1){
             return dp[st][end];
         }
-        int mini=INT_MIN;
+        int maxi=INT_MIN;
         for(int i=st;i<=end;i++){
-            int l=helper(st,i-1,nums,dp);
-            int r=helper(i+1,end,nums,dp);
-            int ops=nums[i]*nums[st-1]*nums[end+1];
-            
-            mini=max(mini,l+r+ops);
-            
+            int op=arr[st-1]*arr[i]*arr[end+1]+helper(st,i-1,arr,dp)+helper(i+1,end,arr,dp);
+            maxi=max(maxi,op);
         }
-        return  dp[st][end]=mini;
+        return dp[st][end]=maxi;
+        
     }
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
-         vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
-         nums.push_back(1);
-         nums.insert(nums.begin(),1);
-        return helper(1,n,nums,dp);
+        vector<int>temp;
+        temp.push_back(1);
+        for(int i=0;i<nums.size();i++){
+            temp.push_back(nums[i]);
+        }
+        temp.push_back(1);
+        vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
+        return helper(1,n,temp,dp);
     }
 };
