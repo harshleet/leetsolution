82925a8a74da6996de88ab02940067d45c2d7d8a
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-16 00:47:13 +0530

    Time: 66 ms (55.97%) | Memory: 45.8 MB (29.38%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-int mod=1e9+7;
     int sumSubarrayMins(vector<int>& arr) {
         int n=arr.size();
         vector<int>ps(n,-1);
         vector<int>ns(n,n);
-
+        int mod=1e9+7;
         stack<int>st;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<arr.size();i++){
             if(st.empty() || arr[st.top()]<=arr[i]){
                 if(!st.empty()){
                     ps[i]=st.top();
                 }
                 st.push(i);
             }else{
-                while(!st.empty() && arr[st.top()]>arr[i]){
+                while(!st.empty() && arr[i]<arr[st.top()]){
                     st.pop();
                 }
                 if(!st.empty()){
@@ -23,15 +22,15 @@ int mod=1e9+7;
                 st.push(i);
             }
         }
-        stack<int>st1; 
-        for(int i=n-1;i>=0;i--){
+        stack<int>st1;
+        for(int i=arr.size()-1;i>=0;i--){
             if(st1.empty() || arr[st1.top()]<arr[i]){
                 if(!st1.empty()){
                     ns[i]=st1.top();
                 }
                 st1.push(i);
             }else{
-                while(!st1.empty() && arr[st1.top()]>=arr[i]){
+                while(!st1.empty() && arr[i]<=arr[st1.top()]){
                     st1.pop();
                 }
                 if(!st1.empty()){
@@ -40,15 +39,12 @@ int mod=1e9+7;
                 st1.push(i);
             }
         }
-        long long ans=0;
-        for(int i = 0; i < n; i++) {
-            
-            long long val = (i - ps[i]) % mod;
-            val = (val * (ns[i] - i)) % mod;
-            val = (val * arr[i]) % mod;
-            ans = (ans + val) % mod;
+        long long  ans=0;
+        for(int i=0;i<n;i++){
+            long long vl=(i-ps[i]);
+            long long vl2=(ns[i]-i);
+            ans=(ans+((vl*vl2)%mod)*arr[i])%mod;
         }
-
         return ans;
     }
 };
