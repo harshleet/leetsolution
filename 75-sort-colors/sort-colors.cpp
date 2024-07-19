a0ae638aa1b8a84c26fe4c7977fd710745f3b0e2
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-19 17:40:29 +0530

    Time: 8 ms (15.84%) | Memory: 10.2 MB (7.49%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int helper(int low,int high,vector<int>&nums){
-        int i=low;
-        int j=high;
-        int pivot=nums[low];
-
-        while(i<j){
-            while(pivot>=nums[i] && i<high){
-                i++;
-            }
-            while(pivot<nums[j] && j>low){
-                j--;
-            }
-            if(i<j){
-                swap(nums[i],nums[j]);
-            }
-        }
-        swap(nums[low],nums[j]);
-        return j;
-    }
-    void quick_sort(int low,int high,vector<int>&nums){
-        if(low<high){
-            int p=helper(low,high,nums);
-            quick_sort(low,p-1,nums);
-            quick_sort(p+1,high,nums);
-        }
-    }
     void sortColors(vector<int>& nums) {
-        quick_sort(0,nums.size()-1,nums);
-        return;
+        int low=0,high=nums.size()-1;
+        int temp=low;
+        while(low<=high && temp<=high){
+            cout<<low<<" "<<high<<" "<<temp<<endl;
+            if(nums[low]==0){
+                low++;
+                temp=low;
+            }else if(nums[high]==2){
+                high--;
+            }else if(nums[low]==2 && nums[high]==0){
+                swap(nums[low],nums[high]);
+                low++;
+                high--;
+                temp=low;
+            }else if(nums[temp]==2){
+                swap(nums[temp],nums[high]);
+                high--;
+            }else if(nums[temp]==0){
+                swap(nums[temp],nums[low]);
+                low++;
+                temp=low;
+            }else{
+                temp++;
+            }
+        }
+
     }
 };
