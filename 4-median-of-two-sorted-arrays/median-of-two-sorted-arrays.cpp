787800e0f93badd0040e9dd695fe6c0350039947
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-20 12:55:17 +0530

    Time: 16 ms (91.25%) | Memory: 94.8 MB (43.35%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,42 +1,48 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-        int n2=nums2.size();
+        int n1=nums1.size(),n2=nums2.size();
         if(n1>n2){
             return findMedianSortedArrays(nums2,nums1);
         }
-     
         int left=(n1+n2+1)/2;
-        int low=0,high=n1;
+
+        int low=0,high=min(left,n1);
+       
         while(low<=high){
-            int mid1=(low+high)/2;
-            int mid2=left-mid1;
-            int l1=INT_MIN,l2=INT_MIN,r2=INT_MAX,r1=INT_MAX;
-            if(n1!=0 && mid1<n1){
-                r1=nums1[mid1];
+            int mid=(low+high)/2;
+
+            // cout<<mid<<" ";
+            int sec=left-mid;
+
+            int lf=INT_MIN,ls=INT_MIN,rf=INT_MAX,rs=INT_MAX;
+            if(mid>0){
+                lf=nums1[mid-1];
             }
-            if(n2!=0 && mid2<n2){
-                r2=nums2[mid2];
+            if(sec>0){
+                ls=nums2[sec-1];
             }
-            if(n1!=0 && mid1-1>=0){
-                l1=nums1[mid1-1];
+            if(mid<n1){
+                rf=nums1[mid];
             }
-            if(n2!=0 && mid2-1>=0){
-                l2=nums2[mid2-1];
+            if(sec<n2){
+                rs=nums2[sec];
             }
-            if(l1<=r2 && l2<=r1){
+            // cout<<lf<<" "<<ls<<" "<<rf<<" "<<rs<<endl;
+
+            if(lf<=rs && ls<=rf){
+                //possible median
                 if((n1+n2)%2==0){
-                    return ((double)max(l1,l2)+min(r1,r2))/(double)2;
+                    return (max(lf,ls)+min(rs,rf))/(double)2;
                 }else{
-                    return (double)(max(l1,l2));
+                    return max(lf,ls);
                 }
-            }else if(l1>r2){
-                high=mid1-1;
+            }else if(rf<ls){
+                low=mid+1;
             }else{
-                low=mid1+1;
+                high=mid-1;
             }
         }
-        return 0.0;
+        return -1;
     }
 };
