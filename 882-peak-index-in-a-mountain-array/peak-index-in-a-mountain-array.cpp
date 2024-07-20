519d13097e2a2af138aa345b7edba2375d124768
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-20 11:15:30 +0530

    Time: 85 ms (36.33%) | Memory: 62.3 MB (8.49%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-         if(arr.size()==1){
+        int n=arr.size();
+        if(arr[0]>arr[1]){
             return 0;
         }
-        int n=arr.size();
-            if(arr[0]>arr[1]){
-                return 0;
-            }
-             if(arr[n-1]>arr[n-2]){
-                return n-1;
-            }
-        
-        int low=1,high=arr.size()-2;
-        int maxi=INT_MIN,mind=-1;
+        if(arr.back()>arr[n-2]){
+            return n-1;
+        }
+
+        int low=1,high=n-2;
+        int ans=0;
         while(low<=high){
             int mid=(low+high)/2;
 
-            if(arr[mid+1]<arr[mid] && arr[mid-1]<arr[mid]){
-                return mid;
-            }
-            else if(arr[mid-1]<arr[mid] && arr[mid]<arr[mid+1] ){
+            if(arr[mid]>arr[mid-1] || arr[mid]<arr[mid+1]){
+                ans=mid;
                 low=mid+1;
-            }else {
-                
+            }else if(arr[mid]>arr[mid+1] || arr[mid-1]>arr[mid]){
                 high=mid-1;
             }
+
         }
-        return 0;
+        return ans;
     }
 };
