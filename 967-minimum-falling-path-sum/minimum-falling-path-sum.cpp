90cbe0ab9044648c5fa6f5ee7082be2e3c08cb9c
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-22 23:18:28 +0530

    Time: 12 ms (59.81%) | Memory: 13.9 MB (17.21%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-    int helper(int row,int col,vector<vector<int>>&dp,vector<vector<int>>& matrix,int& n,int& m){
-        if(row<0 || row>=n || col>=m || col<0){
-            return 1e8;
+    vector<int>dc={-1,0,1};
+    int helper(int r,int c,int n,int m,vector<vector<int>>&dp,vector<vector<int>>&matrix){
+        if(r==n-1){
+            return matrix[r][c]; 
         }
-        if(row==n-1){
-            return matrix[row][col];
+        if(dp[r][c]!=-1){
+            return dp[r][c];
         }
-        
-       
-        if(dp[row][col]!=1e9 ){
-            return dp[row][col];
-        }
-
-        int drow[]={1,1,1};
-        int dcol[]={-1,0,1};
-        int mini=1e8;
+        int mini=INT_MAX;
         for(int i=0;i<3;i++){
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            mini=min(mini,helper(nrow,ncol,dp,matrix,n,m));
+            int nc=c+dc[i];
+            if(nc>=0 && nc<m){
+                mini=min(mini,helper(r+1,nc,n,m,dp,matrix));
+            }
         }
-        return dp[row][col]=matrix[row][col]+mini;
+        return dp[r][c]=mini+matrix[r][c];
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,1e9));
-        int mini=1e8;
+        int n=matrix.size(),m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
         for(int i=0;i<m;i++){
-             mini=min(mini,helper(0,i,dp,matrix,n,m));
+            dp[0][i]=matrix[0][i];
+        }
+        for(int r=1;r<n;r++){
+            for(int c=0;c<m;c++){
+                int mini=INT_MAX;
+                if(c>0){
+                    mini=min(mini,dp[r-1][c-1]);
+                }
+                mini=min(mini,dp[r-1][c]);
+                if(c<m-1){
+                    mini=min(mini,dp[r-1][c+1]);
+                }
+                dp[r][c]=mini+matrix[r][c];
+            }
+        }
+        int mini=INT_MAX;
+        for(int i=0;i<m;i++){
+            mini=min(mini,dp[n-1][i]);
         }
         return mini;
+        
     }
 };
