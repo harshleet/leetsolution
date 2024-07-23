c5ef31f7349932669310e49f86bccd6d71fbdd51
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-23 12:42:32 +0530

    Time: 40 ms (19.76%) | Memory: 30.1 MB (7.48%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    int helper(int ind1,int ind2,string &s1,string &s2,vector<vector<int>>&dp,int n){
-        if(ind1>=n ||ind2>=n){
-            return 0;
-        }
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-        int in1=helper(ind1+1,ind2,s1,s2,dp,n);
-        int in2=helper(ind1,ind2+1,s1,s2,dp,n);
-        int nin=0;
-        if(s1[ind1]==s2[ind2]){
-            nin=1+helper(ind1+1,ind2+1,s1,s2,dp,n);
-        }
-        return dp[ind1][ind2]=max({in1,in2,nin});
-    }
     int minInsertions(string s) {
-        int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        string s1=s;
+        string t=s;
         reverse(s.begin(),s.end());
-        return n-helper(0,0,s1,s,dp,n);
+        vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1));
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=t.size();j++){
+                int nt=max(dp[i][j-1],dp[i-1][j]);
+                int ta=0;
+                if(s[i-1]==t[j-1]){
+                    ta=1+dp[i-1][j-1];
+                }
+                dp[i][j]=max(ta,nt);
+            }
+        }
+        return s.size()-dp[s.size()][t.size()];
     }
 };
