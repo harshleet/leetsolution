7ec0cf3263d54043eb79728581fa734f87173199
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-23 12:23:56 +0530

    Time: 32 ms (35.83%) | Memory: 26.1 MB (7.72%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    int helper(int ind1,int ind2,string text1,string text2,int n,int m){
-        if(ind1>=n || ind2>=m){
-            return 0;
-        }
-        int nt1=helper(ind1+1,ind2,text1,text2,n,m);
-        int nt2=helper(ind1,ind2+1,text1,text2,n,m);
-        int t=0;
-        if(text1[ind1]==text2[ind2]){
-            t=1+helper(ind1+1,ind2+1,text1,text2,n,m);
-        }
-        return max({nt1,nt2,t});
-    }
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size(),m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<=m;i++){
-            dp[0][i]=0;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                int nt1=dp[i-1][j];
-                int nt2=dp[i][j-1];
+        vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size()+1,0));
+
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                int nt=max(dp[i-1][j],dp[i][j-1]);
                 int t=0;
                 if(text1[i-1]==text2[j-1]){
                     t=1+dp[i-1][j-1];
                 }
-                dp[i][j]= max({nt1,nt2,t});
+                dp[i][j]=max(nt,t);
             }
         }
-        return dp[n][m];
+        // for(int i=text;i<=text1.size();i++){
+        //     for(int j=0;j<=text2.size();j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        int i=text1.size(),j=text2.size();
+        string ans="";
+        while(i>=1 && j>=1){
+            if(text1[i-1]==text2[j-1]){
+                ans+=text1[i-1];
+                i--;
+                j--;
+            }else if(dp[i][j-1]>dp[i-1][j]){
+                j--;
+            }else{
+                i--;
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        cout<<ans<<" ";
+        return dp[text1.size()][text2.size()];
     }
 };
