ed806a3d0c1f3e01a963d6563fc33f6f086ec38b
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-26 09:34:38 +0530

    Time: 19 ms (67.35%) | Memory: 22.3 MB (47.45%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
-   void helper(int k,int j,vector<vector<int>>&dp,string& s){
-       if(j==k){
-           dp[k][j]=1;
-           return;
-       }
-      if(j-k==1){
-          if(s[k]==s[j]){
-              dp[k][j]=1;
-              return;
-          }else{
-              dp[k][j]=0;
-              return;
-          }
-      }
-      if(s[k]==s[j] && dp[k+1][j-1]==1){
-           dp[k][j]=1;
-           return;
-      }else{
-              dp[k][j]=0;
-              return;
-      }
-   }
     string longestPalindrome(string s) {
-        if(s.size()==1){
-            return s;
-        }
-        vector<vector<int>>dp(s.length(),vector<int>(s.length(),0));
         int maxi=0;
-        string maxst="";
-        string temp="";
-        for(int i=0;i<s.length();i++){
-             for(int j=i,k=0;j<s.length();j++,k++){
-                 helper(k,j,dp,s);
-                 if(dp[k][j]==1){
-                     if(maxi<j-k+1){
-                        temp=s.substr(k,j-k+1);
-                        maxi=max(maxi,j-k+1);
-                        maxst=temp;
-                     }
-                 }
-             }
-
+        string maxs="";
+        for(int i=0;i<s.size();i++){
+            int l=i,r=i;
+            while(l>=0 && r<s.size()){
+                if(s[l]==s[r]){
+                    if(maxi<r-l+1){
+                        maxi=r-l+1;
+                        maxs=s.substr(l,r-l+1);
+                    }
+                    l--;
+                    r++;
+                }else{
+                    break;
+                }
+                
+            }
+            l=i,r=i+1;
+            while(l>=0 && r<s.size()){
+                if(s[l]==s[r]){
+                    if(maxi<r-l+1){
+                        maxi=r-l+1;
+                        maxs=s.substr(l,r-l+1);
+                    }
+                    l--;
+                    r++;
+                }else{
+                    break;
+                }
+                
+            }
+            
         }
-        return maxst;
+        return maxs;
     }
 };
