d623f838b5c5743f5e6bc94275dec63bfd64e9de
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-26 09:49:10 +0530

    Time: 7 ms (72.52%) | Memory: 7.8 MB (70.45%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-int helper(int i,int j,vector<vector<int>>&dp,string &s){
-    if(i>j){
-        return 1;
-    }
-    if(s[i]!=s[j]){
-        return 0;
-    }
-    if(dp[i][j]!=-1){
-        return dp[i][j];
-    }
-    int c=0;
-    if(s[i]==s[j]){
-        c+=helper(i+1,j-1,dp,s);
-    }
-    return dp[i][j]=c;
-
-}
     int countSubstrings(string s) {
-        int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        int c=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
+         int cnt=0;
+        for(int i=0;i<s.size();i++){
+            int l=i,r=i;
+            while(l>=0 && r<s.size()){
+                if(s[l]==s[r]){
+                    cnt++;
+                    l--;
+                    r++;
+                }else{
+                    break;
+                }
                 
-                c+=helper(i,j,dp,s);
             }
+            l=i,r=i+1;
+            while(l>=0 && r<s.size()){
+                if(s[l]==s[r]){
+                   cnt++;
+                    l--;
+                    r++;
+                }else{
+                    break;
+                }
+                
+            }
+            
         }
-        return c;
+        return cnt;
     }
 };
