afa5ffabe16cea4edff47eecb45bf7c009252b6b
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-26 08:50:47 +0530

    Time: 23 ms (80.88%) | Memory: 16.1 MB (23.89%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>dis(n,vector<int>(n,1e9));
+        vector<vector<int>>dist(n,vector<int>(n,1e9));
         for(int i=0;i<n;i++){
-            dis[i][i]=0;
+            dist[i][i]=0;
         }
         for(auto it:edges){
-            dis[it[0]][it[1]]=it[2];
-            dis[it[1]][it[0]]=it[2];
+            dist[it[0]][it[1]]=it[2];
+            dist[it[1]][it[0]]=it[2];
         }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 for(int k=0;k<n;k++){
-                    if(dis[j][i]+dis[i][k]<dis[j][k]){
-                        dis[j][k]=dis[j][i]+dis[i][k];
+                    if( dist[j][k]>dist[j][i]+dist[i][k] ){
+                        dist[j][k]=dist[j][i]+dist[i][k];
                     }
                 }
             }
         }
-        int mini=INT_MAX,minii=-1;
-         for(int i=0;i<n;i++){
+        int mini=INT_MAX;
+        int minc=-1;
+        for(int i=0;i<n;i++){
             int c=0;
             for(int j=0;j<n;j++){
-                if(i!=j && dis[i][j]<=distanceThreshold){
+                if(dist[i][j]<=distanceThreshold){
                     c++;
                 }
             }
-            cout<<c<<" ";
             if(c<=mini){
                 mini=c;
-                minii=i;
+                minc=i;
             }
-         }
-         return minii;
+        }
+        return minc;
+
     }
 };
