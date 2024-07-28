8e7d91d7d210f978394f6d01c5c6104d3c79d0ab
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-28 15:19:58 +0530

    Time: 214 ms (7.32%) | Memory: 70.8 MB (5.08%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        queue<tuple<int,int,int>>q;
+
+        int n=maze.size();
+        int m=maze[0].size();
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+        queue<vector<int>>q;
         q.push({entrance[0],entrance[1],0});
-        int n=maze.size(),m=maze[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        vis[entrance[0]][entrance[1]]=1;
+        dist[entrance[0]][entrance[1]]=0;
 
         vector<int>dr={1,-1,0,0};
-        vector<int>dc={0,0,-1,1};
-
+        vector<int>dc={0,0,1,-1};
         while(!q.empty()){
-            int r=get<0>(q.front());
-            int c=get<1>(q.front());
-            int lvl=get<2>(q.front());
+            vector<int>v=q.front();
             q.pop();
-          
 
             for(int i=0;i<4;i++){
-                int nr=r+dr[i];
-                int nc=c+dc[i];
-                if(nr>=0 && nc>=0 && nr<n && nc<m && !vis[nr][nc] && maze[nr][nc]=='.'){
-                    cout<<nr<<" "<<nc<<endl;
-                    vis[nr][nc]=1;
-                    q.push({nr,nc,lvl+1});
-                }else if((nr<0 || nc<0 || nr>=n || nc>=m) && (r!=entrance[0] || c!=entrance[1]) ){
-                    return lvl;
-                }               
+                int nr=v[0]+dr[i];
+                int nc=v[1]+dc[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m && maze[nr][nc]=='.' && v[2]+1<dist[nr][nc]){
+                    dist[nr][nc]=1+v[2];
+                    q.push({nr,nc,dist[nr][nc]});
+                }
             }
-
         }
-        return -1;
-
+        int mini=1e9;
+        for(int i=0;i<n;i++){
+            if(i!=entrance[0] || entrance[1]!=0)
+                 mini=min(mini,dist[i][0]);
+        }
+        for(int i=0;i<m;i++){
+            if(entrance[0]!=0 || entrance[1]!=i)
+                  mini=min(mini,dist[0][i]);
+        }
+        for(int i=0;i<n;i++){
+            if(i!=entrance[0] || entrance[1]!=m-1)
+                  mini=min(mini,dist[i][m-1]);
+        }
+        for(int i=0;i<m;i++){
+            if(entrance[0]!=n-1 || entrance[1]!=i)
+                mini=min(mini,dist[n-1][i]);
+        }
+        if(mini==1e9){
+            return -1;
+        }
+        return mini;
     }
 };
