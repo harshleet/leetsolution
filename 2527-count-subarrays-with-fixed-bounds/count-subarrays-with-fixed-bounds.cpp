9d9683a9cfc8eb0a34ba4ed3afdc6ca4f9521c08
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-29 19:13:21 +0530

    Time: 93 ms (37.34%) | Memory: 82.9 MB (20.55%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long long st=0,en=0,ans=0,n=nums.size(),i=0;
-        long long mini=INT_MAX,maxi=0,minii=-1,maxii=-1;
-        while(i<n){
-            if(minK>nums[i] || maxK<nums[i]){
+        int mini=INT_MAX,maxi=0,maxii=-1,minii=-1,st=0,i=0;
+        long long ans=0;
+        while(i<nums.size()){
+            if(nums[i]>maxK || nums[i]<minK){
                 st=i+1;
-                en=i+1;
-                mini=INT_MAX;
                 maxi=0;
+                mini=INT_MAX;
                 i++;
                 continue;
-            }
-            if(nums[i]<=mini){
-                mini=nums[i];
-                minii=i;
             }
             if(nums[i]>=maxi){
                 maxi=nums[i];
                 maxii=i;
             }
-            if(mini==minK && maxi==maxK){
-                ans+=min(minii,maxii)-st+1;
-            }
-            
 
+
+            if(nums[i]<=mini){
+                mini=nums[i];
+                minii=i;
+            }
+
+            if(mini==minK && maxi==maxK){
+                ans=(ans+(min(minii,maxii)-st+1));
+                // ans=ans+(i-max(minii,maxii)+1);
+            }
             i++;
-            en=i;
         }
         return ans;
     }
