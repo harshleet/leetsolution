41ca31806816f299a1cbd8d870817992d4ee057e
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-29 11:34:22 +0530

    Time: 72 ms (43.20%) | Memory: 16 MB (18.44%) - LeetSync

---
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+  
+    int numTeams(vector<int>& rating) {
+        int n=rating.size();
+
+        vector<vector<int>>t(n,vector<int>(4));
+        set<int>temp;
+        for(int i=0;i<n;i++){
+           int leg = distance(temp.begin(), temp.lower_bound(rating[i]));
+            int les = distance(temp.upper_bound(rating[i]), temp.end());
+            temp.insert(rating[i]);
+            t[i][0] = leg;
+            t[i][1] = les;
+        }
+
+        set<int>temp2;
+        for(int i=n-1;i>=0;i--){
+           int rig = distance(temp2.begin(), temp2.lower_bound(rating[i]));
+            int ris = distance(temp2.upper_bound(rating[i]), temp2.end());
+            temp2.insert(rating[i]);
+            t[i][2] = rig;
+            t[i][3] = ris;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            // cout<<t[i][0]<<" "<<t[i][1]<<" "<<t[i][2]<<" "<<t[i][3]<<endl;
+           ans+=t[i][0]*t[i][3];
+           ans+=t[i][1]*t[i][2];
+        }
+        return ans;
+
+    }
+};
