5af82a8ed6fbba4d78a442738ba2a0a20ae34b87
harshleet <122361301+harshleet@users.noreply.github.com>
2024-07-30 18:21:15 +0530

    Time: 71 ms (10.20%) | Memory: 37.3 MB (31.88%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-int mod=1e9+7;
+    int mod=1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        
-       vector<pair<long long,long long>>adj[n];
-       for(int i=0;i<roads.size();i++){
-           adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
-           adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
-       }
-        vector<long long>dist(n,LONG_MAX);
-        vector<long long>ways(n,0);
-        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>pq;
-        dist[0]=0;
-        ways[0]=1;
-        pq.push({0,0});
-        while(!pq.empty()){
-            long long dis=pq.top().first;
-            long long node=pq.top().second;
-            pq.pop();
-          
-            for(auto it:adj[node]){
-               if(dis+it.second<dist[it.first]){
-                   dist[it.first]=dis+it.second;
-                   ways[it.first]=(ways[node]);
-                   pq.push({dist[it.first],it.first});
-               }else if(dis+it.second==dist[it.first]){
-                   ways[it.first]=(ways[it.first]+ways[node])%mod;
-                   
-               }
-            }
-           
-        }
-        return ways[n-1]%mod;;
-    }
 
+        vector<vector<pair<int,long long>>>adj(n);
+        for(int i=0;i<roads.size();i++){
+            adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
+            adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
+        }
+        vector<long long int>dist(n,LLONG_MAX);
+        vector<int>way(n,0);
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
+        pq.push({0,0});
+        dist[0]=0;
+        way[0]=1;
+
+        while(!pq.empty()){
+            long long di=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+
+            for(auto it:adj[node]){
+                if(dist[it.first]>it.second+di){
+                    
+                   dist[it.first]=it.second+di;
+                    way[it.first]=way[node];
+                    pq.push({dist[it.first],it.first});
+                }else if(dist[it.first]==it.second+di){
+                    cout<<it.first<<endl;
+                    way[it.first]=(way[it.first]+way[node])%mod;
+                }
+            }
+
+        }
+         
+        return way[n-1];
+    }
 };
