7384c158d1d810833892af59cd8c137262e977f1
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-01 17:09:18 +0530

    Time: 187 ms (31.64%) | Memory: 111.8 MB (27.60%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,53 +1,51 @@
 class Solution {
 public:
-bool static cmp(string& s1,string &s2){
-    return s1.size()<s2.size();
-}
-bool check(string& s1, string& s2){
-    if(s1.size() != s2.size() + 1) return false;
+    bool possible(string &a,string &b){
     
-    int first = 0;
-    int second = 0;
-    
-    while(first < s1.size()){
-        if(second < s2.size() && s1[first] == s2[second]){
-            first ++;
-            second ++;
+        if(a.size()-b.size()!=1){
+            return false;
         }
-        else first ++;
+        int i=0,j=0;
+        int diff=0;
+        while(i<a.size() && j<b.size()){
+            if(a[i]!=b[j]){
+                i++;
+                diff++;
+            }else{
+                i++;
+                j++;
+            }
+            
+        }
+        
+        return diff<=1;
     }
-    if(first == s1.size() && second == s2.size()) return true;
-    else return false; 
-}
-  int f(vector<string>&nums,vector<vector<int>>&dp,int i,int pre){
-       if(i==nums.size()){
-           return 0;
-       }
-       if(dp[i][pre+1]!=-1){
-           return dp[i][pre+1];
-       }
-         int ntake=f(nums,dp,i+1,pre);
-       int take=0;
-       if(pre==  -1 ||  check(nums[i],nums[pre])){
-          take=1+f(nums,dp,i+1,i);
-       }
-     
-       return dp[i][pre+1]=max(take,ntake);
+    int helper(int ind,int pre,vector<string>&words,vector<vector<int>>&dp){
+        if(ind>=words.size()){
+            return 0;
+        }
+        if(dp[ind][pre+1]!=-1){
+            return dp[ind][pre+1];
+        }
+        int nt=helper(ind+1,pre,words,dp);
+        int t=0;
+        if(pre==-1 || possible(words[ind],words[pre])){
+            t=1+helper(ind+1,ind,words,dp);
+        }
+        return dp[ind][pre+1]=max(nt,t);
     }
-    int longestStrChain(vector<string>& nums) {
-        sort(nums.begin(),nums.end(),cmp);
-         vector<int>dp(nums.size(),1);
-         int gmax=1;
-          for(int ind=0;ind<nums.size();ind++){
-            int maxi=1;
-              for(int pre=0;pre<ind;pre++){
-                if(check(nums[ind],nums[pre])){
-                    maxi=max(maxi,1+dp[pre]);
-                }
-              }
-              dp[ind]=maxi;
-              gmax=max(gmax,maxi);
-          }
-          return gmax;
+    int longestStrChain(vector<string>& words) {
+        priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>>pq;
+        for(int i=0;i<words.size();i++){
+            pq.push({words[i].size(),words[i]});
+        }
+        vector<string>temp;
+        while(!pq.empty()){
+           
+            temp.push_back(pq.top().second);
+            pq.pop();
+        }
+        vector<vector<int>>dp(words.size(),vector<int>(words.size(),-1));
+        return helper(0,-1,temp,dp);
     }
 };
