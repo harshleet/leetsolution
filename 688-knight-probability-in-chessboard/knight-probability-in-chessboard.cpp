d08de5ce10a2a3daa9ec6ec3f0f525d2afb191bd
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-09 10:50:26 +0530

    Time: 7 ms (69.44%) | Memory: 10.6 MB (45.06%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-int drow[8]={1,-1,2,-2,1,-1,2,-2};
-int dcol[8]={2,2,1,-1,-2,-2,-1,1};
-   double dfs(int row,int col,int n,int k,vector<vector<vector<double>>>&dp){
-        if(row<0 || row>=n||col<0 || col>=n){
-             return 0.0;
+vector<int>dr={-2,-2,2,2,-1,-1,1,1};
+vector<int>dc={-1,1,-1,1,-2,2,-2,2};
+    double helper(int r,int c,int n,int k,vector<vector<vector<double>>>&dp){
+        if(r<0 || c<0 || r>=n || c>=n){
+            return 0.0;
         }
         if(k==0){
             return 1.0;
         }
-        if(dp[row][col][k]!=-1){
-            return dp[row][col][k];
+        if(dp[r][c][k]!=-1){
+            return dp[r][c][k];
         }
         double ans=0;
         for(int i=0;i<8;i++){
-            int nrow=row+ drow[i];
-            int ncol=col+ dcol[i];
-            ans+= dfs(nrow,ncol,n,k-1,dp);
-            
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            ans+=helper(nr,nc,n,k-1,dp);
         }
-        return dp[row][col][k]=ans;
-    }
+        return dp[r][c][k]=ans;
 
+    }
     double knightProbability(int n, int k, int row, int column) {
-        vector<vector<vector<double>>>dp(n,vector<vector<double>>(n,vector<double>(k+1,-1)));
-        double get=dfs(row,column,n,k,dp); 
-        double tot=pow(8,k);
-        return get/tot;
+         vector<vector<vector<double>>>dp(n,vector<vector<double>>(n,vector<double>(k+1,-1)));
+        double in=helper(row,column,n,k,dp);
+        // cout<<in<<" ";
+       
+        return in/(double)(pow(8,k));
     }
 };
