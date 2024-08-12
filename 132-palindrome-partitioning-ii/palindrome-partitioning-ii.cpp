a8e7cab399fe6152df83b4e1a643f9c8127bb368
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-12 14:55:35 +0530

    Time: 1780 ms (8.67%) | Memory: 8.2 MB (56.39%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
-    bool isPalindrome(int st, int end, string& s, vector<vector<int>>& pal) {
-        while (st < end) {
-            if (s[st] != s[end]) {
-                return false;
-            }
-            st++;
-            end--;
+    bool isPal(string &s,int st,int en){
+       while(st<=en){
+        if(s[st]!=s[en]){
+            return false;
         }
+        st++;
+        en--;
+       }
         return true;
     }
-    int helper(int ind, string& s, int n, vector<int>& dp,
-               vector<vector<int>>& pal) {
-        if (ind >= n) {
-            return -1;
+    int helper(int i,string &s,vector<int>&dp){
+        if(i>=s.size()){
+            return 0;
         }
-        if (dp[ind] != -1) {
-            return dp[ind];
-        }
-        int mini = INT_MAX;
-        for (int i = 1; i <= n - ind; i++) {
-
-            if (isPalindrome(ind, ind + i - 1, s, pal)) {
-                mini = min(mini, 1 + helper(ind + i, s, n, dp, pal));
+        if(dp[i]!=-1){
+            return dp[i];
+        }  
+        int mini=INT_MAX;
+        for(int k=1;k<=s.size()-i;k++){
+           
+            if( isPal(s,i,i+k-1)){
+                mini=min(mini,1+helper(i+k,s,dp));
             }
+
         }
-        return dp[ind] = mini;
+        return dp[i]=mini;
     }
     int minCut(string s) {
-        vector<int> dp(s.size(), -1);
-        vector<vector<int>> pal(s.size(), vector<int>(s.size(), -1));
-        // for (int i = 0; i < s.size(); i++) {
-        //     pal[i][i] = 1;
-        // }
-        return helper(0, s, s.size(), dp, pal);
+        //s.size()^3
+        vector<int>dp(s.size(),-1);
+        return helper(0,s,dp)-1;
     }
 };
