24f6fe7cc19d0f66fee1b55f2c69fa794343b5fc
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-12 19:37:21 +0530

    Time: 71 ms (22.78%) | Memory: 46.3 MB (17.99%) - LeetSync

--- 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.cpp
+++ 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-int mod=1e9+7;
+int mod= 1e9+7;
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-         priority_queue<pair<long long,long long>>pq;
-         for(int i=0;i<n;i++){
-             pq.push({efficiency[i],speed[i]});
-         }
-         vector<pair<long long,long long>>v;
-         while(!pq.empty()){
-             int eff=pq.top().first;
-             int see=pq.top().second;
-             pq.pop();
-             v.push_back({eff,see});
-         }
-        
-         long long sum=0,eff=INT_MAX;
-         long long maxi=0;
-         priority_queue<long long, vector<long long>, greater<long long>> pq1;
-         for(int i=0;i<k;i++){
-               eff=min(v[i].first,eff);
-               pq1.push(v[i].second);
-               sum+=v[i].second;
-              maxi=max(maxi,(sum*eff));
-         }
-         for(int i=k;i<n;i++){
-             maxi=max(maxi,(sum*eff));
-             eff=min(v[i].first,eff);
-             if(!pq1.empty()){
-                 sum-=pq1.top();
-                 pq1.pop();
-             }
-             sum+=v[i].second;
-             pq1.push(v[i].second);
-         }
-         maxi=max(maxi,(sum*eff));
-    return maxi%mod;
+        priority_queue<pair<long long,long long>>pq;
+        for(int i=0;i<n;i++){
+            pq.push({efficiency[i],speed[i]});
+        }
+
+        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>temp;
+        long long  mini=LLONG_MAX,sum=0;
+        long long maxi=0;
+        while(!pq.empty()){
+            pair<long long,long long>p=pq.top();
+            pq.pop();
+            if(temp.size()<k){
+                temp.push({p.second,p.first});
+                mini=min(mini,p.first);
+                sum=(sum+p.second);
+                maxi=max(maxi,sum*mini);
+            }else{
+                 temp.push({p.second,p.first});
+                 long long tval=temp.top().first;
+                 temp.pop();
+                 sum=(sum+-tval+p.second);
+                 mini=min(mini,p.first);
+                maxi=max(maxi,sum*mini);
+            }
+        }
+        return maxi%mod;
     }
 };
