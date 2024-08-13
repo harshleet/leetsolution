89f6bb66b8724fc904ec36e1464cb63bfa16ddcd
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-13 19:02:55 +0530

    Time: 376 ms (72.27%) | Memory: 174 MB (70.51%) - LeetSync

---
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        int n=colors.size();
+        vector<vector<int>>adj(n);
+        vector<int>ind(n);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            ind[edges[i][1]]++;
+        }
+        queue<int>q;
+        vector<vector<int>>mp(n,vector<int>(26));
+        
+        for(int i=0;i<n;i++){
+            if(ind[i]==0){
+                mp[i][colors[i]-'a']++;
+                q.push(i);
+            }
+        }
+        int maxi=0;
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+
+            maxi=max(maxi,*max_element(mp[node].begin(),mp[node].end()));
+            for(auto it:adj[node]){
+                for(int i=0;i<26;i++){
+                    if(i==colors[it]-'a'){
+                        mp[it][i]=max(mp[node][i]+1,mp[it][i]);
+                    }else{
+                        mp[it][i]=max(mp[node][i],mp[it][i]);
+                    }
+                    //  mp[it][i]=max(mp[node][i],mp[it][i]);
+                }
+                // mp[it][colors[it]-'a']++;
+                // for(int i=0;i<26;i++){
+                //      maxi=max(maxi,mp[it][i]);
+                // }
+                 ind[it]--;
+                if(ind[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(ind[i]!=0){
+                return -1;
+            }
+        }
+        return maxi;
+    }
+};
