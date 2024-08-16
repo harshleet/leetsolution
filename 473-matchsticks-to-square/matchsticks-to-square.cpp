eea46205aa1db1fe50a218c4de8fb9135c0c4ed2
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-16 11:07:38 +0530

    Time: 110 ms (69.19%) | Memory: 21.8 MB (15.09%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/matchsticks-to-square.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    bool helper(int sum, int bitmask, int tar, int cnt, vector<int>& matchsticks, vector<int>& dp) {
-        if (cnt == 4) {
-            return true;
-        }
-        if (dp[bitmask] != -1) {
-            return dp[bitmask];
-        }
-        if (sum == tar) {
-            return dp[bitmask] = helper(0, bitmask, tar, cnt + 1, matchsticks, dp);
-        }
-        
-        bool ans = false;
-        for (int i = 0; i < matchsticks.size(); i++) {
-            if ((bitmask & (1 << i)) == 0 && sum + matchsticks[i] <= tar) {
-                int temp = bitmask;
-                temp |= (1 << i);
-                ans = ans| helper(sum + matchsticks[i], temp, tar, cnt, matchsticks, dp);
-             
+    bool helper(int sum,int bitmask,int tar,int cnt,vector<int>&matchsticks,vector<int>&dp){
+        if(sum==tar){
+            if(cnt<3){
+                return helper(0,bitmask,tar,cnt+1,matchsticks,dp);
+            }else if(cnt==3){
+                if(bitmask==(1<<matchsticks.size())-1){
+                    return true;
+                }
+                return false;
             }
         }
-        
-        return dp[bitmask] = ans;
+        if(dp[bitmask]!=-1){
+            return dp[bitmask];
+        }
+        bool ans= false;
+        for(int i=0;i<matchsticks.size();i++){
+            if((bitmask&(1<<i))==0 && sum+matchsticks[i]<=tar){
+                int temp=bitmask;
+                temp|=(1<<i);
+                ans|=helper(sum+matchsticks[i],temp,tar,cnt,matchsticks,dp);
+            }
+        }
+        return dp[bitmask]=ans;
     }
-
     bool makesquare(vector<int>& matchsticks) {
-        int sum = accumulate(matchsticks.begin(), matchsticks.end(), 0);
-        int maxi = *max_element(matchsticks.begin(), matchsticks.end());
-        if (sum % 4 != 0) {
+        int sum=accumulate(matchsticks.begin(),matchsticks.end(),0);
+        int maxi=*max_element(matchsticks.begin(),matchsticks.end());
+        if(sum%4!=0){
             return false;
         }
-        if (sum / 4 < maxi) {
+        if(sum/4<maxi){
             return false;
         }
-
-        int n = matchsticks.size();
-        vector<int> dp(1 << n, -1);
-        return helper(0, 0, sum / 4, 0, matchsticks, dp);
+        cout<<sum/4<<endl;
+        int n=matchsticks.size();
+        vector<int>dp(1<<n,-1);
+        return helper(0,0,sum/4,0,matchsticks,dp);
     }
 };
