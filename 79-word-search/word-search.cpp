b025250bc51b48e383a233a0f3cfd4e86101df93
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-17 00:15:27 +0530

    Time: 273 ms (73.60%) | Memory: 10 MB (72.52%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    vector<int> dr = {1, -1, 0, 0};
-    vector<int> dc = {0, 0, 1, -1};
-    bool helper(int r, int c, vector<vector<char>>& board, string word,
-                int ind,vector<vector<int>>&vis) {
-        if (ind >= word.size()) {
+    vector<int>dr={1,-1,0,0};
+    vector<int>dc={0,0,-1,1};
+    bool helper(int r,int c,int ind,string& word,vector<vector<char>>&board,int n,int m,vector<vector<int>>&vis){
+        if(ind==word.size()){
             return true;
         }
         vis[r][c]=1;
-        bool fl = false;
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-            if (nr < board.size() && nc < board[0].size() && nr >= 0 && nc >= 0 && board[nr][nc] == word[ind] && !vis[nr][nc]) {
-                // cout<<nr<<" "<<nc<<" ";
-                fl = fl || helper(nr, nc, board, word, ind + 1,vis);
+        bool ans=false;
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(nr>=0 && nc>=0 && nr<n && nc<m && board[nr][nc]==word[ind] && !vis[nr][nc]){
+                if(ind==word.size()-1){
+                    return true;
+                }
+                ans=ans|helper(nr,nc,ind+1,word,board,n,m,vis);
             }
         }
         vis[r][c]=0;
-        return fl;
+        return ans;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        
-        bool f = false;
-        vector<vector<int>>vis(board.size(),vector<int>(board[0].size(),0));
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                if (board[i][j] == word[0]) {
-                    f = f || helper(i, j, board, word, 1,vis);
+        int n=board.size(),m=board[0].size();
+        bool ans=false;
+         vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0]){
+                   
+                    ans=ans|helper(i,j,1,word,board,n,m,vis);
                 }
             }
         }
-        return f;
+        return ans;
     }
 };
