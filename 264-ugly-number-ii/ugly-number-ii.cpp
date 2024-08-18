8bf58c002cd3de1e119eed8c8a0e424f417ed512
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-18 10:18:30 +0530

    Time: 65 ms (34.26%) | Memory: 31.7 MB (26.05%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-   
     int nthUglyNumber(int n) {
-       set<long long int>s;
-       long long int cnt=0;
-       s.insert(1);
-       long long int val=1;
-       while(!s.empty() && n > 0){
-            val=*(s.begin());
-            s.erase(val);
-           n--;
-           if (n == 0) {
-                break;
-            }
-            s.insert(val*2);
-            
-            s.insert(val*3);
-            
-            s.insert(val*5);
-            
+       set<long long>st;
+
+       st.insert(1);
+       int cnt=1;
+       long long val=1;
+       while(!st.empty() && cnt<=n){
+           val=*st.begin();
+           st.erase(val);
+
+           cnt++;
+           if(cnt==n+1){
+             break;
+           }
+           st.insert(val*2);
+           st.insert(val*3);
+           st.insert(val*5);
        }
-       return val;
+        return val;
     }
 };
