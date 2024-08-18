a6ada763362380fda4b6820ff5ba9bf0adde53bd
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-18 10:21:47 +0530

    Time: 101 ms (9.82%) | Memory: 35.7 MB (7.76%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-       set<long long>st;
+        set<long long>s;
+        priority_queue<long long,vector<long long>,greater<long long>>q;
+        int cnt=0;
+        q.push(1);
 
-       st.insert(1);
-       int cnt=1;
-       long long val=1;
-       while(!st.empty() && cnt<=n){
-           val=*st.begin();
-           st.erase(val);
-
-           cnt++;
-           if(cnt==n+1){
-             break;
-           }
-           st.insert(val*2);
-           st.insert(val*3);
-           st.insert(val*5);
-       }
-        return val;
+        while(!q.empty()){
+                long long node=q.top();
+                q.pop();
+                cnt++;
+                // cout<<node<<" "<<cnt<<endl;
+                if(cnt==n){
+                    return node;
+                }
+                if(s.find(node*2)==s.end()){
+                    s.insert(node*2);
+                    q.push(node*2);
+                }
+                if(s.find(node*3)==s.end()){
+                     s.insert(node*3);
+                    q.push(node*3);
+                }
+                if(s.find(node*5)==s.end()){
+                     s.insert(node*5);
+                    q.push(node*5);
+                }
+        }
+        return -1;
     }
 };
