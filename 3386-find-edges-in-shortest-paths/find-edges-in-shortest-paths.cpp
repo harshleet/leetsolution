e845bb90d02a45e9cc24564606c63771c3627290
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-19 21:36:10 +0530

    Time: 2160 ms (34.61%) | Memory: 201.4 MB (70.36%) - LeetSync

--- 3386-find-edges-in-shortest-paths/find-edges-in-shortest-paths.cpp
+++ 3386-find-edges-in-shortest-paths/find-edges-in-shortest-paths.cpp
@@ -1,62 +1,67 @@
 class Solution {
 public:
+   
     vector<bool> findAnswer(int n, vector<vector<int>>& edges) {
-        vector<long long int> dist(n, INT_MAX);
-        vector<bool> ans(edges.size(), false);
-        vector<vector<pair<int, int>>> adj(n);
-        for (int i = 0; i < edges.size(); i++) {
-            adj[edges[i][0]].push_back({edges[i][1], edges[i][2]});
-            adj[edges[i][1]].push_back({edges[i][0], edges[i][2]});
+        int m=edges.size();
+        vector<vector<pair<int,int>>>adj(n);
+      
+        for(auto it:edges){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
+           
         }
-        priority_queue<pair<long long int, long long int>,
-                       vector<pair<long long int, long long int>>,
-                       greater<pair<long long int, long long int>>>
-            pq;
-        pq.push({0, 0});
-        dist[0] = 0;
-        while (!pq.empty()) {
-            int dis = pq.top().first;
-            int node = pq.top().second;
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,0});
+
+        vector<int>dis(n,1e9);
+        dis[0]=0;
+
+        while(!pq.empty()){
+            int dist=pq.top().first;
+            int node=pq.top().second;
             pq.pop();
-            for (auto it : adj[node]) {
-                if (dist[it.first] > dis + it.second) {
-                    dist[it.first] = dis + it.second;
-                    pq.push({dist[it.first], it.first});
+
+            for(auto it:adj[node]){
+                if(dist+it.second<dis[it.first]){
+                    dis[it.first]=dist+it.second;
+                    pq.push({dis[it.first],it.first});
                 }
             }
         }
-        int mini=dist[n-1];
-        if (dist[n - 1] == INT_MAX) {
+
+        vector<bool>ans(m,false);
+        if(dis[n-1]==1e9){
             return ans;
         }
-        vector<long long int> dist2(n, INT_MAX);
-        priority_queue<pair<long long int, long long int>,
-                       vector<pair<long long int, long long int>>,
-                       greater<pair<long long int, long long int>>>
-            pq2;
-        pq2.push({0, n-1});
-        dist2[n-1] = 0;
-        while (!pq2.empty()) {
-            int dis = pq2.top().first;
-            int node = pq2.top().second;
+        int val=dis[n-1];
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq2;
+        pq2.push({0,n-1});
+
+        vector<int>dis2(n,1e9);
+        dis2[n-1]=0;
+
+        while(!pq2.empty()){
+            int dist=pq2.top().first;
+            int node=pq2.top().second;
             pq2.pop();
-            for (auto it : adj[node]) {
-                if (dist2[it.first] > dis + it.second) {
-                    dist2[it.first] = dis + it.second;
-                    pq2.push({dist2[it.first], it.first});
+
+            for(auto it:adj[node]){
+                if(dist+it.second<dis2[it.first]){
+                    dis2[it.first]=dist+it.second;
+                    pq2.push({dis2[it.first],it.first});
                 }
             }
         }
-        for(int i=0;i<dist2.size();i++){
-            cout<<dist[i]<<" ";
-            cout<<dist2[i]<<" ";
-            cout<<endl;
-        }
-        for(int i=0;i<edges.size();i++){
-            if((dist[edges[i][0]]+dist2[edges[i][1]]+edges[i][2]==mini)||(dist[edges[i][1]]+dist2[edges[i][0]]+edges[i][2]==mini)){
+
+        for(int i=0;i<m;i++){
+            int node1=edges[i][0],node2=edges[i][1];
+            if(dis[node1]+dis2[node2]+edges[i][2]==val  ||dis[node2]+dis2[node1]+edges[i][2]==val){
                 ans[i]=true;
             }
         }
+     
         return ans;
     }
 };
