b11fd718d6dd98980fb3f0f8fc67efc14fa14f7e
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-19 09:10:50 +0530

    Time: 31 ms (13.21%) | Memory: 157.3 MB (8.56%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int helper(int n,int len,int copylen,int op){
-        if(len>n){
-            return 1e8;
-        }
-        if(n==len){
+    int helper(int len,int op,int n,int clen,vector<vector<int>>&dp){
+        if(len==n){
             return 0;
         }
-        int copy=1e9;
-        if(op!=1){
-           copy=1+helper(n,len,len,1);
+        if(len>n){
+            return 1e9;
         }
-        int paste=1e9;
-        if(copylen!=0){
-            paste=1+helper(n,copylen+len,copylen,2);
+        if(dp[len][clen]!=-1){
+            return dp[len][clen];
         }
-        return min(copy,paste);
+        int ans=1e9;
+        if(clen==0){
+            ans=1+helper(len,0,n,len,dp);
+        }else{
+            ans=min(ans,1+helper(clen+len,-1,n,0,dp));
+            ans=min(ans,1+helper(clen+len,0,n,clen,dp));
+        }
+        return dp[len][clen]=ans;
     }
     int minSteps(int n) {
-       
-        return helper(n,1,0,0);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return helper(1,-1,n,0,dp);
     }
 };
