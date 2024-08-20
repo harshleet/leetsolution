273c62c35bdd0ff75e9e64c9f59ea072b3e357bb
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-20 17:08:35 +0530

    Time: 33 ms (47.58%) | Memory: 19.6 MB (42.44%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>&temp){
+
+    int helper(vector<int>&temp){
+        int m=temp.size();
+        vector<int>ps(m,-1);
+        vector<int>ns(m,m);
+
         stack<int>st;
-           int n=temp.size();
-        vector<int>ps(temp.size(),-1);
-        vector<int>ns(temp.size(),n);
-     
-        for(int i=0;i<n;i++){
+
+        for(int i=0;i<m;i++){
             if(st.empty() || temp[st.top()]<temp[i]){
+                
                 if(!st.empty()){
                     ps[i]=st.top();
                 }
                 st.push(i);
             }else{
+
                 while(!st.empty() && temp[st.top()]>=temp[i]){
                     st.pop();
                 }
@@ -22,46 +26,52 @@ public:
                 st.push(i);
             }
         }
-        stack<int>st2;
-        for(int i=n-1;i>=0;i--){
-            if(st2.empty() || temp[st2.top()]<temp[i]){
-                if(!st2.empty()){
-                    ns[i]=st2.top();
-                }
-                st2.push(i);
+
+        while(!st.empty()){
+            st.pop();
+        }
+
+        for(int i=m-1;i>=0;i--){
+            if(st.empty() || temp[st.top()]<temp[i]){
+                
+                if(!st.empty()){
+                    ns[i]=st.top();
+                }st.push(i);
             }else{
-                while(!st2.empty() && temp[st2.top()]>=temp[i]){
-                    st2.pop();
+
+                while(!st.empty() && temp[st.top()]>=temp[i]){
+                    st.pop();
                 }
-                if(!st2.empty()){
-                    ns[i]=st2.top();
+                if(!st.empty()){
+                    ns[i]=st.top();
                 }
-                st2.push(i);
+                st.push(i);
             }
         }
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            // cout<<temp[i]<<" "<<ps[i]<<" "<<ns[i]<<" ";
-           maxi=max(maxi,(ns[i]-ps[i]-1)*temp[i]);
+
+        int ans=0;
+        for(int i=0;i<m;i++){
+            if(temp[i]!=0){
+                // cout<<ns[i]<<" "<<ps[i]<<" "<<i<<endl;
+                ans=max(ans,(ns[i]-ps[i]-1)*temp[i]);
+            }
         }
-        cout<<endl;
-        
-        return maxi;
+        return ans;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>>temp(n,vector<int>(m,0));
+        vector<int>temp(m,0);
+
         int maxi=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<matrix.size();i++){
             for(int j=0;j<m;j++){
-                if(i==0 && matrix[i][j]=='1'){
-                    temp[i][j]=matrix[i][j]-'0';
-                }else if(matrix[i][j]=='1'){
-                    temp[i][j]=temp[i-1][j]+(matrix[i][j]-'0');
+                if(matrix[i][j]=='1'){
+                    temp[j]++;
+                }else{
+                    temp[j]=0;
                 }
             }
-            maxi=max(maxi,solve(temp[i]));
+            maxi=max(maxi,helper(temp));
         }
         return maxi;
     }
