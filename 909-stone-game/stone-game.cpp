f449d6c962c94642b72a9b30341dc723c28d42e3
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-20 15:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (49.06%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -2,6 +2,34 @@ class Solution {
 public:
     
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int maxi=0;
+        int st=0,en=piles.size()-1,col=0,col2=0,t=0;
+        while(st<=en){
+            if(t==0){
+                if(piles[st]>=piles[en]){
+           
+                    col+=piles[st];
+                    st++;
+                }else{
+           
+                    col+=piles[en];
+                    en--;
+                }
+                t=1;
+            }else{
+                if(piles[st]<=piles[en]){
+     
+                    col2+=piles[st];
+                    st++;
+                }else{
+                    col2+=piles[en];
+                    en--;
+                }
+                t=0;
+            }
+            
+        }
+        
+        return col-col2>=0;
     }
 };
