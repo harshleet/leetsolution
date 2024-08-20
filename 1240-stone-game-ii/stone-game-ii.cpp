570549a7259dbb7a7fd0c655c05e259883bf3988
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-20 14:36:29 +0530

    Time: 189 ms (5.11%) | Memory: 47.9 MB (9.17%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,36 +1,32 @@
 class Solution {
-    public:
-    vector<vector<vector<int>>> dp;
-    int helper(int m, int ind2, vector<int>& piles, int st1, int st2, int t) {
-        int n=piles.size();
-        if (ind2>=n) {
+public:
+    int helper(int ind, int m, int t, vector<int>& piles, int n, vector<int>& pre,vector<vector<vector<int>>>&dp) {
+        if (ind >= n) {
             return 0;
         }
-       if(dp[m][ind2][t]!=-1){
-           return dp[m][ind2][t];
-       }
-        
-        if (t == 1) {
-            int result=0;
-            int stone=0;
-            for(int i=1;i<=2*m && ind2+i-1<n ;i++){
-                stone+=piles[ind2+i-1];
-               result=max(result,stone+helper(max(i,m), ind2+i, piles, st1 + stone, st2, 0));
+
+        if(dp[ind][m][t]!=-1){
+            return dp[ind][m][t];
+        }
+        if (t == 0) {
+            int val=0;
+            for (int i = ind; i < min(2 * m + ind, n); i++) {
+                val = max(val,pre[i+1] - pre[ind] + helper(i+1, max(m,i-ind+1), 1, piles, n, pre,dp));
             }
-            return dp[m][ind2][t]= result;
-            
-        } int result=INT_MAX;
-         int stone=0;
-            for(int i=1;i<=2*m && ind2+i-1<n;i++){
-                   stone+=piles[ind2+i-1];
-               result=min(result,helper(max(i,m), ind2+i, piles, st1 , st2+stone, 1));
+            return val;
+        }
+            int val=INT_MAX;
+            for (int i = ind; i < min(2 * m + ind, n); i++) {
+                val =min(val, helper(i+1, max(m,i-ind+1), 0, piles, n, pre,dp));
             }
-            return dp[m][ind2][t]= result;
-      
+            return dp[ind][m][t]=val;
     }
     int stoneGameII(vector<int>& piles) {
-        int n=piles.size();
-       dp.resize(n + 1, vector<vector<int>>(n, vector<int>(2, -1)));
-        return helper(1, 0, piles, 0, 0, 1);
+        vector<int> pre(piles.size() + 1, 0);
+        for (int i = 0; i < piles.size(); i++) {
+            pre[i + 1] = pre[i] + piles[i];
+        }
+        vector<vector<vector<int>>>dp(piles.size(),vector<vector<int>>(2*piles.size()+1,vector<int>(2,-1)));
+        return helper(0, 1, 0, piles, piles.size(), pre,dp);
     }
 };
