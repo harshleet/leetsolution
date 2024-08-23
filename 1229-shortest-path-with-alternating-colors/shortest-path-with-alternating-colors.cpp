17cdb727e1ab0f7db3c7a1c469cecf73a073c25a
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-23 16:55:51 +0530

    Time: 22 ms (14.56%) | Memory: 19.7 MB (26.41%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
         vector<vector<pair<int,int>>>adj(n);
 
-        map<vector<int>,int>vis;
+        
         for(auto it:redEdges){
             adj[it[0]].push_back({it[1],1});
             // adj[it[1]].push_back({it[0],1});
@@ -13,23 +13,26 @@ public:
             adj[it[0]].push_back({it[1],2});
             // adj[it[1]].push_back({it[0],2});
         }
-        vector<int>dis(n,1e9);
+        vector<vector<int>>vis(n,vector<int>(3,0));
 
+        vector<int>dis(n,1e9);
         queue<vector<int>>q;
-        q.push({0,-1,0});
+        q.push({0,0,0});
+        vis[0][0]=1;
         dis[0]=0;
+
 
         while(!q.empty()){
             vector<int>t=q.front();
             q.pop();
 
             for(auto it:adj[t[0]]){
-                if((t[1]==-1 || (t[1]==2 && it.second==1) || (t[1]==1 && it.second==2)) && dis[it.first]>1+t[2] && vis.find({t[0],it.first,it.second})==vis.end()){
-                     vis[{t[0],it.first,it.second}]=1;
+                if((t[1]==0 || (t[1]==2 && it.second==1) || (t[1]==1 && it.second==2)) && dis[it.first]>1+t[2] && !vis[it.first][it.second]){
+                     vis[it.first][it.second]=1;
                     dis[it.first]=1+t[2];
                     q.push({it.first,it.second,1+t[2]});
-                }else if(t[1]==-1 || (t[1]==2 && it.second==1) || (t[1]==1 && it.second==2)&& vis.find({t[0],it.first,it.second})==vis.end()){
-                     vis[{t[0],it.first,it.second}]=1;
+                }else if((t[1]==0|| (t[1]==2 && it.second==1) || (t[1]==1 && it.second==2))&& !vis[it.first][it.second]){
+                     vis[it.first][it.second]=1;
                     q.push({it.first,it.second,1+t[2]});
                 }
             }
