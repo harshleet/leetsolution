335670727febd5001c68b3ede2cc4cc771da929b
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-27 17:24:32 +0530

    Time: 158 ms (20.96%) | Memory: 74.5 MB (16.52%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        vector<pair<int, double>> adj[n];
-        for (int i = 0; i < edges.size(); i++) {
-            adj[edges[i][0]].push_back({edges[i][1], succProb[i] });
-            adj[edges[i][1]].push_back({ edges[i][0], succProb[i] });  
+        vector<vector<pair<int,double>>>adj(n);
+
+        int j=0;
+        for(auto it:edges){
+            adj[it[0]].push_back({it[1],succProb[j]});
+             adj[it[1]].push_back({it[0],succProb[j]});
+            j++;
         }
-        priority_queue<pair<double,int>>pq;
         vector<double>dis(n,0.0);
+        priority_queue<pair<double,int>>pq;
+
         pq.push({1.0,start_node});
         dis[start_node]=1.0;
-
         while(!pq.empty()){
-            double dist=pq.top().first;
+            double prob=pq.top().first;
             int node=pq.top().second;
             pq.pop();
- 
+
             for(auto it:adj[node]){
-                if(dist*it.second>dis[it.first]){
-                    dis[it.first]=dist*it.second;
-                    pq.push({dist*it.second,it.first});
+                double val=prob*it.second;
+                if(dis[it.first]<val){
+                    dis[it.first]=val;
+                    pq.push({val,it.first});
                 }
             }
+
         }
+
         return dis[end_node];
 
     }
