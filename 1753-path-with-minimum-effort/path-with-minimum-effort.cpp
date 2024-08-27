ee44275310528a80d36f92d75c2c99c82c9ee7b5
harshleet <122361301+harshleet@users.noreply.github.com>
2024-08-27 17:07:14 +0530

    Time: 200 ms (14.40%) | Memory: 38.5 MB (21.84%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size(),m=heights[0].size();
+        vector<vector<int>>dis(n,vector<int>(m,1e9));
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+
+        pq.push({0,0,0});
+        dis[0][0]=0;
         vector<int>dr={1,-1,0,0};
         vector<int>dc={0,0,-1,1};
-
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
-
-        int ans=INT_MAX;
-        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-        pq.push({0,0,0});
-        dist[0][0]=0;
         while(!pq.empty()){
-            int dis=get<0>(pq.top());
-            int r=get<1>(pq.top());
-            int c=get<2>(pq.top());
+            int diff=pq.top()[0];
+            int r=pq.top()[1];
+            int c=pq.top()[2];
+
             pq.pop();
 
             for(int i=0;i<4;i++){
                 int nr=r+dr[i];
                 int nc=c+dc[i];
+
+                
                 if(nr>=0 && nc>=0 && nr<n && nc<m){
-                    int mini=max(dis,abs(heights[nr][nc]-heights[r][c]));
-                     if(dist[nr][nc]>mini){
-                        dist[nr][nc]=mini;
-                        pq.push({mini,nr,nc});
-                     }
+                    int maxi=max(diff,abs(heights[nr][nc]-heights[r][c]));
+                    if(maxi<dis[nr][nc]){
+                        dis[nr][nc]=maxi;
+                        pq.push({dis[nr][nc],nr,nc});
+                    }
                 }
-               
             }
-            
+
         }
-        return dist[n-1][m-1];
+        return dis[n-1][m-1];
     }
 };
