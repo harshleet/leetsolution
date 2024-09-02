124cf350ad6d4f77b2f75d0d5db4338862d4e358
harshleet <122361301+harshleet@users.noreply.github.com>
2024-09-02 23:15:34 +0530

    Time: 241 ms (12.00%) | Memory: 103.9 MB (90.81%) - LeetSync

--- 2483-task-scheduler-ii/task-scheduler-ii.cpp
+++ 2483-task-scheduler-ii/task-scheduler-ii.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     long long taskSchedulerII(vector<int>& tasks, int space) {
-        long long timer=1,i=0;
-        int n=tasks.size();
         map<long long,long long>last;
-        for(long long i=0;i<n;i++){
-            cout<<tasks[i]<<" "<<timer<<endl;
-            if(last.find(tasks[i])==last.end()){
+
+        long long timer=0;
+        for(int i=0;i<tasks.size();i++){
+            timer++;
+            if(last.find(tasks[i])!=last.end() && timer-last[tasks[i]]<=space){
+                // cout<<i<<" "<<last[tasks[i]]<<endl;
+                timer+=(last[tasks[i]]+space-timer+1);
                 last[tasks[i]]=timer;
-                if(i!=n-1){
-                   timer++;
-                } 
-            }else if(space+last[tasks[i]]+1<=timer){
-               
+            }else{
                 last[tasks[i]]=timer;
-                 if(i!=n-1){
-                   timer++;
-                }
-            }else if(space+last[tasks[i]]+1>timer){
-                timer=space+last[tasks[i]]+1;
-                last[tasks[i]]=timer;
-                 if(i!=n-1){
-                   timer++;
-                }
-                 
             }
         }
-        
         return timer;
     }
 };
