630f045b096ed6f52d27d8a20c83cc79269e2222
harshleet <122361301+harshleet@users.noreply.github.com>
2024-09-08 09:30:42 +0530

    Time: 255 ms (74.21%) | Memory: 122.1 MB (49.62%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,38 +1,40 @@
 class MedianFinder {
 public:
-priority_queue<int>pq1;
-priority_queue<int,vector<int>,greater<int>>pq2;
+    priority_queue<int>pq1;
+    priority_queue<int,vector<int>,greater<int>>pq2;
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        if(pq2.size()==pq1.size()){
-            if(!pq1.empty() && pq1.top()>num){
-                pq2.push(pq1.top());
+        if(pq1.empty()){
+            pq1.push(num);
+        }else if(pq1.size()>pq2.size()){
+            if(num>=pq1.top()){
+                pq2.push(num);
+            }else{
+                int val=pq1.top();
                 pq1.pop();
                 pq1.push(num);
-            }else{
-                pq2.push(num);
+                pq2.push(val);
             }
-            
-        }else{
-            if(!pq2.empty() && pq2.top()<num){
-                pq1.push(pq2.top());
-                pq2.pop();
-                pq2.push(num);
-            }else{
+        }else if(pq1.size()==pq2.size()){
+            if(num<=pq2.top()){
                 pq1.push(num);
+            }else{
+                int val=pq2.top();
+                pq2.pop();
+                pq1.push(val);
+                pq2.push(num);
             }
-            
         }
     }
     
     double findMedian() {
-        if(pq1.size()<pq2.size()){
-            return pq2.top();
+        if(pq1.size()==pq2.size()){
+            return (pq1.top()+pq2.top())/2.0;
         }else{
-            return (pq1.top()+pq2.top())/double(2);
+            return double(pq1.top());
         }
     }
 };
