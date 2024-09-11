f5eff1f8aeb12751a4a27ccd3ed79662a878f646
harshleet <122361301+harshleet@users.noreply.github.com>
2024-09-11 19:55:13 +0530

    Time: 21 ms (67.66%) | Memory: 25.9 MB (70.61%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,65 +1,44 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string text1, string text2) {
-        int n=text1.size(),m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<=m;i++){
-            dp[0][i]=0;
-        }
+    
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n=str1.size(),m=str2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                int nt1=dp[i-1][j];
-                int nt2=dp[i][j-1];
-                int t=0;
-                if(text1[i-1]==text2[j-1]){
-                    t=1+dp[i-1][j-1];
+                
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
-                dp[i][j]= max({nt1,nt2,t});
+                
             }
         }
-      
         int i=n,j=m;
-        string lcs="";
-        while(i>=1 && j>=1){
-            if(text1[i-1]==text2[j-1]){
-                lcs+=text1[i-1];
-                i--;
-                j--;
-            }else if(dp[i-1][j]>dp[i][j-1]){
-                i--;
-            }else{
-                j--;
-            }
-        }
-        reverse(lcs.begin(),lcs.end());
-        i=0,j=0;
-        int k=0;
         string ans="";
-        while(i<n && j<m && k<lcs.size()){
-            if(text1[i]==lcs[k] && text2[j]==lcs[k] ){
-                ans+=text1[i];
-                i++;
-                j++;
-                k++;
-            }if(text1[i]!=lcs[k]){
-                ans+=text1[i];
-                i++;
-            }if(text2[j]!=lcs[k]){
-                ans+=text2[j];
-                j++;
+        while(i>=1 && j>=1){
+            if(str1[i-1]==str2[j-1]){
+                ans+=str1[i-1];
+                i--;
+                j--;
+            }else if(dp[i][j-1]>dp[i-1][j]){
+                 ans+=str2[j-1];
+                j--;
+            }else{
+                 ans+=str1[i-1];
+                i--;
             }
         }
-        while(i<n){
-            ans+=text1[i];
-            i++;
+        while(i>=1){
+            ans+=str1[i-1];
+                i--;
         }
-        while(j<m){
-            ans+=text2[j];
-            j++;
+        while(j>=1){
+            ans+=str2[j-1];
+                j--;
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
