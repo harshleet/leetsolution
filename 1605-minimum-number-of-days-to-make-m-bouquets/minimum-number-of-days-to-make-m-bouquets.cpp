fe7e63db773c440c2578b42bb0db7f0725ba7984
harshleet <122361301+harshleet@users.noreply.github.com>
2024-10-30 09:36:37 +0530

    Time: 23 ms (57.76%) | Memory: 69.3 MB (38.38%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    int isPos(int mid,vector<int>&bloomDay,int m,int k){
-        int cnt=0,a=0;
-        // for(int i=0;i<k;i++){
-        
-        // }
+    bool isPos(int mid,vector<int>& bloomDay, int m, int k){
+        cout<<mid<<endl;
+        int c=0;
+
+        int temp=0;
         for(int i=0;i<bloomDay.size();i++){
-                if(bloomDay[i]<=mid){
-                   cnt++;
-                }else{
-                    cnt=0;
-                }
-                if(cnt==k){
-                    a++;
-                    cnt=0;
-                }
+            if(bloomDay[i]<=mid){
+                temp++;
+            }else{
+                temp=0;
+            }
+            if(temp==k){
+                c++;
+                temp=0;
+            }
         }
-        return a>=m;
+        return c>=m;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int low=1,high=*max_element(bloomDay.begin(),bloomDay.end()),ans=-1;
-        while(low<=high){
-            int mid=(low+high)/2;
+        int l=*min_element(bloomDay.begin(),bloomDay.end()),h=*max_element(bloomDay.begin(),bloomDay.end());
+
+        cout<<l<<" "<<h<<endl;
+        int ans=-1;
+        while(l<=h){
+            int mid=(l+h)/2;
             if(isPos(mid,bloomDay,m,k)){
                 ans=mid;
-                 high=mid-1;
+                h=mid-1;
             }else{
-                 low=mid+1;
+                l=mid+1;
             }
         }
         return ans;
