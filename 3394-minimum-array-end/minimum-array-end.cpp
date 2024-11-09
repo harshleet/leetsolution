ceb7d7ec901159bc7d3e41d8e0ffeedb890fdc1f
harshleet <122361301+harshleet@users.noreply.github.com>
2024-11-09 11:36:26 +0530

    Time: 3 ms (31.25%) | Memory: 9.2 MB (23.78%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
- long long bitSetToInt(string &mask){
+
+    long long bitSetToInt(string &mask){
         long long ans=0;
         for(int i=mask.size()-1;i>=1;i--){
             if(mask[i]=='1'){
@@ -10,20 +11,20 @@ public:
         return ans;
     }
     long long minEnd(int n, int x) {
-        //first intution is hard but you try the testcases on a copy it wis easy
+        string t1=bitset<64>(n-1).to_string();
+        string t2=bitset<64>(x).to_string();
 
-        //keep in mind that 1 in this remain intact what changes is the position whre their is zero's
-        // yt -> https://www.youtube.com/watch?v=eA1xIDUqIDc
-        string mask=bitset<63>(x).to_string();
-        string mask2=bitset<63>(n-1).to_string();
-        int j=mask2.size()-1;
-        for(int i=mask.size()-1;i>=0;i--){
-           if(mask[i]=='0'){
-               mask[i]=mask2[j];
-               j--;
-           }
+        // cout<<t1<<" "<<t2<<endl;
+        int c=63;
+        for(int j=63;j>=0;j--){
+            if(t2[j]=='0'){
+                t2[j]=t1[c];
+                c--;
+            }
         }
-        long long ans=bitSetToInt(mask);
+
+         long long ans=bitSetToInt(t2);
         return ans;
+        
     }
 };
