1345500363c13553a6abbdf0ff5943bda5a2fc82
harshleet <122361301+harshleet@users.noreply.github.com>
2024-12-24 13:33:18 +0530

    Time: 138 ms (15.39%) | Memory: 22.5 MB (20.41%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
+        //important point here is tahtwe can go to any point again and again
+        //number of nodes are also less whicvh are 12 sop we can use store the visited in binary
+
         int n=graph.size();
-
-        map<pair<int,int>,int>seen;
-
+        vector<vector<int>>adj(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<graph[i].size();j++){
+                adj[i].push_back(graph[i][j]);
+                adj[graph[i][j]].push_back(i);
+            }
+        }
         queue<tuple<int,int,int>>q;
+        map<pair<int,int>,int>mp;
         for(int i=0;i<n;i++){
             q.push({i,1<<i,0});
-           seen[{i,1<<i}]=1;
+            mp[{i,1<<i}]=1;
         }
+
         
         while(!q.empty()){
+
             int node=get<0>(q.front());
             int bitm=get<1>(q.front());
-            int step=get<2>(q.front());
+            int st=get<2>(q.front());
             q.pop();
-            // cout<<node<<" "<<bitm<<" "<<step<<endl;
-           
+
             if(bitm==(1<<n)-1){
-                return step;
+                return st;
             }
-           
-           
-            for(auto it:graph[node]){
-                if(seen.find({bitm|1<<it,it})==seen.end()){
-                     seen[{bitm|1<<it,it}]=1;
-                q.push({it,bitm|1<<it,step+1});
+
+            for(auto it:adj[node]){
+                if(mp.find({it,bitm|1<<it})==mp.end()){
+                    mp[{it,bitm|1<<it}]=1;
+                    q.push({it,bitm|1<<it,st+1});
                 }
             }
-
-
         }
-       return 0;
+        return 1;
     }
 };
