603db66246a3cd445c44f822246aaefeeae10087
harshleet <122361301+harshleet@users.noreply.github.com>
2024-12-24 10:45:10 +0530

    Time: 159 ms (51.37%) | Memory: 164.5 MB (21.98%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-  void dfs(int node,int no,vector<int>&vis,vector<int>&pvis,vector<vector<int>>&adj,int &maxi){
-    vis[node]=1;
-    pvis[node]=no;
-    // cout<<node<<endl;
-         for(auto it:adj[node]){
-                if(vis[it]==0){
-                   dfs(it,no+1,vis,pvis,adj,maxi);
-                }else if(pvis[it]!=0){
-                    maxi=max(maxi,abs(pvis[node]-pvis[it]+1));
-                }
-       }
-       pvis[node]=0;
-  }
-    int longestCycle(vector<int>& edges) {
-        int n=edges.size();
-        vector<vector<int>>adj(n);
+    void dfs(int node,vector<int>&vis,vector<int>&pvis,vector<vector<int>>&adj,int& maxi,int no){
+        vis[node]=1;
+        pvis[node]=no;
 
-        for(int i=0;i<edges.size();i++){
+        for(auto it:adj[node]){
+            if(vis[it]==0){
+                dfs(it,vis,pvis,adj,maxi,no+1);
+            }else if(pvis[it]!=0){
+                maxi=max(maxi,no-pvis[it]+1);
+            }
+        }
+        pvis[node]=0;
+    }
+    int longestCycle(vector<int>& edges) {
+        //bfs se cycle detct hojaegi but length detct nhi hogi
+
+        int n=edges.size();
+        vector<int>pvis(n,0);
+        vector<int>vis(n,0);
+        vector<vector<int>>adj(n);
+        for(int i=0;i<n;i++){
             if(edges[i]!=-1)
                adj[i].push_back(edges[i]);
         }
-        vector<int>vis(n,0);
-        vector<int>pvis(n,0);
         int maxi=-1;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(i,1,vis,pvis,adj,maxi);
+                dfs(i,vis,pvis,adj,maxi,1);
             }
         }
         return maxi;
-       
     }
 };
