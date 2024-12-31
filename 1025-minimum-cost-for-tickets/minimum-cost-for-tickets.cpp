119bae4de11295186f90315d0a99b30d22ebfd10
harshleet <122361301+harshleet@users.noreply.github.com>
2024-12-31 14:18:12 +0530

    Time: 14 ms (5.07%) | Memory: 23.9 MB (5.11%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-vector<int>d={1,7,30};
-    int helper(int ind,int last,vector<int>&days,vector<int>&costs, vector<vector<int>>&dp){
+    int helper(int day,int ind,vector<vector<int>>&dp,int maxi,vector<int>& days, vector<int>& costs){
         if(ind>=days.size()){
             return 0;
         }
-        if(dp[ind][last+1]!=-1){
-            return dp[ind][last+1];
+        if(dp[day][ind]!=-1){
+            return dp[day][ind];
         }
-         int mini=1e8;
-        if(last>=days[ind]){
-            mini=min(mini,helper(ind+1,last,days,costs,dp));    
-        }
-       
-        for(int i=0;i<3;i++){
-           mini=min(mini,costs[i]+helper(ind+1,days[ind]+d[i]-1,days,costs,dp));
-        }
-        return dp[ind][last+1]=mini;
 
+        int nt=1e8,od=1e8,sd=1e8,td=1e8;
+
+        if(day>days[ind]){
+            nt=helper(day,ind+1,dp,maxi,days,costs);
+        }
+        
+        od=costs[0]+helper(days[ind]+1,ind+1,dp,maxi,days,costs);
+        sd=costs[1]+helper(days[ind]+7,ind+1,dp,maxi,days,costs);
+        td=costs[2]+helper(days[ind]+30,ind+1,dp,maxi,days,costs);
+
+        return dp[day][ind]=min({od,nt,sd,td});
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        sort(days.begin(),days.end());
-        vector<vector<int>>dp(days.size(),vector<int>(days.back()+31,-1));
-        return helper(0,-1,days,costs,dp);
+       sort(days.begin(),days.end());
+        vector<vector<int>>dp(days.back()+32,vector<int>(days.size(),-1));
+
+        return helper(1,0,dp,days.back(),days,costs);
     }
 };
