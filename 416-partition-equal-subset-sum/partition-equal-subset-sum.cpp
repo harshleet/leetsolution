352705b799f673bb6e1d0433090d8600652516e4
harshleet <122361301+harshleet@users.noreply.github.com>
2025-01-09 23:49:31 +0530

    Time: 192 ms (23.10%) | Memory: 100.4 MB (33.14%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-   bool helper(int ind,int sum,int target,vector<int>&nums,vector<vector<int>>&dp){
-    if(sum==target){
+    bool helper(int ind,int sum,vector<vector<int>>&dp,vector<int>&nums,int target){
+        if(sum==target){
             return true;
-     }
-    if(ind>=nums.size() || sum>target){
-        return false;
-    }
-    if(dp[ind][sum]!=-1){
-        return dp[ind][sum];
-    }
-      int nt=helper(ind+1,sum,target,nums,dp);
-      int t=helper(ind+1,sum+nums[ind],target,nums,dp);
-
-      return dp[ind][sum]=nt|t;
-
-   }
-    bool canPartition(vector<int>& nums) {
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%2==1){
+        }
+        if(ind>=nums.size()){
             return false;
         }
-        vector<vector<int>>dp(nums.size(),vector<int>((sum/2)+1,-1));
-        return helper(0,0,sum/2,nums,dp);
+        if(dp[ind][sum]!=-1){
+            return dp[ind][sum];
+        }
+
+        bool nt=helper(ind+1,sum,dp,nums,target);
+        bool t=false;
+        if(sum+nums[ind]<=target){
+            t=helper(ind+1,sum+nums[ind],dp,nums,target);
+        }
+        return dp[ind][sum]=nt|t; 
+
+
+    }
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size(),target=accumulate(nums.begin(),nums.end(),0);
+        if(target%2!=0){
+            return false;
+        }
+        vector<vector<int>>dp(n,vector<int>((target/2)+1,-1));
+        return helper(0,0,dp,nums,target/2);
     }
 };
