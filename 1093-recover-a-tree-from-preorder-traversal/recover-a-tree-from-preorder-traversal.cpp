60f225e0af69f63cf5e3428ab8186547ab50fffd
harshleet <122361301+harshleet@users.noreply.github.com>
2025-02-22 23:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (55.58%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
@@ -1,59 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* recoverFromPreorder(string traversal) {
-        stack<pair<TreeNode*, int>> st;
-        int i = 0;
-        int n = traversal.size();
-
-        string dig = "";
-        while (i < n && traversal[i] != '-') {
-            dig += traversal[i];
-            i++;
+    TreeNode* recoverFromPreorder(string S) {
+        unordered_map<int, TreeNode *> mp;
+        int cur = 0;
+        for (int i = 0; i < S.size(); i++) {
+            if (S[i] == '-') {
+                cur += 1;
+            }
+            else {
+                string num = "";
+                while (i < S.size() && S[i] >= '0' && S[i] <= '9')
+                    num += S[i++];
+                i--;
+                int cn = stoi(num);
+                mp[cur] = new TreeNode(cn);
+                if (cur)
+                    if (mp[cur - 1]->left)
+                        mp[cur - 1]->right = mp[cur];
+                    else
+                        mp[cur - 1]->left = mp[cur];
+                cur = 0;
+            }
         }
-        TreeNode* root = new TreeNode(stoi(dig));
-        st.push({root, 0});
-
-        while (i < n) {
-            int cnt = 0;
-
-            while (i < n && traversal[i] == '-') {
-                cnt++;
-                i++;
-            }
-
-            dig = "";
-            while (i < n && traversal[i] != '-') {
-                dig += traversal[i];
-                i++;
-            }
-            int val = stoi(dig);
-
-            TreeNode* node = new TreeNode(val);
-
-            while (!st.empty() && st.top().second >= cnt) {
-                st.pop();
-            }
-
-            if (st.top().first->left == nullptr) {
-                st.top().first->left = node;
-            } else {
-                st.top().first->right = node;
-            }
-
-            st.push({node, cnt});
-        }
-
-        return root;
+        return mp[0];
     }
 };
