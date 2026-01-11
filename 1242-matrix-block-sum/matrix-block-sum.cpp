e75feb962b9c0de1ca79f7abacf90b1111a95c8a
harshleet <122361301+harshleet@users.noreply.github.com>
2026-01-11 20:12:59 +0530

    Time: 7 ms (30.39%) | Memory: 13.7 MB (63.04%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.cpp
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -1,22 +1,24 @@
-#include <vector>
-
 class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, 0));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int val = 0;
-                for (int x = max(0, i - k); x <= min(n - 1, i + k); x++) {
-                    for (int y = max(0, j - k); y <= min(m - 1, j + k); y++) {
-                        val += mat[x][y];
-                    }
-                }
-                ans[i][j] = val;
+        int n=mat.size(),m=mat[0].size();
+        vector<vector<int>>pre(n,vector<int>(m+1,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                pre[i][j+1]=pre[i][j]+mat[i][j];
             }
-        }
+        } 
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int sum=0;
+                for(int f=max(0,i-k);f<=min(n-1,i+k);f++){
+                    int minc=max(0,j-k),maxc=min(m-1,j+k);
+                    sum+=pre[f][maxc+1]-pre[f][minc];
+                }
+                ans[i][j]=sum;
+            }
+        } 
         return ans;
     }
 };
