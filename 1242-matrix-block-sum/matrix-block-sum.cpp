4598d10e085abb92bdbc4de1a42102722eb67e81
harshleet <122361301+harshleet@users.noreply.github.com>
2026-01-13 22:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (19.49%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.cpp
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -2,20 +2,35 @@ class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int n=mat.size(),m=mat[0].size();
-        vector<vector<int>>pre(n,vector<int>(m+1,0));
-        for(int i=0;i<n;i++){
+        vector<vector<int>>pre(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
             for(int j=0;j<m;j++){
-                pre[i][j+1]=pre[i][j]+mat[i][j];
+                pre[i][j+1]=pre[i][j]+mat[i-1][j];
             }
         } 
+        // for(int i=0;i<n+1;i++){
+        //     for(int j=0;j<m+1;j++){
+        //         cout<<pre[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // } 
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n;j++){
+                pre[j+1][i]+=pre[j][i];
+            }
+        }
+        // for(int i=0;i<n+1;i++){
+        //     for(int j=0;j<m+1;j++){
+        //         cout<<pre[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // } 
         vector<vector<int>>ans(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 int sum=0;
-                for(int f=max(0,i-k);f<=min(n-1,i+k);f++){
-                    int minc=max(0,j-k),maxc=min(m-1,j+k);
-                    sum+=pre[f][maxc+1]-pre[f][minc];
-                }
+                int minc=max(0,j-k),maxc=min(m-1,j+k),minr=max(0,i-k),maxr=min(n-1,i+k);  
+                sum=pre[maxr+1][maxc+1]-pre[maxr+1][minc]-pre[minr][maxc+1]+pre[minr][minc];
                 ans[i][j]=sum;
             }
         } 
