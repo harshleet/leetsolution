955757373ec38fb9a9c4eb3ae9333dd229db2c9e
harshleet <122361301+harshleet@users.noreply.github.com>
2026-09-12 14:42:35 +0530

    Time: 183 ms (60.04%) | Memory: 187 MB (14.18%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
-    void dfs(int node,int par,int timer, vector<vector<int>>&ans,vector<int>adj[],vector<int>&vis, vector<int>&intime,vector<int>&low){
+    vector<vector<int>>ans;
+    int timer=0;
+    void helper(int node,int parent,vector<vector<pair<int,int>>>&adj,vector<int>&disc,vector<int>&low,int n,vector<int>&vis,  vector<vector<int>>& connections){
         vis[node]=1;
-        intime[node]=timer;
+        disc[node]=timer;
         low[node]=timer;
         timer++;
-        for(auto it:adj[node]){
-            if(it==par){
-                continue;
-            }
-            if(vis[it]==0){
-                dfs(it,node,timer,ans,adj,vis,intime,low);
-                low[node]=min(low[node],low[it]);
-                if(low[it]>intime[node]){
-                    ans.push_back({it,node});
-                }
-            }else{
-                  low[node]=min(low[node],low[it]);
-            }
 
+
+        for(auto it:adj[node]){
+            if(!vis[it.first]){
+                // not visited and definetely not a parent
+                helper(it.first,node,adj,disc,low,n,vis,connections);
+                // update the low with something if child had been reached
+                low[node]=min(low[node],low[it.first]);
+
+                // now we check disc
+                if(low[it.first]>disc[node]){
+                    ans.push_back(connections[it.second]);
+                }
+
+            }else if(it.first==parent){
+                //if parent do nothing
+                continue;
+            }else{
+                //visted and not a parent
+                low[node]=min(low[node],low[it.first]);
+            }
         }
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<int>adj[n];
-        vector<int>vis(n,0);
-        int step=1;
+        // bridges question
+        vector<vector<pair<int,int>>>adj(n);
         for(int i=0;i<connections.size();i++){
-            adj[connections[i][0]].push_back(connections[i][1]);
-            adj[connections[i][1]].push_back(connections[i][0]);
+            adj[connections[i][0]].push_back({connections[i][1],i});
+            adj[connections[i][1]].push_back({connections[i][0],i});
         }
-        int timer=1;
-        vector<int>intime(n);
+        vector<int>disc(n,-1);
         vector<int>low(n);
-        vector<vector<int>>ans;
-        dfs(0,-1,timer,ans,adj,vis,intime,low);
+        vector<int>vis(n);
+        helper(0,-1,adj,disc,low,n,vis,connections);
         return ans;
     }
 };
